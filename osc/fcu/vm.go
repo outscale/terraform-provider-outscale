a04371b994eeff2f82b8bccc8d9e706da3ac72f7
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-02-07 15:10:51 -0600

    Add interfaces for the VM service

---
+++ osc/fcu/vm.go
@@ -0,0 +1,7 @@
+package fcu
+
+//VMOperations defines all the operations needed for FCU VMs
+type VMOperations struct{}
+
+//VMService all the necessary actions for them VM service
+type VMService interface{}
