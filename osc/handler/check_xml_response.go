3c23925235a63f09428a3acd1b8016513c6c210d
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-02-07 15:08:14 -0600

    Should return only an error and receive a pointer interface

--- osc/handler/check_xml_response.go
+++ osc/handler/check_xml_response.go
@@ -9,16 +9,16 @@ import (
 )
 
 // UnmarshalXML unmarshals a response body for the XML protocol.
-func UnmarshalXML(r *http.Response) (interface{}, error) {
+func UnmarshalXML(v interface{}, r *http.Response) error {
 	defer r.Body.Close()
 	decoder, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		return "", fmt.Errorf("Read body: %v", err)
+		return fmt.Errorf("Read body: %v", err)
 	}
 	var data interface{}
 
 	if err := xml.Unmarshal([]byte(decoder), &data); err != nil {
-		return nil, errors.New("SerializationError" + "failed decoding EC2 Query response" + fmt.Sprint(err))
+		return errors.New("SerializationError" + "failed decoding EC2 Query response" + fmt.Sprint(err))
 	}
-	return data, nil
+	return nil
 }
--- osc/handler/check_xml_response_test.go
+++ osc/handler/check_xml_response_test.go
@@ -10,7 +10,8 @@ import (
 func TestUnmarshalXML(t *testing.T) {
 	buf := bytes.NewReader([]byte("<OperationNameResponse><Str>myname</Str><FooNum>123</FooNum><FalseBool>false</FalseBool><TrueBool>true</TrueBool><Float>1.2</Float><Double>1.3</Double><Long>200</Long><Char>a</Char><RequestId>request-id</RequestId></OperationNameResponse>"))
 	res := &http.Response{StatusCode: 200, Body: ioutil.NopCloser(buf), Header: http.Header{}}
-	if _, err := UnmarshalXML(res); err != nil {
+	var v interface{}
+	if err := UnmarshalXML(v, res); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
