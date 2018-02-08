8482ad12877fc2cf24fa5dc12b4b9e56f78eff56
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-02-07 19:32:28 -0600

    Change xml marshaler

--- osc/handler/check_xml_response.go
+++ osc/handler/check_xml_response.go
@@ -4,21 +4,21 @@ import (
 	"encoding/xml"
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+
+	"github.com/aws/aws-sdk-go/private/protocol/xml/xmlutil"
 )
 
 // UnmarshalXML unmarshals a response body for the XML protocol.
 func UnmarshalXML(v interface{}, r *http.Response) error {
 	defer r.Body.Close()
-	decoder, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return fmt.Errorf("Read body: %v", err)
-	}
-	var data interface{}
 
-	if err := xml.Unmarshal([]byte(decoder), &data); err != nil {
+	decoder := xml.NewDecoder(r.Body)
+	err := xmlutil.UnmarshalXML(v, decoder, "")
+
+	if err != nil {
 		return errors.New("SerializationError" + "failed decoding EC2 Query response" + fmt.Sprint(err))
 	}
+
 	return nil
 }
--- osc/handler/check_xml_response_test.go
+++ osc/handler/check_xml_response_test.go
@@ -10,8 +10,8 @@ import (
 func TestUnmarshalXML(t *testing.T) {
 	buf := bytes.NewReader([]byte("<OperationNameResponse><Str>myname</Str><FooNum>123</FooNum><FalseBool>false</FalseBool><TrueBool>true</TrueBool><Float>1.2</Float><Double>1.3</Double><Long>200</Long><Char>a</Char><RequestId>request-id</RequestId></OperationNameResponse>"))
 	res := &http.Response{StatusCode: 200, Body: ioutil.NopCloser(buf), Header: http.Header{}}
-	var v interface{}
-	if err := UnmarshalXML(v, res); err != nil {
+	var v struct{}
+	if err := UnmarshalXML(&v, res); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
