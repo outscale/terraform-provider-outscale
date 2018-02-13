0e4febf7982b2396aa4010652a865c92610be828
Cramirez <user@Cristian-MacBook-Pro.local>
2018-02-13 15:35:52 -0600

    add oapi outscale vm datasource test

---
+++ outscale/data_source_outscale_oapi_vm_test.go
@@ -0,0 +1,52 @@
+package outscale
+
+import (
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccOutscaleOAPIVMDataSource_basic(t *testing.T) {
+	o := os.Getenv("OUTSCALE_OAPI")
+
+	oapi, err := strconv.ParseBool(o)
+	if err != nil {
+		oapi = false
+	}
+
+	if oapi == false {
+		t.Skip()
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOAPIVMDataSourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.outscale_vm.basic_web", "image_id", "ami-8a6a0120"),
+					resource.TestCheckResourceAttr(
+						"data.outscale_vm.basic_web", "instance_type", "t2.micro"),
+				),
+			},
+		},
+	})
+}
+
+// Lookup based on InstanceID
+const testAccOAPIVMDataSourceConfig = `
+resource "outscale_vm" "basic" {
+  image_id = "ami-8a6a0120"
+	instance_type = "t2.micro"
+}
+
+data "outscale_vm" "basic_web" {
+	filter {
+    name = "instance-id"
+    values = ["${outscale_vm.basic.id}"]
+  }
+}`
