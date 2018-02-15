f4a445ecbde3151deaf11f041f67d1809bef0c07
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-02-15 09:01:10 -0600

    Add Missong fields

--- osc/fcu/fcu_structs.go
+++ osc/fcu/fcu_structs.go
@@ -35,6 +35,12 @@ type DescribeInstancesOutput struct {
 
 	NextToken *string `locationName:"nextToken" type:"string"`
 
+	OwnerId *string `locationName:"ownerId" locationNameList:"item" type:"string"`
+
+	RequesterId *string `locationName:"requesterId" locationNameList:"item" type:"string"`
+
+	ReservationId *string `locationName:"reservationId" locationNameList:"item" type:"string"`
+
 	Reservations []*Reservation `locationName:"reservationSet" locationNameList:"item" type:"list"`
 
 	GroupSet []*GroupIdentifier `locationName:"groupSet" locationNameList:"item" type:"list"`
