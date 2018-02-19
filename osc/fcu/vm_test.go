2cbd95951bc6cc676d01a586980320727b2d0ad8
Cramirez <user@Cristian-MacBook-Pro.local>
2018-02-19 17:23:28 -0600

    add test for verification on missing attr like ownerid, requesterid, reservationid

--- osc/fcu/vm_test.go
+++ osc/fcu/vm_test.go
@@ -192,3 +192,108 @@ func TestVM_TerminateInstances(t *testing.T) {
 		t.Fatalf("Expected InstanceID:(%s), Got(%s)", outputInstanceID, expectedID)
 	}
 }
+
+func TestVM_GetOwnerId(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var maxC int64
+	imageID := "ami-8a6a0120"
+	maxC = 1
+
+	input := &RunInstancesInput{
+		ImageId:  &imageID,
+		MaxCount: &maxC,
+		MinCount: &maxC,
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, `<?xml version="1.0" encoding="UTF-8"?>
+      <RunInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2014-06-15/"><requestId>193ddebf-63d4-466d-9fe1-d5b74b9962f0</requestId><reservationId>r-071eb05d</reservationId><ownerId>520679080430</ownerId><groupSet><item><groupId>sg-6ed31f3e</groupId><groupName>default</groupName></item></groupSet><instancesSet><item><instanceId>i-d470ce8f</instanceId><imageId>ami-8a6a0120</imageId><instanceState><code>0</code><name>pending</name></instanceState><privateDnsName>ip-10-9-10-212.eu-west-2.compute.internal</privateDnsName><dnsName></dnsName><keyName></keyName><amiLaunchIndex>0</amiLaunchIndex><productCodes/><instanceType>m1.small</instanceType><launchTime>2018-02-08T00:51:38.866Z</launchTime><placement><availabilityZone>eu-west-2a</availabilityZone><groupName></groupName><tenancy>default</tenancy></placement><kernelId></kernelId><monitoring><state>disabled</state></monitoring><privateIpAddress>10.9.10.212</privateIpAddress><groupSet><item><groupId>sg-6ed31f3e</groupId><groupName>default</groupName></item></groupSet><architecture>x86_64</architecture><rootDeviceType>ebs</rootDeviceType><rootDeviceName>/dev/sda1</rootDeviceName><blockDeviceMapping><item><deviceName>/dev/sda1</deviceName><ebs><volumeId>vol-ee2f2a14</volumeId><status>attaching</status><attachTime>2018-02-08T00:51:38.866Z</attachTime><deleteOnTermination>true</deleteOnTermination></ebs></item></blockDeviceMapping><virtualizationType>hvm</virtualizationType><clientToken></clientToken><hypervisor>xen</hypervisor><networkInterfaceSet/><ebsOptimized>false</ebsOptimized></item></instancesSet></RunInstancesResponse>
+      `)
+	})
+
+	server, err := client.VM.RunInstance(input)
+	if err != nil {
+		t.Errorf("VM.RunInstance returned error: %v", err)
+	}
+
+	ownerID := *server.OwnerId
+	expectedOwnerID := "520679080430"
+
+	if ownerID != expectedOwnerID {
+		t.Fatalf("Expected OwnerID:(%s), Got(%s)", ownerID, expectedOwnerID)
+	}
+}
+
+func TestVM_GetRequesterID(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var maxC int64
+	imageID := "ami-8a6a0120"
+	maxC = 1
+
+	input := &RunInstancesInput{
+		ImageId:  &imageID,
+		MaxCount: &maxC,
+		MinCount: &maxC,
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, `<?xml version="1.0" encoding="UTF-8"?>
+      <RunInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2014-06-15/"><requestId>193ddebf-63d4-466d-9fe1-d5b74b9962f0</requestId><reservationId>r-071eb05d</reservationId><ownerId>520679080430</ownerId><groupSet><item><groupId>sg-6ed31f3e</groupId><groupName>default</groupName></item></groupSet><instancesSet><item><instanceId>i-d470ce8f</instanceId><imageId>ami-8a6a0120</imageId><instanceState><code>0</code><name>pending</name></instanceState><privateDnsName>ip-10-9-10-212.eu-west-2.compute.internal</privateDnsName><dnsName></dnsName><keyName></keyName><amiLaunchIndex>0</amiLaunchIndex><productCodes/><instanceType>m1.small</instanceType><launchTime>2018-02-08T00:51:38.866Z</launchTime><placement><availabilityZone>eu-west-2a</availabilityZone><groupName></groupName><tenancy>default</tenancy></placement><kernelId></kernelId><monitoring><state>disabled</state></monitoring><privateIpAddress>10.9.10.212</privateIpAddress><groupSet><item><groupId>sg-6ed31f3e</groupId><groupName>default</groupName></item></groupSet><architecture>x86_64</architecture><rootDeviceType>ebs</rootDeviceType><rootDeviceName>/dev/sda1</rootDeviceName><blockDeviceMapping><item><deviceName>/dev/sda1</deviceName><ebs><volumeId>vol-ee2f2a14</volumeId><status>attaching</status><attachTime>2018-02-08T00:51:38.866Z</attachTime><deleteOnTermination>true</deleteOnTermination></ebs></item></blockDeviceMapping><virtualizationType>hvm</virtualizationType><clientToken></clientToken><hypervisor>xen</hypervisor><networkInterfaceSet/><ebsOptimized>false</ebsOptimized></item></instancesSet></RunInstancesResponse>
+      `)
+	})
+
+	server, err := client.VM.RunInstance(input)
+	if err != nil {
+		t.Errorf("VM.RunInstance returned error: %v", err)
+	}
+
+	requesterID := *server.RequesterId
+	expectedrequesterID := "193ddebf-63d4-466d-9fe1-d5b74b9962f0"
+
+	fmt.Println(requesterID, expectedrequesterID)
+
+	if requesterID != expectedrequesterID {
+		t.Fatalf("Expected OwnerID:(%s), Got(%s)", requesterID, expectedrequesterID)
+	}
+}
+func TestVM_GetReservationID(t *testing.T) {
+	setup()
+	defer teardown()
+
+	var maxC int64
+	imageID := "ami-8a6a0120"
+	maxC = 1
+
+	input := &RunInstancesInput{
+		ImageId:  &imageID,
+		MaxCount: &maxC,
+		MinCount: &maxC,
+	}
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		fmt.Fprintf(w, `<?xml version="1.0" encoding="UTF-8"?>
+      <RunInstancesResponse xmlns="http://ec2.amazonaws.com/doc/2014-06-15/"><requestId>193ddebf-63d4-466d-9fe1-d5b74b9962f0</requestId><reservationId>r-071eb05d</reservationId><ownerId>520679080430</ownerId><groupSet><item><groupId>sg-6ed31f3e</groupId><groupName>default</groupName></item></groupSet><instancesSet><item><instanceId>i-d470ce8f</instanceId><imageId>ami-8a6a0120</imageId><instanceState><code>0</code><name>pending</name></instanceState><privateDnsName>ip-10-9-10-212.eu-west-2.compute.internal</privateDnsName><dnsName></dnsName><keyName></keyName><amiLaunchIndex>0</amiLaunchIndex><productCodes/><instanceType>m1.small</instanceType><launchTime>2018-02-08T00:51:38.866Z</launchTime><placement><availabilityZone>eu-west-2a</availabilityZone><groupName></groupName><tenancy>default</tenancy></placement><kernelId></kernelId><monitoring><state>disabled</state></monitoring><privateIpAddress>10.9.10.212</privateIpAddress><groupSet><item><groupId>sg-6ed31f3e</groupId><groupName>default</groupName></item></groupSet><architecture>x86_64</architecture><rootDeviceType>ebs</rootDeviceType><rootDeviceName>/dev/sda1</rootDeviceName><blockDeviceMapping><item><deviceName>/dev/sda1</deviceName><ebs><volumeId>vol-ee2f2a14</volumeId><status>attaching</status><attachTime>2018-02-08T00:51:38.866Z</attachTime><deleteOnTermination>true</deleteOnTermination></ebs></item></blockDeviceMapping><virtualizationType>hvm</virtualizationType><clientToken></clientToken><hypervisor>xen</hypervisor><networkInterfaceSet/><ebsOptimized>false</ebsOptimized></item></instancesSet></RunInstancesResponse>
+      `)
+	})
+
+	server, err := client.VM.RunInstance(input)
+	if err != nil {
+		t.Errorf("VM.RunInstance returned error: %v", err)
+	}
+
+	reservationID := *server.ReservationId
+	expectedReservationID := "r-071eb05d"
+
+	fmt.Println("OUTPUT =>", reservationID, expectedReservationID)
+
+	if reservationID != expectedReservationID {
+		t.Fatalf("Expected OwnerID:(%s), Got(%s)", reservationID, expectedReservationID)
+	}
+}
