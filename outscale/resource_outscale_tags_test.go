0feb59e8446a3f4c62a0e3f2d5087e9da5e54318
Cramirez <user@Cristian-MacBook-Pro.local>
2018-02-27 16:56:50 -0600

    adding outscale tag resource testing

--- outscale/resource_outscale_tags_test.go
+++ outscale/resource_outscale_tags_test.go
@@ -1,39 +1,70 @@
 package outscale
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/fcu"
 )
 
-func TestAccOutscaleTag_basic(t *testing.T) {
+func TestAccOutscaleVM_tags(t *testing.T) {
+	var v fcu.Instance
+
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		// CheckDestroy: testAccCheckTagDestroy,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOutscaleVMDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccTagConfig_basic(),
-				Check:  resource.ComposeTestCheckFunc(
-				// resource.TestCheckResourceAttr(),
+				Config: testAccCheckInstanceConfigTags,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleVMExists("outscale_vm.foo", &v),
+					testAccCheckTags(&v.Tags, "foo", "bar"),
+					// Guard against regression of https://github.com/hashicorp/terraform/issues/914
+					testAccCheckTags(&v.Tags, "#", ""),
 				),
 			},
 		},
 	})
 }
 
-func testAccTagConfig_basic() string {
-	return `
-resource "outscale_vm" "basic" {
-	Tag_id = "ami-8a6a0120"
-	instance_type = "t2.micro"
-	key_name = "terraform-basic"
-	security_group = ["sg-6ed31f3e"]
+func testAccCheckTags(
+	ts *[]*fcu.Tag, key string, value string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		m := tagsToMap(*ts)
+		v, ok := m[key]
+		if value != "" && !ok {
+			return fmt.Errorf("Missing tag: %s", key)
+		} else if value == "" && ok {
+			return fmt.Errorf("Extra tag: %s", key)
+		}
+		if value == "" {
+			return nil
+		}
+
+		if v != value {
+			return fmt.Errorf("%s: bad value: %s", key, v)
+		}
+
+		return nil
+	}
+}
+
+const testAccCheckInstanceConfigTags = `
+resource "outscale_vm" "foo" {
+	image_id = "ami-8a6a0120"
+	instance_type = "m1.small"
+	tags {
+		foo = "bar"
+	}
 }
 
 resource "outscale_tag" "foo" {
-	name = "tf-testing-%d"
-	instance_id = "${outscale_vm.basic.id}"
+	resource_ids = ["${outscale_vm.foo.id}"]
+	tags {
+		faz = "baz"
+	}
 }
-	`
-}
+`
