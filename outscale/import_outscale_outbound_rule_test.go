da078bb53a0dd967c0491b35e3db92525f4fe6b5
Cramirez <user@Cristian-MacBook-Pro.local>
2018-03-02 00:41:19 -0600

    temp commit for import tests

---
+++ outscale/import_outscale_outbound_rule_test.go
@@ -0,0 +1,32 @@
+package outscale
+
+// import (
+// 	"testing"
+
+// 	"github.com/hashicorp/terraform/helper/acctest"
+// 	"github.com/hashicorp/terraform/helper/resource"
+// )
+
+// func TestAccOutscaleOutboundRule_importBasic(t *testing.T) {
+// 	resourceName := "outscale_outbound_rule.egress_1"
+
+// 	rInt := acctest.RandInt()
+
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:     func() { testAccPreCheck(t) },
+// 		Providers:    testAccProviders,
+// 		CheckDestroy: testAccCheckOutscaleSGRuleDestroy,
+// 		Steps: []resource.TestStep{
+// 			resource.TestStep{
+// 				Config: testAccOutscaleOutboundRuleConfig(rInt),
+// 			},
+
+// 			resource.TestStep{
+// 				ResourceName:            resourceName,
+// 				ImportState:             true,
+// 				ImportStateVerify:       true,
+// 				ImportStateVerifyIgnore: []string{"associate_public_ip_address", "user_data", "security_group"},
+// 			},
+// 		},
+// 	})
+// }
