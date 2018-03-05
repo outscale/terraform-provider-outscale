d9535b1e9dfd761ab38a701e7b952a0b23954f3f
Cramirez <user@Cristian-MacBook-Pro.local>
2018-03-05 13:44:11 -0600

    adding outscale tag and tags datasources

--- outscale/provider.go
+++ outscale/provider.go
@@ -62,8 +62,10 @@ func Provider() terraform.ResourceProvider {
 			"outscale_public_ip_link":     GetResource(fcu, "outscale_public_ip_link")(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
-			"outscale_vm":  GetDatasource(fcu, "outscale_vm")(),
-			"outscale_vms": GetDatasource(fcu, "outscale_vms")(),
+			"outscale_vm":   GetDatasource(fcu, "outscale_vm")(),
+			"outscale_vms":  GetDatasource(fcu, "outscale_vms")(),
+			"outscale_tag":  GetDatasource(fcu, "outscale_tag")(),
+			"outscale_tags": GetDatasource(fcu, "outscale_tags")(),
 		},
 
 		ConfigureFunc: providerConfigureClient,
