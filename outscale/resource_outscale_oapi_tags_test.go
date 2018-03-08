f6188f8afe2689523974f1b11a4115f54860424c
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-08 14:09:14 -0600

    adding outscale tags resource as well its test

---
+++ outscale/resource_outscale_oapi_tags_test.go
@@ -0,0 +1,82 @@
+package outscale
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/fcu"
+)
+
+func TestAccOutscaleOAPIVM_tags(t *testing.T) {
+	o := os.Getenv("OUTSCALE_OAPI")
+
+	oapi, err := strconv.ParseBool(o)
+	if err != nil {
+		oapi = false
+	}
+
+	if oapi == false {
+		t.Skip()
+	}
+	var v fcu.Instance
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOutscaleVMDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckOAPIInstanceConfigTags,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleVMExists("outscale_vm.foo", &v),
+					testAccCheckOAPITags(&v.Tags, "foo", "bar"),
+					// Guard against regression of https://github.com/hashicorp/terraform/issues/914
+					testAccCheckOAPITags(&v.Tags, "#", ""),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOAPITags(
+	ts *[]*fcu.Tag, key string, value string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		m := tagsToMap(*ts)
+		v, ok := m[0]["Key"]
+		if value != "" && !ok {
+			return fmt.Errorf("Missing tag: %s", key)
+		} else if value == "" && ok {
+			return fmt.Errorf("Extra tag: %s", key)
+		}
+		if value == "" {
+			return nil
+		}
+
+		if v != value {
+			return fmt.Errorf("%s: bad value: %s", key, v)
+		}
+
+		return nil
+	}
+}
+
+const testAccCheckOAPIInstanceConfigTags = `
+resource "outscale_vm" "foo" {
+	image_id = "ami-8a6a0120"
+	instance_type = "m1.small"
+	tag = {
+		foo = "bar"
+	}
+}
+
+resource "outscale_tag" "foo" {
+	resource_ids = ["${outscale_vm.foo.id}"]
+	tag = {
+		faz = "baz"
+	}
+}
+`
