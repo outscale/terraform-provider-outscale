a6b47035fe63356308c51eaaf7b7c6a7818bc142
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-08 17:31:08 -0600

    adding tf test

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,34 @@
-# resource "outscale_image" "outscale_image" {
-#     name = "terraform testtest"
-#     instance_id = "i-55031358"
-#     no_reboot = "true"
-# }
+resource "outscale_inbound_rule" "outscale_inbound_rule1" {
+     count = 1 
 
-data "outscale_vms" "basic_web" {}
+    ip_permissions = {
+        from_port = 22
+        to_port = 22
+        ip_protocol = "tcp"
+        ip_ranges = ["46.231.147.8/32"]
+    }
+
+    group_id = "${outscale_firewall_rules_set.outscale_firewall_rules_set.id}"
+}
+
+resource "outscale_inbound_rule" "outscale_inbound_rule2" {
+count = 1
+
+     ip_permissions = {
+        from_port = 443
+        to_port = 443
+        ip_protocol = "tcp"
+        ip_ranges = ["46.231.147.8/32"]
+    }
+
+    group_id = "${outscale_firewall_rules_set.outscale_firewall_rules_set.id}"
+}
+
+resource "outscale_firewall_rules_set" "outscale_firewall_rules_set" {
+    count = 1
+
+    group_description = "test group tf"
+    group_name = "sg1-test-group_test1"
+}
+
+data "outscale_firewall_rules_set" "by_filter" {}
