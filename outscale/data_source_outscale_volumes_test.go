a44eba2880e6155121a251d12e6635dbc434c6e6
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-12 12:29:53 -0600

    outscale volumes datasource test

--- outscale/data_source_outscale_volumes_test.go
+++ outscale/data_source_outscale_volumes_test.go
@@ -6,35 +6,40 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccOutscaleEbsVolumesDataSource_basic(t *testing.T) {
+func TestAccOutscaleVolumeDataSource_multipleFilters(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckOutscaleEbsVolumesDataSourceConfig,
+				Config: testAccCheckOutscaleVolumeDataSourceConfigWithMultipleFilters,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckAwsEbsVolumeDataSourceID("data.outscale_volumes.ebs_volume"),
-					resource.TestCheckResourceAttr("data.outscale_volumes.ebs_volume", "size", "40"),
+					testAccCheckOutscaleVolumeDataSourceID("data.outscale_volumes.ebs_volume"),
+					resource.TestCheckResourceAttr("data.outscale_volumes.ebs_volume", "volume_set.0.size", "10"),
+					resource.TestCheckResourceAttr("data.outscale_volumes.ebs_volume", "volume_set.0.volume_type", "gp2"),
 				),
 			},
 		},
 	})
 }
 
-const testAccCheckOutscaleEbsVolumesDataSourceConfig = `
-resource "outscale_volume" "example" {
+const testAccCheckOutscaleVolumeDataSourceConfigWithMultipleFilters = `
+resource "outscale_volume" "external1" {
     availability_zone = "eu-west-2a"
     volume_type = "gp2"
-    size = 40
+    size = 10
     tags {
-        Name = "External Volume"
+        Name = "External Volume 1"
     }
 }
 data "outscale_volumes" "ebs_volume" {
     filter {
+	name = "size"
+	values = ["${outscale_volume.external1.size}"]
+    }
+    filter {
 	name = "volume-type"
-	values = ["${outscale_volume.example.volume_type}"]
+	values = ["${outscale_volume.external1.volume_type}"]
     }
 }
 `
