503e9d4deb58768f0e93b4e4bfd73b31810210b6
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-12 12:30:52 -0600

    volume and volume link resource oapi test

---
+++ outscale/resource_outscale_oapi_volume_link_test.go
@@ -0,0 +1,90 @@
+package outscale
+
+import (
+	"fmt"
+	"log"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/fcu"
+)
+
+func TestAccOutscaleOAPIVolumeAttachment_basic(t *testing.T) {
+	var i fcu.Instance
+	var v fcu.Volume
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOAPIVolumeAttachmentDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVolumeAttachmentConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"outscale_volume_link.ebs_att", "device", "/dev/sdh"),
+					testAccCheckInstanceExists(
+						"outscale_vm.web", &i),
+					testAccCheckOAPIVolumeExists(
+						"outscale_volume.example", &v),
+					testAccCheckOAPIVolumeAttachmentExists(
+						"outscale_volume_link.ebs_att", &i, &v),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOAPIVolumeAttachmentDestroy(s *terraform.State) error {
+	for _, rs := range s.RootModule().Resources {
+		log.Printf("\n\n----- This is never called")
+		if rs.Type != "outscale_volume_link" {
+			continue
+		}
+	}
+	return nil
+}
+
+func testAccCheckOAPIVolumeAttachmentExists(n string, i *fcu.Instance, v *fcu.Volume) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		for _, b := range i.BlockDeviceMappings {
+			if rs.Primary.Attributes["device"] == *b.DeviceName {
+				if b.Ebs.VolumeId != nil && rs.Primary.Attributes["volume_id"] == *b.Ebs.VolumeId {
+					// pass
+					return nil
+				}
+			}
+		}
+
+		return fmt.Errorf("Error finding instance/volume")
+	}
+}
+
+const testAccOAPIVolumeAttachmentConfig = `
+resource "outscale_vm" "web" {
+	image_id = "ami-8a6a0120"
+	instance_type = "t1.micro"
+	tags {
+		Name = "HelloWorld"
+	}
+}
+resource "outscale_volume" "example" {
+  availability_zone = "eu-west-2a"
+	size = 1
+}
+resource "outscale_volume_link" "ebs_att" {
+  device = "/dev/sdh"
+	volume_id = "${outscale_volume.example.id}"
+	instance_id = "${outscale_vm.web.id}"
+}
+`
---
+++ outscale/resource_outscale_oapi_volume_test.go
@@ -0,0 +1,117 @@
+package outscale
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/fcu"
+)
+
+func TestAccOutscaleOAPIVolume_basic(t *testing.T) {
+	var v fcu.Volume
+	resource.Test(t, resource.TestCase{
+		PreCheck:      func() { testAccPreCheck(t) },
+		IDRefreshName: "outscale_volume.test",
+		Providers:     testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOutscaleOAPIVolumeConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOAPIVolumeExists("outscale_volume.test", &v),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOutscaleOAPIVolume_updateSize(t *testing.T) {
+	var v fcu.Volume
+	resource.Test(t, resource.TestCase{
+		PreCheck:      func() { testAccPreCheck(t) },
+		IDRefreshName: "outscale_volume.test",
+		Providers:     testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOutscaleOAPIVolumeConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOAPIVolumeExists("outscale_volume.test", &v),
+					resource.TestCheckResourceAttr("outscale_volume.test", "size", "1"),
+				),
+			},
+			{
+				Config: testOutscaleOAPIVolumeConfigUpdateSize,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOAPIVolumeExists("outscale_volume.test", &v),
+					resource.TestCheckResourceAttr("outscale_volume.test", "size", "10"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOAPIVolumeExists(n string, v *fcu.Volume) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		conn := testAccProvider.Meta().(*OutscaleClient).FCU
+
+		request := &fcu.DescribeVolumesInput{
+			VolumeIds: []*string{aws.String(rs.Primary.ID)},
+		}
+
+		var response *fcu.DescribeVolumesOutput
+		var err error
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			response, err = conn.VM.DescribeVolumes(request)
+			if err != nil {
+				if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		})
+
+		if err == nil {
+			if response.Volumes != nil && len(response.Volumes) > 0 {
+				*v = *response.Volumes[0]
+				return nil
+			}
+		}
+		return fmt.Errorf("Error finding EC2 volume %s", rs.Primary.ID)
+	}
+}
+
+const testAccOutscaleOAPIVolumeConfig = `
+resource "outscale_volume" "test" {
+  availability_zone = "eu-west-2a"
+  volume_type = "gp2"
+  size = 1
+  tags {
+    Name = "tf-acc-test-ebs-volume-test"
+  }
+}
+`
+
+const testOutscaleOAPIVolumeConfigUpdateSize = `
+resource "outscale_volume" "test" {
+  availability_zone = "eu-west-2a"
+  volume_type = "gp2"
+  size = 10
+  tags {
+    Name = "tf-acc-test-ebs-volume-test"
+  }
+}
+`
