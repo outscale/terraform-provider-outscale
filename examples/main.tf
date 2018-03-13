921401ed59f47ea89d96ecf01d80025dd2f6ee47
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-13 10:55:40 -0600

    terraform test for SG

--- examples/main.tf
+++ examples/main.tf
@@ -1,33 +1,48 @@
-resource "outscale_inbound_rule" "outscale_inbound_rule1" {
-    ip_permissions = {
-        from_port = 22
-        to_port = 22
-        ip_protocol = "tcp"
-        ip_ranges = ["46.231.147.8/32"]
-    }
+resource "outscale_outbound_rule" "outscale_outbound_rule1" {
+  ip_permissions = {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    ip_ranges   = ["46.231.147.8/32"]
+  }
 
-    group_id = "${outscale_firewall_rules_set.outscale_firewall_rules_set.id}"
+  ip_permissions = {
+    from_port   = 443
+    to_port     = 443
+    ip_protocol = "tcp"
+    ip_ranges   = ["46.231.147.8/32"]
+  }
+
+  group_id = "${outscale_firewall_rules_set.outscale_firewall_rules_set.id}"
 }
 
 resource "outscale_inbound_rule" "outscale_inbound_rule2" {
-     ip_permissions = {
-        from_port = 443
-        to_port = 443
-        ip_protocol = "tcp"
-        ip_ranges = ["46.231.147.8/32"]
-    }
+  ip_permissions = {
+    from_port   = 443
+    to_port     = 443
+    ip_protocol = "tcp"
+    ip_ranges   = ["46.231.147.8/32"]
+  }
 
-    group_id = "${outscale_firewall_rules_set.outscale_firewall_rules_set.id}"
+  ip_permissions = {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    ip_ranges   = ["46.231.147.8/32"]
+  }
+
+  group_id = "${outscale_firewall_rules_set.outscale_firewall_rules_set.id}"
 }
 
 resource "outscale_firewall_rules_set" "outscale_firewall_rules_set" {
-    group_description = "test group tf"
-    group_name = "sg1-test-group_test1"
+  group_description = "test group tf"
+  group_name        = "sg1-test-group_test1"
+  vpc_id            = "vpc-e9d09d63"
 }
 
 data "outscale_firewall_rules_set" "by_filter" {
-		filter {
-			name = "group-name"
-			values = ["${outscale_firewall_rules_set.outscale_firewall_rules_set.group_name}"]
-		}
-	}
+  filter {
+    name   = "group-name"
+    values = ["${outscale_firewall_rules_set.outscale_firewall_rules_set.group_name}"]
+  }
+}
