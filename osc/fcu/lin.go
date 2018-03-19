42eb1e8d3f0f2350ab9ea3cd1ba95d37daf57216
Antonio Cabrera <antonio.cabreraglz@gmail.com>
2018-03-19 16:35:47 -0600

    Add functions to attach and detach internet gateways

--- osc/fcu/lin.go
+++ osc/fcu/lin.go
@@ -142,3 +142,49 @@ func (v VMOperations) DeleteVpc(input *DeleteVpcInput) (*DeleteVpcOutput, error)
 
 	return output, nil
 }
+
+func (v VMOperations) AttachInternetGateway(input *AttachInternetGatewayInput) (*AttachInternetGatewayOutput, error) {
+	inURL := "/"
+	endpoint := "AttachInternetGateway"
+	output := &AttachInternetGatewayOutput{}
+
+	if input == nil {
+		input = &AttachInternetGatewayInput{}
+	}
+
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) DetachInternetGateway(input *DetachInternetGatewayInput) (*DetachInternetGatewayOutput, error) {
+	inURL := "/"
+	endpoint := "DetachInternetGateway"
+	output := &DetachInternetGatewayOutput{}
+
+	if input == nil {
+		input = &DetachInternetGatewayInput{}
+	}
+
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
