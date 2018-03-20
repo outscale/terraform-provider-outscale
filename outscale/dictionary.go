58199973fee2edb10d319ba7b9df9c85e323e28d
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-19 23:15:29 -0600

    adding lin attr oapi resource to the dictionary

--- outscale/dictionary.go
+++ outscale/dictionary.go
@@ -40,7 +40,8 @@ func init() {
 			"outscale_public_ip": resourceOutscaleOAPIPublicIP,
 			// "outscale_inbound_rule":  resourceOutscaleOAPIInboundRule,
 			// "outscale_outbound_rule": resourceOutscaleOAPIOutboundRule,
-			"outscale_tag": resourceOutscaleOAPITags,
+			"outscale_tag":            resourceOutscaleOAPITags,
+			"outscale_lin_attributes": resourceOutscaleOAPILinAttributes,
 		},
 	}
 	datasources = Dictionary{
