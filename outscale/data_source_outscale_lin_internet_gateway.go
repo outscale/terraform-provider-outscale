485388168bc261e961104254cbcac15a43aeffe7
Juan Preza <juan.preza.jaime@gmail.com>
2018-03-20 15:18:06 -0600

    Add Outscale Lin Internet Gateway

---
+++ outscale/data_source_outscale_lin_internet_gateway.go
@@ -0,0 +1,112 @@
+package outscale
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/fcu"
+)
+
+func datasourceOutscaleLinInternetGateway() *schema.Resource {
+	return &schema.Resource{
+		Read: datasourceOutscaleLinInternetGatewayRead,
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"attachement_set": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"state": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vpc_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"internet_gateway_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tag_set": dataSourceTagsSchema(),
+			"request_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func datasourceOutscaleLinInternetGatewayRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*OutscaleClient).FCU
+
+	filters, filtersOk := d.GetOk("filter")
+	internetID, insternetIDOk := d.GetOk("internet_gateway_id")
+
+	if filtersOk == false && insternetIDOk == false {
+		return fmt.Errorf("One of filters, or instance_id must be assigned")
+	}
+
+	// Build up search parameters
+	params := &fcu.DescribeInternetGatewaysInput{}
+	if filtersOk {
+		params.Filters = buildOutscaleDataSourceFilters(filters.(*schema.Set))
+	}
+	if insternetIDOk {
+		params.InternetGatewayIds = []*string{aws.String(internetID.(string))}
+	}
+
+	var resp *fcu.DescribeInternetGatewaysOutput
+	var err error
+	err = resource.Retry(120*time.Second, func() *resource.RetryError {
+		resp, err = conn.VM.DescribeInternetGateways(params)
+
+		if err != nil {
+			if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return resource.RetryableError(err)
+	})
+	if err != nil {
+		log.Printf("[DEBUG] Error reading LIN Internet Gateway id (%s)", err)
+	}
+
+	log.Printf("[DEBUG] Setting LIN Internet Gateway id (%s)", err)
+
+	d.Set("request_id", resp.RequesterId)
+	d.Set("internet_gateway_id", resp.InternetGateways[0].InternetGatewayId)
+	d.Set("tag_set", tagsToMap(resp.InternetGateways[0].Tags))
+
+	err = d.Set("attachement_set", flattenInternetGwAttachements(resp.InternetGateways[0].Attachments))
+	if err != nil {
+		return err
+	}
+
+	if err := d.Set("tag_set", dataSourceTags(resp.InternetGateways[0].Tags)); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func flattenInternetGwAttachements(attachements []*fcu.InternetGatewayAttachment) []map[string]interface{} {
+	res := make([]map[string]interface{}, len(attachements))
+
+	for i, a := range attachements {
+		res[i]["state"] = a.State
+		res[i]["vpc_id"] = a.VpcId
+	}
+
+	return res
+}
---
+++ outscale/data_source_outscale_lin_internet_gateway_test.go
@@ -0,0 +1,144 @@
+package outscale
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/fcu"
+)
+
+func TestAccOutscaleLinInternetGatewayDatasource_basic(t *testing.T) {
+	o := os.Getenv("OUTSCALE_OAPI")
+
+	oapi, err := strconv.ParseBool(o)
+	if err != nil {
+		oapi = false
+	}
+
+	if oapi != false {
+		t.Skip()
+	}
+	var conf fcu.InternetGateway
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOutscaleLinInternetGatewayDatasourceDestroyed,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccOutscaleLinInternetGatewayDatasourceConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleLinInternetGatewayDatasourceExists("outscale_lin_internet_gateway.gateway", &conf),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOutscaleLinInternetGatewayDatasourceExists(n string, res *fcu.InternetGateway) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No internet gateway id is set")
+		}
+		var resp *fcu.DescribeInternetGatewaysOutput
+		conn := testAccProvider.Meta().(*OutscaleClient)
+
+		err := resource.Retry(5*time.Minute, func() *resource.RetryError {
+			var err error
+			resp, err = conn.FCU.VM.DescribeInternetGateways(&fcu.DescribeInternetGatewaysInput{
+				InternetGatewayIds: []*string{aws.String(rs.Primary.ID)},
+			})
+			if err != nil {
+				if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		})
+		if err != nil {
+			return err
+		}
+		if len(resp.InternetGateways) != 1 ||
+			*resp.InternetGateways[0].InternetGatewayId != rs.Primary.ID {
+			return fmt.Errorf("Internet Gateway not found")
+		}
+
+		*res = *resp.InternetGateways[0]
+
+		return nil
+	}
+}
+
+func testAccCheckOutscaleLinInternetGatewayDatasourceDestroyed(s *terraform.State) error {
+	conn := testAccProvider.Meta().(*OutscaleClient)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "outscale_lin_internet_gateway" {
+			continue
+		}
+
+		// Try to find an internet gateway
+		var resp *fcu.DescribeInternetGatewaysOutput
+		err := resource.Retry(5*time.Minute, func() *resource.RetryError {
+			var err error
+			resp, err = conn.FCU.VM.DescribeInternetGateways(&fcu.DescribeInternetGatewaysInput{
+				InternetGatewayIds: []*string{aws.String(rs.Primary.ID)},
+			})
+
+			if err != nil {
+				if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+
+			return resource.RetryableError(err)
+		})
+
+		if resp == nil {
+			return nil
+		}
+
+		if err == nil {
+			if len(resp.InternetGateways) > 0 {
+				return fmt.Errorf("still exist")
+			}
+			return nil
+		}
+
+		// Verify the error is what we want
+		ec2err, ok := err.(awserr.Error)
+		if !ok {
+			return err
+		}
+		if ec2err.Code() != "InvalidInternetGateway.NotFound" {
+			return err
+		}
+	}
+
+	return nil
+}
+
+const testAccOutscaleLinInternetGatewayDatasourceConfig = `
+resource "outscale_lin_internet_gateway" "gateway" {}
+
+data "outscale_lin_internet_gateway" "test" {
+    filter {
+		name = "internet-gateway-id"
+		values = ["${outscale_lin_internet_gateway.gateway.id}"]
+    }
+`
