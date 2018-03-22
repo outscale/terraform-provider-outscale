1405cd94d43bb49662f24d28f6f8f4a0e3b65cda
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-21 21:21:03 -0600

    fixing typos for subnet

--- outscale/resource_outscale_subnet.go
+++ outscale/resource_outscale_subnet.go
@@ -31,33 +31,53 @@ func resourceOutscaleSubNet() *schema.Resource {
 //Create SubNet
 func resourceOutscaleSubNetCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).FCU
-	req := &fcu.CreateSubnetInput{
-		CidrBlock: aws.String(d.Get("cidr_block").(string)),
-		VpcId:     aws.String(d.Get("vpc_id").(string)),
+
+	createOpts := &fcu.CreateSubnetInput{
+		AvailabilityZone: aws.String(d.Get("availability_zone").(string)),
+		CidrBlock:        aws.String(d.Get("cidr_block").(string)),
+		VpcId:            aws.String(d.Get("vpc_id").(string)),
 	}
-	if a, aok := d.GetOk("availability_zone"); aok {
-		req.AvailabilityZone = aws.String(a.(string))
-	}
-	if a, aok := d.GetOk("dry_run"); aok {
-		req.DryRun = aws.Bool(a.(bool))
-	}
+
 	var res *fcu.CreateSubnetOutput
 	var err error
-	err = resource.Retry(40*time.Minute, func() *resource.RetryError {
-		res, err = conn.VM.CreateSubNet(req)
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		res, err = conn.VM.CreateSubNet(createOpts)
 
 		if err != nil {
 			if strings.Contains(err.Error(), "RequestLimitExceeded") {
-				fmt.Printf("[INFO] Request limit exceeded")
 				return resource.RetryableError(err)
 			}
 			return resource.NonRetryableError(err)
 		}
 
-		return resource.RetryableError(err)
+		return nil
 	})
 
-	d.SetId(*res.Subnet.SubnetId)
+	if err != nil {
+		return fmt.Errorf("Error creating subnet: %s", err)
+	}
+
+	// Get the ID and store it
+	subnet := res.Subnet
+	d.SetId(*subnet.SubnetId)
+	log.Printf("[INFO] Subnet ID: %s", *subnet.SubnetId)
+
+	// Wait for the Subnet to become available
+	log.Printf("[DEBUG] Waiting for subnet (%s) to become available", *subnet.SubnetId)
+	stateConf := &resource.StateChangeConf{
+		Pending: []string{"pending"},
+		Target:  []string{"available"},
+		Refresh: SubnetStateRefreshFunc(conn, *subnet.SubnetId),
+		Timeout: 10 * time.Minute,
+	}
+
+	_, err = stateConf.WaitForState()
+
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for subnet (%s) to become ready: %s",
+			d.Id(), err)
+	}
 
 	return resourceOutscaleSubNetRead(d, meta)
 }
@@ -67,18 +87,12 @@ func resourceOutscaleSubNetCreate(d *schema.ResourceData, meta interface{}) erro
 func resourceOutscaleSubNetRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).FCU
 
-	id := d.Id()
-
-	log.Printf("[DEBUG] Reading Subnet(%s)", id)
-
-	req := &fcu.DescribeSubnetsInput{
-		SubnetIds: []*string{aws.String(id)},
-	}
-
 	var resp *fcu.DescribeSubnetsOutput
 	var err error
 	err = resource.Retry(120*time.Second, func() *resource.RetryError {
-		resp, err = conn.VM.DescribeSubNet(req)
+		resp, err = conn.VM.DescribeSubNet(&fcu.DescribeSubnetsInput{
+			SubnetIds: []*string{aws.String(d.Id())},
+		})
 
 		if err != nil {
 			if strings.Contains(err.Error(), "RequestLimitExceeded:") {
@@ -86,20 +100,29 @@ func resourceOutscaleSubNetRead(d *schema.ResourceData, meta interface{}) error 
 			}
 			return resource.NonRetryableError(err)
 		}
-		return resource.RetryableError(err)
+		return nil
 	})
+
 	if err != nil {
-		log.Printf("[DEBUG] Error reading Subnet (%s)", err)
+		if strings.Contains(err.Error(), "InvalidSubnetID.NotFound") {
+			// Update state to indicate the subnet no longer exists.
+			d.SetId("")
+			return nil
+		}
+		return err
+	}
+	if resp == nil {
+		return nil
 	}
 
-	log.Printf("[DEBUG] Setting Subnet (%s)", err)
+	subnet := resp.Subnets[0]
 
-	d.Set("subnet_id", resp.Subnets[0].SubnetId)
-	d.Set("availability_zone", resp.Subnets[0].AvailabilityZone)
-	d.Set("cidr_block", resp.Subnets[0].CidrBlock)
-	d.Set("vpc_id", resp.Subnets[0].VpcId)
+	d.Set("subnet_id", subnet.SubnetId)
+	d.Set("availability_zone", subnet.AvailabilityZone)
+	d.Set("cidr_block", subnet.CidrBlock)
+	d.Set("vpc_id", subnet.VpcId)
 
-	if err := d.Set("tag_set", dataSourceTags(resp.Subnets[0].Tags)); err != nil {
+	if err := d.Set("tag_set", tagsToMap(subnet.Tags)); err != nil {
 		return err
 	}
 
@@ -126,7 +149,7 @@ func resourceOutscaleSubNetDelete(d *schema.ResourceData, meta interface{}) erro
 			}
 			return resource.NonRetryableError(err)
 		}
-		return resource.RetryableError(err)
+		return nil
 	})
 	if err != nil {
 		log.Printf("[DEBUG] Error deleting Subnet(%s)", err)
@@ -136,40 +159,43 @@ func resourceOutscaleSubNetDelete(d *schema.ResourceData, meta interface{}) erro
 	return nil
 }
 
-func readOutscaleSubNet(d *schema.ResourceData, subnet *fcu.Subnet) error {
-	if err := d.Set("availability_zone", subnet.AvailabilityZone); err != nil {
-		fmt.Printf("[WARN] ERROR readOutscaleSubNet1 (%s)", err)
+func SubnetStateRefreshFunc(conn *fcu.Client, id string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
 
-		return err
+		var resp *fcu.DescribeSubnetsOutput
+		var err error
+		err = resource.Retry(120*time.Second, func() *resource.RetryError {
+			resp, err = conn.VM.DescribeSubNet(&fcu.DescribeSubnetsInput{
+				SubnetIds: []*string{aws.String(id)},
+			})
+
+			if err != nil {
+				if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+
+		if err != nil {
+			if strings.Contains(err.Error(), "InvalidSubnetID.NotFound") {
+				resp = nil
+			} else {
+				log.Printf("Error on SubnetStateRefresh: %s", err)
+				return nil, "", err
+			}
+		}
+
+		if resp == nil {
+			// Sometimes AWS just has consistency issues and doesn't see
+			// our instance yet. Return an empty state.
+			return nil, "", nil
+		}
+
+		subnet := resp.Subnets[0]
+		return subnet, *subnet.State, nil
 	}
-	if err := d.Set("available_ip_address_count", subnet.AvailableIpAddressCount); err != nil {
-		fmt.Printf("[WARN] ERROR readOutscaleSubNet2 (%s)", err)
-
-		return err
-	}
-	if err := d.Set("cidr_block", subnet.CidrBlock); err != nil {
-		fmt.Printf("[WARN] ERROR readOutscaleSubNet3 (%s)", err)
-
-		return err
-	}
-	if err := d.Set("state", subnet.State); err != nil {
-		fmt.Printf("[WARN] ERROR readOutscaleSubNet4 (%s)", err)
-
-		return err
-	}
-	if err := d.Set("subnet_id", subnet.SubnetId); err != nil {
-		fmt.Printf("[WARN] ERROR readOutscaleSubNet5 (%s)", err)
-
-		return err
-	}
-
-	if err := d.Set("vpc_id", subnet.VpcId); err != nil {
-		fmt.Printf("[WARN] ERROR readOutscaleSubNet6 (%s)", err)
-
-		return err
-	}
-
-	return nil
 }
 
 func getSubNetSchema() map[string]*schema.Schema {
