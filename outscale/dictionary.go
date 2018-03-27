f178ef0a80a7bc5ff0a577491d4095bc076bc21e
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-27 12:56:02 -0600

    adding client_endpoint to the provider

--- outscale/dictionary.go
+++ outscale/dictionary.go
@@ -35,6 +35,7 @@ func init() {
 			"outscale_vm_attributes":             resourceOutscaleVMAttributes,
 			"outscale_nat_service":               resourceOutscaleNatService,
 			"outscale_subnet":                    resourceOutscaleSubNet,
+			"outscale_client_endpoint":           resourceOutscaleCustomerGateway,
 		},
 		"oapi": ResourceMap{
 			"outscale_vm":                   resourceOutscaleOApiVM,
