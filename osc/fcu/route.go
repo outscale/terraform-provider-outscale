19c8f4f929856131f7e28ca826a268dc83234c3e
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-28 23:11:22 -0600

    adding route endpoints for the resources

--- osc/fcu/route.go
+++ osc/fcu/route.go
@@ -92,3 +92,157 @@ func (v VMOperations) DescribeRouteTables(input *DescribeRouteTablesInput) (*Des
 
 	return output, nil
 }
+
+func (v VMOperations) CreateRouteTable(input *CreateRouteTableInput) (*CreateRouteTableOutput, error) {
+	inURL := "/"
+	endpoint := "CreateRouteTable"
+	output := &CreateRouteTableOutput{}
+
+	if input == nil {
+		input = &CreateRouteTableInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) DisableVgwRoutePropagation(input *DisableVgwRoutePropagationInput) (*DisableVgwRoutePropagationOutput, error) {
+	inURL := "/"
+	endpoint := "DisableVgwRoutePropagation"
+	output := &DisableVgwRoutePropagationOutput{}
+
+	if input == nil {
+		input = &DisableVgwRoutePropagationInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) EnableVgwRoutePropagation(input *EnableVgwRoutePropagationInput) (*EnableVgwRoutePropagationOutput, error) {
+	inURL := "/"
+	endpoint := "EnableVgwRoutePropagation"
+	output := &EnableVgwRoutePropagationOutput{}
+
+	if input == nil {
+		input = &EnableVgwRoutePropagationInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) DisassociateRouteTable(input *DisassociateRouteTableInput) (*DisassociateRouteTableOutput, error) {
+	inURL := "/"
+	endpoint := "DisassociateRouteTable"
+	output := &DisassociateRouteTableOutput{}
+
+	if input == nil {
+		input = &DisassociateRouteTableInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) DeleteRouteTable(input *DeleteRouteTableInput) (*DeleteRouteTableOutput, error) {
+	inURL := "/"
+	endpoint := "DeleteRouteTable"
+	output := &DeleteRouteTableOutput{}
+
+	if input == nil {
+		input = &DeleteRouteTableInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) AssociateRouteTable(input *AssociateRouteTableInput) (*AssociateRouteTableOutput, error) {
+	inURL := "/"
+	endpoint := "AssociateRouteTable"
+	output := &AssociateRouteTableOutput{}
+
+	if input == nil {
+		input = &AssociateRouteTableInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
+
+func (v VMOperations) ReplaceRouteTableAssociation(input *ReplaceRouteTableAssociationInput) (*ReplaceRouteTableAssociationOutput, error) {
+	inURL := "/"
+	endpoint := "ReplaceRouteTableAssociation"
+	output := &ReplaceRouteTableAssociationOutput{}
+
+	if input == nil {
+		input = &ReplaceRouteTableAssociationInput{}
+	}
+	req, err := v.client.NewRequest(context.TODO(), endpoint, http.MethodGet, inURL, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = v.client.Do(context.TODO(), req, output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
