005554df686b71bccaa60bd4974a1ce2be6ee865
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-03-30 09:38:33 -0600

    adding support for icu tags

--- outscale/resource_outscale_tags.go
+++ outscale/resource_outscale_tags.go
@@ -327,6 +327,19 @@ func tagsFromMap(m map[string]interface{}) []*fcu.Tag {
 	return result
 }
 
+func tagsFromMapICU(m map[string]interface{}) []*icu.Tag {
+	result := make([]*icu.Tag, 0, len(m))
+	for k, v := range m {
+		t := &icu.Tag{
+			Key:   aws.String(k),
+			Value: aws.String(v.(string)),
+		}
+		result = append(result, t)
+	}
+
+	return result
+}
+
 // tagsToMap turns the list of tag into a map.
 func tagsToMap(ts []*fcu.Tag) []map[string]string {
 	result := make([]map[string]string, len(ts))
