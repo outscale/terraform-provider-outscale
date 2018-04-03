d1f4b93c6aee60b8226fc18697f11150af5e5aae
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-04-03 11:39:22 -0500

    adding outscale DS lin and oapi version to the provider

--- outscale/dictionary.go
+++ outscale/dictionary.go
@@ -81,6 +81,7 @@ func init() {
 			"outscale_lin_internet_gateways": datasourceOutscaleLinInternetGateways,
 			"outscale_subnet":                dataSourceOutscaleSubnet,
 			"outscale_subnets":               dataSourceOutscaleSubnets,
+			"outscale_lin":                   dataSourceOutscaleVpc,
 		},
 		"oapi": ResourceMap{
 			"outscale_vm":                    dataSourceOutscaleOAPIVM,
@@ -100,6 +101,7 @@ func init() {
 			"outscale_subnets":               dataSourceOutscaleOAPISubnets,
 			"outscale_vm_state":              dataSourceOutscaleOAPIVMState,
 			"outscale_vms_state":             dataSourceOutscaleOAPIVMSState,
+			"outscale_lin":                   dataSourceOutscaleOAPIVpc,
 		},
 	}
 }
