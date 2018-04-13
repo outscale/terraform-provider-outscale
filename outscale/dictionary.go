fdaa2b5f8f2734533e8df0ff9fc6e1b165b98ca1
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-04-12 20:15:05 -0500

    adding resource to the provider

--- outscale/dictionary.go
+++ outscale/dictionary.go
@@ -41,6 +41,7 @@ func init() {
 			"outscale_route_table":               resourceOutscaleRouteTable,
 			"outscale_route_table_link":          resourceOutscaleRouteTableAssociation,
 			"outscale_dhcp_option_link":          resourceOutscaleDHCPOptionLink,
+			"outscale_vpn_gateway":               resourceOutscaleVpnGateway,
 		},
 		"oapi": ResourceMap{
 			"outscale_vm":                   resourceOutscaleOApiVM,
--- outscale/provider.go
+++ outscale/provider.go
@@ -77,6 +77,7 @@ func Provider() terraform.ResourceProvider {
 			"outscale_route_table":               GetResource(fcu, "outscale_route_table")(),
 			"outscale_route_table_link":          GetResource(fcu, "outscale_route_table_link")(),
 			"outscale_dhcp_option_link":          GetResource(fcu, "outscale_dhcp_option_link")(),
+			"outscale_vpn_gateway":               GetResource(fcu, "outscale_vpn_gateway")(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"outscale_vm":                    GetDatasource(fcu, "outscale_vm")(),
