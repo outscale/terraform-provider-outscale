6fce13d54114236b6fe000d1d701b8489882cf5a
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-04-17 22:51:51 -0500

    adding test for tf

--- examples/main.tf
+++ examples/main.tf
@@ -1,30 +1,15 @@
-resource "outscale_lin" "outscale_lin" {
+resource "outscale_lin" "outscale_lin1" {
   cidr_block = "10.0.0.0/16"
 }
 
 resource "outscale_route_table" "outscale_route_table" {
-  vpc_id = "${outscale_lin.outscale_lin.vpc_id}"
+  vpc_id = "${outscale_lin.outscale_lin1.vpc_id}"
 }
 
-resource "outscale_subnet" "outscale_subnet" {
-  vpc_id     = "${outscale_lin.outscale_lin.vpc_id}"
-  cidr_block = "10.0.0.0/18"
+resource "outscale_route_table" "outscale_route_table2" {
+  vpc_id = "${outscale_lin.outscale_lin1.vpc_id}"
 }
 
-resource "outscale_route_table_link" "outscale_route_table_link" {
-  route_table_id = "${outscale_route_table.outscale_route_table.route_table_id}"
-  subnet_id      = "${outscale_subnet.outscale_subnet.subnet_id}"
-}
-
-resource "outscale_client_endpoint" "outscale_client_endpoint" {
-  bgp_asn    = "3"
-  ip_address = "171.33.74.122"
-  type       = "ipsec.1"
-}
-
-resource "outscale_dhcp_option" "outscale_dhcp_option" {}
-
-resource "outscale_dhcp_option_link" "outscale_dhcp_option_link" {
-  dhcp_options_id = "${outscale_dhcp_option.outscale_dhcp_option.dhcp_options_id}"
-  vpc_id          = "${outscale_lin.outscale_lin.vpc_id}"
+data "outscale_route_tables" "outscale_route_tables" {
+  route_table_id = ["${outscale_route_table.outscale_route_table.route_table_id}", "${outscale_route_table.outscale_route_table2.route_table_id}"]
 }
