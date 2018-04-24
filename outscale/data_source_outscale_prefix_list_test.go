ca7e10eba0dd64cce564b086b5d24665c78075f5
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-04-24 11:26:27 -0500

    adding prefix list functionality and test

---
+++ outscale/data_source_outscale_prefix_list_test.go
@@ -0,0 +1,63 @@
+package outscale
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccDataSourceOutscalePrefixList(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccDataSourceOutscalePrefixListConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccDataSourceOutscalePrefixListCheck("data.outscale_prefix_list.s3_by_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceOutscalePrefixListCheck(name string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[name]
+		if !ok {
+			return fmt.Errorf("root module has no resource called %s", name)
+		}
+
+		attr := rs.Primary.Attributes
+
+		if attr["prefix_list_name"] != "com.amazonaws.us-west-2.s3" {
+			return fmt.Errorf("bad name %s", attr["prefix_list_name"])
+		}
+		if attr["prefix_list_id"] != "pl-68a54001" {
+			return fmt.Errorf("bad id %s", attr["prefix_list_id"])
+		}
+
+		var (
+			cidrBlockSize int
+			err           error
+		)
+
+		if cidrBlockSize, err = strconv.Atoi(attr["cidr_blocks.#"]); err != nil {
+			return err
+		}
+		if cidrBlockSize < 1 {
+			return fmt.Errorf("cidr_blocks seem suspiciously low: %d", cidrBlockSize)
+		}
+
+		return nil
+	}
+}
+
+const testAccDataSourceOutscalePrefixListConfig = `
+data "outscale_prefix_list" "s3_by_id" {
+  prefix_list_id = "pl-68a54001"
+}
+`
