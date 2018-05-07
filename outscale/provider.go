424621f72aa2f1b64d9692392c0c4aa54b214bc7
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-05-07 12:59:36 -0500

    adding image launch permission resource and oapi functionality and test as well to the provider

--- outscale/provider.go
+++ outscale/provider.go
@@ -90,6 +90,7 @@ func Provider() terraform.ResourceProvider {
 			"outscale_snapshot":                      GetResource(fcu, "outscale_snapshot")(),
 			"outscale_image_register":                GetResource(fcu, "outscale_image_register")(),
 			"outscale_keypair_importation":           GetResource(fcu, "outscale_keypair_importation")(),
+			"outscale_image_launch_permission":       GetResource(fcu, "outscale_image_launch_permission")(),
 		},
 		DataSourcesMap: map[string]*schema.Resource{
 			"outscale_vm":                    GetDatasource(fcu, "outscale_vm")(),
