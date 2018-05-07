4840a3a75b0eca478aef1665ed2f4e8117e10f1f
Cristian Ramirez <cristianramirez@Cramirez.local>
2018-05-07 16:59:50 -0500

    adding lin peerings DS normal and oapi version functionality and test

---
+++ outscale/data_source_outscale_oapi_lin_peerings_test.go
@@ -0,0 +1,54 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceOutscaleOAPILinPeeringsConnection_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccDataSourceOutscaleOAPILinPeeringsConnectionConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.outscale_lin_peerings.test_by_id", "lin_peering.#", "1"),
+				),
+				// ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+const testAccDataSourceOutscaleOAPILinPeeringsConnectionConfig = `
+resource "outscale_lin" "foo" {
+  ip_range = "10.1.0.0/16"
+
+  tag {
+	  Name = "terraform-testacc-vpc-peering-connection-data-source-foo"
+  }
+}
+
+resource "outscale_lin" "bar" {
+  ip_range = "10.2.0.0/16"
+
+  tag {
+	  Name = "terraform-testacc-vpc-peering-connection-data-source-bar"
+  }
+}
+
+resource "outscale_lin_peering" "test" {
+	lin_id = "${outscale_lin.foo.id}"
+	peer_lin_id = "${outscale_lin.bar.id}"
+
+    tag {
+      Name = "terraform-testacc-vpc-peering-connection-data-source-foo-to-bar"
+    }
+}
+
+data "outscale_lin_peerings" "test_by_id" {
+	lin_peering_id = "[${outscale_lin_peering.test.id}]"
+}
+`
