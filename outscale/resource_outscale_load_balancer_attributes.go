38254af6af55c4c385dce6e07b980fba775a426a
Cristian Ramirez <cristiano.rosetti@gmail.com>
2018-05-28 12:13:47 -0500

    taking out the arguments to a simpler level so terraform does not crash reading computed and arguments values

--- outscale/resource_outscale_load_balancer_attributes.go
+++ outscale/resource_outscale_load_balancer_attributes.go
@@ -24,9 +24,25 @@ func resourceOutscaleLoadBalancerAttributes() *schema.Resource {
 		},
 
 		Schema: map[string]*schema.Schema{
+			"emit_interval": &schema.Schema{
+				Type:     schema.TypeInt,
+				Optional: true,
+			},
+			"enabled": &schema.Schema{
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+			"s3_bucket_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"s3_bucket_prefix": &schema.Schema{
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 			"load_balancer_attributes": &schema.Schema{
 				Type:     schema.TypeList,
-				Required: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"access_log": &schema.Schema{
@@ -36,22 +52,18 @@ func resourceOutscaleLoadBalancerAttributes() *schema.Resource {
 								Schema: map[string]*schema.Schema{
 									"emit_interval": &schema.Schema{
 										Type:     schema.TypeInt,
-										Optional: true,
 										Computed: true,
 									},
 									"enabled": &schema.Schema{
 										Type:     schema.TypeBool,
-										Required: true,
+										Computed: true,
 									},
-
 									"s3_bucket_name": &schema.Schema{
 										Type:     schema.TypeString,
-										Optional: true,
 										Computed: true,
 									},
 									"s3_bucket_prefix": &schema.Schema{
 										Type:     schema.TypeString,
-										Optional: true,
 										Computed: true,
 									},
 								},
@@ -76,45 +88,27 @@ func resourceOutscaleLoadBalancerAttributes() *schema.Resource {
 func resourceOutscaleLoadBalancerAttributesCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).LBU
 
-	v, ok := d.GetOk("load_balancer_attributes")
+	v, ok := d.GetOk("enabled")
 	v1, ok1 := d.GetOk("load_balancer_name")
 
-	if !ok && ok1 {
-		return fmt.Errorf("please provide the required attributes")
+	if !ok && !ok1 {
+		return fmt.Errorf("please provide the enabled and load_balancer_name required attributes")
 	}
 
 	elbOpts := &lbu.ModifyLoadBalancerAttributesInput{
 		LoadBalancerName: aws.String(v1.(string)),
 	}
-
-	lb := v.([]interface{})[0].(map[string]interface{})
-
-	a := lb["access_log"].(map[string]interface{})
-
-	if v, ok := a["enabled"]; ok && v == "" {
-		return fmt.Errorf("please provide the enable attribute")
-	}
-
-	b, err := strconv.ParseBool(a["enabled"].(string))
-	if err != nil {
-		return err
-	}
-
 	access := &lbu.AccessLog{
-		Enabled: aws.Bool(b),
+		Enabled: aws.Bool(v.(bool)),
 	}
 
-	if v, ok := a["emit_interval"]; ok && v != "" {
-		i, err := strconv.Atoi(v.(string))
-		if err != nil {
-			return err
-		}
-		access.EmitInterval = aws.Int64(int64(i))
+	if v, ok := d.GetOk("emit_interval"); ok {
+		access.EmitInterval = aws.Int64(int64(v.(int)))
 	}
-	if v, ok := a["s3_bucket_name"]; ok && v != "" {
+	if v, ok := d.GetOk("s3_bucket_name"); ok {
 		access.S3BucketName = aws.String(v.(string))
 	}
-	if v, ok := a["s3_bucket_prefix"]; ok && v != "" {
+	if v, ok := d.GetOk("s3_bucket_prefix"); ok {
 		access.S3BucketPrefix = aws.String(v.(string))
 	}
 
@@ -122,6 +116,7 @@ func resourceOutscaleLoadBalancerAttributesCreate(d *schema.ResourceData, meta i
 		AccessLog: access,
 	}
 
+	var err error
 	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
 		_, err = conn.API.ModifyLoadBalancerAttributes(elbOpts)
 
@@ -141,21 +136,6 @@ func resourceOutscaleLoadBalancerAttributesCreate(d *schema.ResourceData, meta i
 
 	d.SetId(*elbOpts.LoadBalancerName)
 	log.Printf("[INFO] LBU Attr ID: %s", d.Id())
-
-	acc := make(map[string]string)
-	ac := make(map[string]interface{})
-	acc["emit_interval"] = strconv.Itoa(int(aws.Int64Value(access.EmitInterval)))
-	acc["enabled"] = strconv.FormatBool(aws.BoolValue(access.Enabled))
-	acc["s3_bucket_name"] = aws.StringValue(access.S3BucketName)
-	acc["s3_bucket_prefix"] = aws.StringValue(access.S3BucketPrefix)
-	ac["access_log"] = acc
-
-	l := make([]map[string]interface{}, 1)
-	l[0] = ac
-
-	if err := d.Set("load_balancer_attributes", l); err != nil {
-		return err
-	}
 
 	return resourceOutscaleLoadBalancerAttributesRead(d, meta)
 }
@@ -217,63 +197,62 @@ func resourceOutscaleLoadBalancerAttributesRead(d *schema.ResourceData, meta int
 func resourceOutscaleLoadBalancerAttributesUpdate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).LBU
 
-	if d.HasChange("load_balancer_attributes") {
-		o, n := d.GetChange("load_balancer_attributes")
-		_, n1 := d.GetChange("load_balancer_name")
+	elbOpts := &lbu.ModifyLoadBalancerAttributesInput{}
+	access := &lbu.AccessLog{}
+	if d.HasChange("load_balancer_name") {
+		_, n := d.GetChange("load_balancer_name")
 
-		fmt.Printf("OLD => %+v", o.([]interface{})[0].(map[string]interface{}))
-		fmt.Printf("NEW => %+v", n.([]interface{})[0].(map[string]interface{}))
+		elbOpts.LoadBalancerName = aws.String(n.(string))
+	}
+	if d.HasChange("enabled") {
+		_, n := d.GetChange("enabled")
 
-		elbOpts := &lbu.ModifyLoadBalancerAttributesInput{
-			LoadBalancerName: aws.String(n1.(string)),
-		}
-
-		lb := n.([]interface{})[0].(map[string]interface{})
-
-		a := lb["access_log"].(map[string]interface{})
-		b, err := strconv.ParseBool(a["enabled"].(string))
+		b, err := strconv.ParseBool(n.(string))
 		if err != nil {
 			return err
 		}
 
-		access := &lbu.AccessLog{
-			Enabled: aws.Bool(b),
-		}
+		access.Enabled = aws.Bool(b)
+	}
+	if d.HasChange("emit_interval") {
+		_, n := d.GetChange("emit_interval")
 
-		if v, ok := a["emit_interval"]; ok {
-			i, err := strconv.Atoi(v.(string))
-			if err != nil {
-				return err
-			}
-			access.EmitInterval = aws.Int64(int64(i))
-		}
-		if v, ok := a["s3_bucket_name"]; ok {
-			access.S3BucketName = aws.String(v.(string))
-		}
-		if v, ok := a["s3_bucket_prefix"]; ok {
-			access.S3BucketPrefix = aws.String(v.(string))
-		}
-
-		elbOpts.LoadBalancerAttributes = &lbu.LoadBalancerAttributes{
-			AccessLog: access,
-		}
-
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			_, err = conn.API.ModifyLoadBalancerAttributes(elbOpts)
-
-			if err != nil {
-				if strings.Contains(fmt.Sprint(err), "Throttling") {
-					return resource.RetryableError(
-						fmt.Errorf("[WARN] Error creating LBU Attr Listener with SSL Cert, retrying: %s", err))
-				}
-				return resource.NonRetryableError(err)
-			}
-			return nil
-		})
-
+		i, err := strconv.Atoi(n.(string))
 		if err != nil {
 			return err
 		}
+		access.EmitInterval = aws.Int64(int64(i))
+	}
+	if d.HasChange("s3_bucket_name") {
+		_, n := d.GetChange("s3_bucket_name")
+
+		access.S3BucketName = aws.String(n.(string))
+	}
+	if d.HasChange("s3_bucket_prefix") {
+		_, n := d.GetChange("s3_bucket_prefix")
+		access.S3BucketPrefix = aws.String(n.(string))
+	}
+
+	elbOpts.LoadBalancerAttributes = &lbu.LoadBalancerAttributes{
+		AccessLog: access,
+	}
+
+	var err error
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		_, err = conn.API.ModifyLoadBalancerAttributes(elbOpts)
+
+		if err != nil {
+			if strings.Contains(fmt.Sprint(err), "Throttling") {
+				return resource.RetryableError(
+					fmt.Errorf("[WARN] Error, retrying: %s", err))
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
 	}
 
 	return resourceOutscaleLoadBalancerAttributesRead(d, meta)
