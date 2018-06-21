2050916e51eb661ccf8676d9541d47fbf9c9e031
Marin Salinas <marinssalinas@gmail.com>
2018-06-21 12:37:58 -0500

    add outscale_load_balancer_ssl_certificare resource test

---
+++ outscale/resource_outscale_load_balancer_ssl_certificate_test.go
@@ -0,0 +1,92 @@
+package outscale
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/acctest"
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccOutscaleLBUSSLCertificate_basic(t *testing.T) {
+	o := os.Getenv("OUTSCALE_OAPI")
+
+	oapi, err := strconv.ParseBool(o)
+	if err != nil {
+		oapi = false
+	}
+
+	if oapi {
+		t.Skip()
+	}
+
+	rInt := acctest.RandIntRange(0, 10)
+	unixFile := "test-fixtures/eim-ssl-unix-line-endings.pem"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:      func() { testAccPreCheck(t) },
+		IDRefreshName: "outscale_load_balancer_ssl_certificate.test",
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckOutscaleLBUDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOutscaleSSLCertificateConfig(rInt, unixFile),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"outscale_load_balancer_ssl_certificate.test", "ssl_certificate.load_balancer_port", "80"),
+				),
+			},
+		},
+	})
+}
+
+func testAccOutscaleSSLCertificateConfig(r int, fName string) string {
+	return fmt.Sprintf(`
+resource "outscale_load_balancer" "bar" {
+  availability_zones = ["eu-west-2a"]
+  load_balancer_name = "foobar-terraform-lbu-%d"
+  listeners {
+    instance_port = 8000
+    instance_protocol = "HTTP"
+    load_balancer_port = 80
+    protocol = "HTTP"
+  }
+
+	tag {
+		bar = "baz"
+	}
+
+}
+
+resource "outscale_server_certificate" "test_cert" {
+  server_certificate_name = "terraform-test-cert-%d"
+	certificate_body = "${file("%s")}"
+	private_key =  <<EOF
+-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQDKdH6BU9Q0xBVPfeX5NjCC/B2Pm3WsFGnTtRw4abkD+r4to9wD
+eYUgjH2yPCyonNOA8mNiCQgDTtaLfbA8LjBYoodt7rgaTO7C0ugRtmTNK96DmYxm
+f8Gs5ZS6eC3yeaFv58d1w2mow7tv0+DRk8uXwzVfaaMxoalsCtlLznmZHwIDAQAB
+AoGABZj69nBu6ZaSUERW23EYHkcCOjo+Iqfd1TCouxaROv7vyytApgfyGlhIEWmA
+gpjzcBlDji5Zvl2rqOesu707MOuJavZvluo+JHy/VIuU+yGUrWuO/QVCu6Jn3yns
+vS7g48ConuZ962cTzRPcpPDspONBVOAhVCF33Y8PsnxV0wECQQD5RqeoqxEUupsy
+QhrDui0KkYXLdT0uhrEQ69n9rvAiQoHPsiX0MswfEKnj/g9N3VwGLdgWytT0TvcI
+8fDPRB4/AkEAz+qF3taX77gB69XRPQwCGWqE1fHIFMwX7QeYdEsk3iRZ0EKVcdp6
+vIPCB2Cq4a4eXcaFa/bXen4yeYgyTbeNIQJBAO92dWctdoowPRiJskZmGhC1/Q6X
+gH+qenyj5VSy8hInS6anH5i4F6icDGhtzmvhgx6YeaZjkTFkjiG0sb2aVWcCQQDD
+WL7UwtzX/xPXB/ril5C1Xo5WESgC2ks0ielkgmGuUYsNEDInWbXtvwGjOuDyz0x6
+oRYkfTSxQzabVyqkOGvhAkBtbjUxOD8wgBIjb4T6mAMokQo6PeEAZGUTyPifjJNo
+detWVr2WRvgNgQvcRnNPECwfq1RtMJJpavaI3kgeaSxg
+-----END RSA PRIVATE KEY-----
+EOF
+}
+
+resource "outscale_load_balancer_ssl_certificate" "test" {
+	load_balancer_name = "${outscale_load_balancer.bar.id}"
+	load_balancer_port = "${outscale_load_balancer.bar.listeners.0.load_balancer_port}"
+	ssl_certificate_id = "${outscale_server_certificate.test_cert.id}"
+}
+`, r, r, fName)
+
+}
