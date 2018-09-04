a0d5ad458811cdde07cde388f7fcf5f884a2784d
Marin Salinas <marinssalinas@gmail.com>
2018-09-04 12:28:01 -0500

    add volumes-ids filter test on volumes datasource

--- outscale/data_source_outscale_oapi_volumes_test.go
+++ outscale/data_source_outscale_oapi_volumes_test.go
@@ -36,6 +36,33 @@ func TestAccOutscaleOAPIVolumeDataSource_multipleFilters(t *testing.T) {
 	})
 }
 
+func TestAccOutscaleOAPIVolumeDataSource_multipleVIdsFilters(t *testing.T) {
+	o := os.Getenv("OUTSCALE_OAPI")
+
+	oapi, err := strconv.ParseBool(o)
+	if err != nil {
+		oapi = false
+	}
+
+	if !oapi {
+		t.Skip()
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckOutscaleOAPIVolumesDataSourceConfigWithMultipleVolumeIDsFilter,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleOAPIVolumeDataSourceID("data.outscale_volumes.outscale_volumes"),
+					resource.TestCheckResourceAttr("data.outscale_volumes.outscale_volumes", "volume_set.0.size", "40"),
+				),
+			},
+		},
+	})
+}
+
 const testAccCheckOutscaleOAPIVolumeDataSourceConfigWithMultipleFilters = `
 resource "outscale_volume" "external1" {
     sub_region_name = "eu-west-2a"
@@ -54,5 +81,24 @@ data "outscale_volumes" "ebs_volume" {
 	name = "volume-type"
 	values = ["${outscale_volume.external1.type}"]
     }
+}
+`
+
+const testAccCheckOutscaleOAPIVolumesDataSourceConfigWithMultipleVolumeIDsFilter = `
+resource "outscale_volume" "outscale_volume" {
+	sub_region_name = "dv-west-1a"
+	size = 40
+}
+
+resource "outscale_volume" "outscale_volume2" {
+	sub_region_name = "dv-west-1a"
+	size = 40
+}
+
+data "outscale_volumes" "outscale_volumes" {
+	filter {
+		name = "volume-ids"
+		values = ["${outscale_volume.outscale_volume.volume_id}", "${outscale_volume.outscale_volume2.volume_id}"]
+	}
 }
 `
