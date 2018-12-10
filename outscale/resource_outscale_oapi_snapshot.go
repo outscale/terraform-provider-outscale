2fb6d58087ee05b1fddd2f76d5fb34918f34e71b
Arturo Espinoza <arturo.espinoza@peopleconnect.us>
2018-12-10 15:19:15 -0600

    testing

--- outscale/resource_outscale_oapi_snapshot.go
+++ outscale/resource_outscale_oapi_snapshot.go
@@ -3,7 +3,6 @@ package outscale
 import (
 	"fmt"
 	"log"
-	"strconv"
 	"strings"
 	"time"
 
@@ -169,13 +168,13 @@ func resourceOutscaleOAPISnapshotRead(d *schema.ResourceData, meta interface{}) 
 
 	d.Set("description", snapshot.Description)
 	d.Set("account_id", snapshot.AccountId)
-	d.Set("progress", strconv.FormatInt(snapshot.Progress, 10))
+	d.Set("progress", snapshot.Progress)
 	d.Set("snapshot_id", snapshot.SnapshotId)
 	d.Set("account_alias", snapshot.AccountAlias)
 	d.Set("volume_id", snapshot.VolumeId)
 	d.Set("state", snapshot.State)
 	d.Set("volume_id", snapshot.VolumeId)
-	d.Set("volume_size", strconv.FormatInt(snapshot.VolumeSize, 10))
+	d.Set("volume_size", snapshot.VolumeSize)
 	d.Set("tags", tagsOAPIToMap(snapshot.Tags))
 	d.Set("request_id", res.OK.ResponseContext.RequestId)
 
