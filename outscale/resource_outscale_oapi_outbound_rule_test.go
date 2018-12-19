1422b5b5c5697659583fd08efe4cf7e6a8e44805
Arturo Espinoza <arturo.espinoza@peopleconnect.us>
2018-12-19 13:09:46 -0600

    fix resource security_group_rule test

--- outscale/resource_outscale_oapi_outbound_rule_test.go
+++ outscale/resource_outscale_oapi_outbound_rule_test.go
@@ -49,12 +49,12 @@ resource "outscale_security_group_rule" "outscale_security_group_rule" {
 	to_port_range     = 22
 	ip_protocol       = "tcp"
 	ip_range          = "46.231.147.8/32"
-	security_group_id = "1"
+	security_group_id = "${outscale_security_group.outscale_security_group.security_group_id}"
 }
 
-#resource "outscale_security_group" "outscale_security_group" {
-#    description         = "test group"
-#    security_group_name = "sg1-test-group_test"
-#}
+resource "outscale_security_group" "outscale_security_group" {
+	description         = "test group"
+	security_group_name = "sg1-test-group_test"
+}
 `)
 }
