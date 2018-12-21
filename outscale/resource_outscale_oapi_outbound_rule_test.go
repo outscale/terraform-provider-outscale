dfa983f75559db09ce3872bdd8ccfb58599c6cbe
Arturo Espinoza <arturo.espinoza@peopleconnect.us>
2018-12-21 11:32:08 -0600

    testing security_group rule

--- outscale/resource_outscale_oapi_outbound_rule_test.go
+++ outscale/resource_outscale_oapi_outbound_rule_test.go
@@ -47,7 +47,6 @@ resource "outscale_security_group_rule" "outscale_security_group_rule" {
 	flow              = "Inbound"
 	from_port_range   = 22
 	to_port_range     = 22
-	ip_protocol       = "tcp"
 	ip_range          = "46.231.147.8/32"
 	security_group_id = "${outscale_security_group.outscale_security_group.security_group_id}"
 	rules {
@@ -57,8 +56,6 @@ resource "outscale_security_group_rule" "outscale_security_group_rule" {
 		ip_ranges         = ["46.231.147.8/32"]
 		security_groups_members {
 			security_group_id = "${outscale_security_group.outscale_security_group.security_group_id}"	
-			account_id = "2"
-			security_group_name = "3"
 		}
 	}
 }
