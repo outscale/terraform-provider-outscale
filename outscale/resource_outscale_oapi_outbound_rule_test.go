f5d86b83b9a4bcf6ffee9b83d10df2a64fef975d
Arturo Espinoza <arturo.espinoza@peopleconnect.us>
2019-01-14 16:48:02 -0600

    test outbound rule

--- outscale/resource_outscale_oapi_outbound_rule_test.go
+++ outscale/resource_outscale_oapi_outbound_rule_test.go
@@ -44,7 +44,7 @@ func TestAccOutscaleOAPIOutboundRule(t *testing.T) {
 func testAccOutscaleOAPISecurityGroupRuleEgressConfig(rInt int) string {
 	return fmt.Sprintf(`
 resource "outscale_security_group_rule" "outscale_security_group_rule" {
-	flow              = "Inbound"
+	flow              = "Outbound"
 	security_group_id = "${outscale_security_group.outscale_security_group.security_group_id}"
 	rules {
 		from_port_range   = 22
