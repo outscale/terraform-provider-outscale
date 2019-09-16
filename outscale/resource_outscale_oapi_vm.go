781ee402a62c1ec58fd165d8d1283c401d2be2e1
PacoDw <__pm@outlook.com>
2019-09-16 09:55:23 -0500

    chore: removed wrong dependency

--- outscale/resource_outscale_oapi_vm.go
+++ outscale/resource_outscale_oapi_vm.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/hashicorp/terraform/helper/hashcode"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/terraform-providers/terraform-provider-outscale/osc/oapi"
