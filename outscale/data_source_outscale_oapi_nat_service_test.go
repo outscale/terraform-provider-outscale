31a306f814c1e3397cae3d73aa09a6b3ab87091d
Marin Salinas <marinssalinas@gmail.com>
2019-09-16 20:34:22 -0500

    test: fix ds nat service test case

--- outscale/data_source_outscale_oapi_nat_service_test.go
+++ outscale/data_source_outscale_oapi_nat_service_test.go
@@ -27,7 +27,7 @@ func TestAccOutscaleOAPINatServiceDataSource_Instance(t *testing.T) {
 				Config: testAccCheckOutscaleOAPINatServiceDataSourceConfig,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckOutscaleNatServiceDataSourceID("data.outscale_nat_service.nat"),
-					resource.TestCheckResourceAttr("data.outscale_nat_service.nat", "subnet_id", "subnet-861fbecc"),
+					resource.TestCheckResourceAttrSet("data.outscale_nat_service.nat", "subnet_id"),
 				),
 			},
 		},
@@ -35,7 +35,47 @@ func TestAccOutscaleOAPINatServiceDataSource_Instance(t *testing.T) {
 }
 
 const testAccCheckOutscaleOAPINatServiceDataSourceConfig = `
+resource "outscale_net" "outscale_net" {
+    ip_range = "10.0.0.0/16"
+}
+
+resource "outscale_subnet" "outscale_subnet" {
+    net_id     = "${outscale_net.outscale_net.net_id}"
+    ip_range = "10.0.0.0/18"
+}
+
+resource "outscale_public_ip" "outscale_public_ip" {}
+
+resource "outscale_nat_service" "outscale_nat_service" {
+    depends_on   = ["outscale_route.outscale_route"]
+    subnet_id    = "${outscale_subnet.outscale_subnet.subnet_id}"
+    public_ip_id = "${outscale_public_ip.outscale_public_ip.public_ip_id}"
+}
+
+resource "outscale_route_table" "outscale_route_table" {
+    net_id = "${outscale_net.outscale_net.net_id}"
+}
+
+resource "outscale_route" "outscale_route" {
+    destination_ip_range = "0.0.0.0/0"
+    gateway_id           = "${outscale_internet_service.outscale_internet_service.internet_service_id}"
+    route_table_id       = "${outscale_route_table.outscale_route_table.route_table_id}"
+}
+
+resource "outscale_route_table_link" "outscale_route_table_link" {
+    subnet_id      = "${outscale_subnet.outscale_subnet.subnet_id}"
+    route_table_id = "${outscale_route_table.outscale_route_table.id}"
+}
+
+resource "outscale_internet_service" "outscale_internet_service" {}
+
+resource "outscale_internet_service_link" "outscale_internet_service_link" {
+    net_id              = "${outscale_net.outscale_net.net_id}"
+    internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
+}
+
+
 data "outscale_nat_service" "nat" {
-	nat_service_id = "nat-08f41400"
+	nat_service_id = "${outscale_nat_service.outscale_nat_service.id}"
 }
 `
