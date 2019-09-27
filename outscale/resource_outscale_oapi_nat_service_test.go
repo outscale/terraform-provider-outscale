65e0d1f5206b3949b66312b5244fd40cc9598ced
PacoDw <__pm@outlook.com>
2019-09-27 10:56:13 -0500

    chore: added another test case with datasource

--- outscale/resource_outscale_oapi_nat_service_test.go
+++ outscale/resource_outscale_oapi_nat_service_test.go
@@ -43,6 +43,35 @@ func TestAccOutscaleOAPINatService_basic(t *testing.T) {
 	})
 }
 
+func TestAccOutscaleOAPINatService_basicWithDataSource(t *testing.T) {
+	o := os.Getenv("OUTSCALE_OAPI")
+
+	isOAPI, err := strconv.ParseBool(o)
+	if err != nil {
+		isOAPI = false
+	}
+
+	if !isOAPI {
+		t.Skip()
+	}
+
+	var natGateway oapi.NatService
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOAPINatGatewayDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccOAPINatGatewayConfigWithDataSource,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOAPINatGatewayExists("outscale_nat_service.outscale_nat_service", &natGateway),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckOAPINatGatewayDestroy(s *terraform.State) error {
 	conn := testAccProvider.Meta().(*OutscaleClient).OAPI
 
@@ -210,4 +239,54 @@ resource "outscale_internet_service_link" "outscale_internet_service_link" {
     net_id              = "${outscale_net.outscale_net.net_id}"
     internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
 }
+
+`
+const testAccOAPINatGatewayConfigWithDataSource = `
+	resource "outscale_net" "outscale_net" {
+		ip_range = "10.0.0.0/16"
+	}
+
+	resource "outscale_subnet" "outscale_subnet" {
+		net_id     = "${outscale_net.outscale_net.net_id}"
+		ip_range = "10.0.0.0/18"
+	}
+
+	resource "outscale_public_ip" "outscale_public_ip" {
+	}
+
+	resource "outscale_nat_service" "outscale_nat_service" {
+		depends_on   = ["outscale_route.outscale_route"]
+		subnet_id    = "${outscale_subnet.outscale_subnet.subnet_id}"
+		public_ip_id = "${outscale_public_ip.outscale_public_ip.public_ip_id}"
+	}
+
+	resource "outscale_route_table" "outscale_route_table" {
+		net_id = "${outscale_net.outscale_net.net_id}"
+	}
+
+	resource "outscale_route" "outscale_route" {
+		destination_ip_range = "0.0.0.0/0"
+		gateway_id           = "${outscale_internet_service.outscale_internet_service.internet_service_id}"
+		route_table_id       = "${outscale_route_table.outscale_route_table.route_table_id}"
+	}
+
+	resource "outscale_route_table_link" "outscale_route_table_link" {
+		subnet_id      = "${outscale_subnet.outscale_subnet.subnet_id}"
+		route_table_id = "${outscale_route_table.outscale_route_table.id}"
+	}
+
+	resource "outscale_internet_service" "outscale_internet_service" {
+	}
+
+	resource "outscale_internet_service_link" "outscale_internet_service_link" {
+		net_id              = "${outscale_net.outscale_net.net_id}"
+		internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
+	}
+
+	data "outscale_nat_service" "outscale_nat_service" {
+		filter {
+				name   = "nat_service_ids"
+				values = ["${outscale_nat_service.outscale_nat_service.nat_service_id}"]
+		}    
+	}
 `
