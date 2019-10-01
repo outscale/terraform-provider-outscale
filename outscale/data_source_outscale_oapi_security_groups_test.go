a6f0d9c3a2fa7b9d456a2d3a450ba7b6ef0df46a
Marin Salinas <marinssalinas@gmail.com>
2019-10-01 12:12:27 -0500

    feat: add request_id to security_grups data source

--- outscale/data_source_outscale_oapi_security_groups_test.go
+++ outscale/data_source_outscale_oapi_security_groups_test.go
@@ -41,41 +41,49 @@ func TestAccDataSourceOutscaleOAPISecurityGroups_vpc(t *testing.T) {
 
 func testAccDataSourceOutscaleOAPISecurityGroupConfigVPC(rInt int) string {
 	return fmt.Sprintf(`
+	resource "outscale_net" "outscale_net" {
+		ip_range = "10.0.0.0/16"
+	}
+
 	resource "outscale_security_group" "test" {
-		net_id = "vpc-e9d09d63"
+		net_id = "${outscale_net.outscale_net.id}"
 		description = "Used in the terraform acceptance tests"
-		security_group_name = "test-1--%d"
+		security_group_name = "test-1-%[1]d"
 		tag = {
 			Name = "tf-acctest"
-			Seed = "%d"
+			Seed = "%[1]d"
 		}
 	}
+
 	resource "outscale_security_group" "test2" {
-		net_id = "vpc-e9d09d63"
+		net_id = "${outscale_net.outscale_net.id}"
 		description = "Used in the terraform acceptance tests"
-		security_group_name = "test-2--%d"
+		security_group_name = "test-2-%[1]d"
 		tag = {
 			Name = "tf-acctest"
-			Seed = "%d"
+			Seed = "%[1]d"
 		}
 	}
+
 	resource "outscale_security_group" "test3" {
-		net_id = "vpc-e9d09d63"
+		net_id = "${outscale_net.outscale_net.id}"
 		description = "Used in the terraform acceptance tests"
-		security_group_name = "test-3--%d"
+		security_group_name = "test-3-%[1]d"
 		tag = {
 			Name = "tf-acctest"
-			Seed = "%d"
+			Seed = "%[1]d"
 		}
 	}
+	
 	data "outscale_security_groups" "by_id" {
-		security_group_id = ["${outscale_security_group.test.id}", "${outscale_security_group.test2.id}", "${outscale_security_group.test3.id}"]
+		security_group_ids = ["${outscale_security_group.test.id}", "${outscale_security_group.test2.id}", "${outscale_security_group.test3.id}"]
 	}
+
 	data "outscale_security_groups" "by_filter" {
 		filter {
-			name = "group-name"
+			name = "security_group_names"
 			values = ["${outscale_security_group.test.security_group_name}"]
 		}
 	}
-	`, rInt, rInt, rInt, rInt, rInt, rInt)
+	`, rInt)
 }
