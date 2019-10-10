334a8604a2ad6d9520008081224267949aa73f57
PacoDw <__pm@outlook.com>
2019-10-10 12:58:14 -0500

    fix: change test config

--- outscale/data_source_outscale_oapi_nics_test.go
+++ outscale/data_source_outscale_oapi_nics_test.go
@@ -1,55 +1,54 @@
 package outscale
 
 import (
+	"fmt"
 	"os"
-	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
 func TestAccOutscaleOAPINicsDataSource(t *testing.T) {
-	o := os.Getenv("OUTSCALE_OAPI")
+	subregion := os.Getenv("OUTSCALE_REGION")
 
-	oapi, err := strconv.ParseBool(o)
-	if err != nil {
-		oapi = false
-	}
-
-	if oapi != false {
-		t.Skip()
-	}
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
+		PreCheck: func() {
+			skipIfNoOAPI(t)
+			testAccPreCheck(t)
+		},
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckOutscaleOAPINicsDataSourceConfig,
+				Config: testAccCheckOutscaleOAPINicsDataSourceConfig(subregion),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleNicsDataSourceID("data.outscale_nat_services.nat"),
-					resource.TestCheckResourceAttr("data.outscale_nics.outscale_nics", "network_interface_set.#", "1"),
+					resource.TestCheckResourceAttr("data.outscale_nics.outscale_nics", "nics.#", "1"),
 				),
 			},
 		},
 	})
 }
 
-const testAccCheckOutscaleOAPINicsDataSourceConfig = `
-resource "outscale_net" "outscale_net" {
-    ip_range = "10.0.0.0/16"
+func testAccCheckOutscaleOAPINicsDataSourceConfig(subregion string) string {
+	return fmt.Sprintf(`
+		resource "outscale_net" "outscale_net" {
+			ip_range = "10.0.0.0/16"
+		}
+		
+		resource "outscale_subnet" "outscale_subnet" {
+			subregion_name = "%sa"
+			ip_range       = "10.0.0.0/16"
+			net_id         = "${outscale_net.outscale_net.net_id}"
+		}
+		
+		resource "outscale_nic" "outscale_nic" {
+			subnet_id = "${outscale_subnet.outscale_subnet.subnet_id}"
+		}
+		
+		data "outscale_nics" "outscale_nics" {
+			filter {
+				name   = "nic_ids"
+				values = ["${outscale_nic.outscale_nic.id}"]
+			}
+		}
+	`, subregion)
 }
-
-resource "outscale_subnet" "outscale_subnet" {
-    availability_zone   = "eu-west-2a"
-    ip_range          = "10.0.0.0/16"
-    net_id              = "${outscale_net.outscale_net.net_id}"
-}
-
-resource "outscale_nic" "outscale_nic" {
-    subnet_id = "${outscale_subnet.outscale_subnet.subnet_id}"
-}
-
-data "outscale_nics" "outscale_nics" {
-	nic_id = ["${outscale_nic.outscale_nic.id}"]
-}
-`
