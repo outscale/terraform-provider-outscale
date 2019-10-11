fff775c7462689816e38867fd66ac36baf4d0b7f
matthias.gatto <matthias.gatto@matthiasgatto-2.local>
2019-10-11 14:14:22 +0200

    tags: import oapi function from old files
    Signed-off-by: matthias.gatto <matthias.gatto@matthiasgatto-2.local>

--- outscale/resource_outscale_oapi_tags.go
+++ outscale/resource_outscale_oapi_tags.go
@@ -331,3 +331,210 @@ func assignOapiTags(tag []interface{}, resourceID string, conn *oapi.Client) err
 	}
 	return nil
 }
+
+// diffOAPITags takes our tag locally and the ones remotely and returns
+// the set of tag that must be created, and the set of tag that must
+// be destroyed.
+func diffOAPITags(oldTags, newTags []oapi.ResourceTag) ([]oapi.ResourceTag, []oapi.ResourceTag) {
+	// First, we're creating everything we have
+	create := make(map[string]interface{})
+	for _, t := range newTags {
+		create[t.Key] = t.Value
+	}
+
+	// Build the list of what to remove
+	var remove []oapi.ResourceTag
+	for _, t := range oldTags {
+		old, ok := create[t.Key]
+		if !ok || old != t.Value {
+			remove = append(remove, t)
+		}
+	}
+
+	return tagsOAPIFromMap(create), remove
+}
+
+func tagsOAPIFromMap(m map[string]interface{}) []oapi.ResourceTag {
+	result := make([]oapi.ResourceTag, 0, len(m))
+	for k, v := range m {
+		t := oapi.ResourceTag{
+			Key:   k,
+			Value: v.(string),
+		}
+		result = append(result, t)
+	}
+
+	return result
+}
+
+func tagsOAPIFromSliceMap(m []interface{}) []oapi.ResourceTag {
+	result := make([]oapi.ResourceTag, 0, len(m))
+	for _, v := range m {
+		tag := v.(map[string]interface{})
+		t := oapi.ResourceTag{
+			Key:   tag["key"].(string),
+			Value: tag["value"].(string),
+		}
+		result = append(result, t)
+	}
+
+	return result
+}
+
+// tagsOAPI	ToMap turns the list of tag into a map.
+func tagsOAPIToMap(ts []oapi.ResourceTag) []map[string]string {
+	result := make([]map[string]string, len(ts))
+	if len(ts) > 0 {
+		for k, t := range ts {
+			tag := make(map[string]string)
+			tag["key"] = t.Key
+			tag["value"] = t.Value
+			result[k] = tag
+		}
+	} else {
+		result = make([]map[string]string, 0)
+	}
+
+	return result
+}
+
+func tagsToMapI(ts []*icu.Tag) []map[string]string {
+	result := make([]map[string]string, len(ts))
+	if len(ts) > 0 {
+		for k, t := range ts {
+			tag := make(map[string]string)
+			tag["key"] = *t.Key
+			tag["value"] = *t.Value
+			result[k] = tag
+		}
+	} else {
+		result = make([]map[string]string, 0)
+	}
+
+	fmt.Printf("[DEBUG] TAG_SET %s", result)
+
+	return result
+}
+
+func tagsOAPIToMapString(ts []oapi.ResourceTag) map[string]string {
+	tags := make(map[string]string)
+	if len(ts) > 0 {
+		for _, t := range ts {
+			tags[t.Key] = t.Value
+		}
+	}
+	return tags
+}
+
+func tagsOAPISchema() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeMap,
+		Optional: true,
+		Computed: true,
+		ForceNew: true,
+	}
+}
+
+func tagsListOAPISchema() *schema.Schema {
+	return &schema.Schema{
+		Type: schema.TypeList,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"key": {
+					Type:     schema.TypeString,
+					Optional: true,
+					Computed: true,
+				},
+				"value": {
+					Type:     schema.TypeString,
+					Computed: true,
+					Optional: true,
+				},
+			},
+		},
+		Computed: true,
+		Optional: true,
+	}
+}
+
+func tagsListOAPISchemaForceNew() *schema.Schema {
+	tagsSchema := tagsListOAPISchema()
+	tagsSchema.ForceNew = true
+	return tagsSchema
+}
+
+func tagsOAPIListSchemaComputed() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Computed: true,
+		Elem: &schema.Resource{
+			Schema: map[string]*schema.Schema{
+				"key": {
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+				"value": {
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+			},
+		},
+	}
+}
+
+func tagsOAPISchemaComputed() *schema.Schema {
+	return &schema.Schema{
+		Type:     schema.TypeMap,
+		Computed: true,
+	}
+}
+
+func setOAPITags(conn *oapi.Client, d *schema.ResourceData) error {
+
+	if d.HasChange("tags") {
+		oraw, nraw := d.GetChange("tags")
+		o := oraw.([]interface{})
+		n := nraw.([]interface{})
+		create, remove := diffOAPITags(tagsOAPIFromSliceMap(o), tagsOAPIFromSliceMap(n))
+
+		// Set tag
+		if len(remove) > 0 {
+			err := resource.Retry(60*time.Second, func() *resource.RetryError {
+				_, err := conn.POST_DeleteTags(oapi.DeleteTagsRequest{
+					ResourceIds: []string{d.Id()},
+					Tags:        remove,
+				})
+				if err != nil {
+					if strings.Contains(fmt.Sprint(err), ".NotFound") {
+						return resource.RetryableError(err) // retry
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		if len(create) > 0 {
+			err := resource.Retry(60*time.Second, func() *resource.RetryError {
+				_, err := conn.POST_CreateTags(oapi.CreateTagsRequest{
+					ResourceIds: []string{d.Id()},
+					Tags:        create,
+				})
+				if err != nil {
+					if strings.Contains(fmt.Sprint(err), ".NotFound") {
+						return resource.RetryableError(err) // retry
+					}
+					return resource.NonRetryableError(err)
+				}
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
