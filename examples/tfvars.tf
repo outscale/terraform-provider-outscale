d867b89163561d7be228263c1fb6a4cf20a024b1
Marin Salinas <marinssalinas@gmail.com>
2019-10-25 15:27:09 -0500

    chore: update example files to support terraform 0.12 version

---
+++ examples/tfvars.tf
@@ -0,0 +1,11 @@
+
+variable "image_id" {}
+variable "vm_type" {}
+variable "keypair_name" {}
+variable "security_group_id" {}
+variable "region" {}
+variable "account_id" {}
+variable "vm_id" {}
+variable "snapshot_id" {}
+variable "dhcp_options_set_id" {}
+variable "security_group_name" {}
