4e1a127b589e5bf04e101cd99f5792739c0b6c2c
PacoDw <__pm@outlook.com>
2019-12-04 11:48:58 -0600

    wip: testing tf configuration in nat service

--- outscale/resource_outscale_nat_service_test.go
+++ outscale/resource_outscale_nat_service_test.go
@@ -179,91 +179,90 @@ func testAccCheckOAPINatGatewayExists(n string, ng *oapi.NatService) resource.Te
 }
 
 const testAccOAPINatGatewayConfig = `
-	resource "outscale_net" "outscale_net" {
-		ip_range = "10.0.0.0/16"
-	}
+resource "outscale_net" "outscale_net" {
+  ip_range = "10.1.0.0/16"
+}
 
-	resource "outscale_subnet" "outscale_subnet" {
-		net_id   = "${outscale_net.outscale_net.net_id}"
-		ip_range = "10.0.0.0/18"
-	}
+resource "outscale_subnet" "outscale_subnet" {
+	net_id   = "${outscale_net.outscale_net.id}"
+  ip_range = "10.1.1.0/24"
+}
 
-	resource "outscale_public_ip" "outscale_public_ip" {}
+resource "outscale_internet_service" "outscale_internet_service" {}
 
-	resource "outscale_nat_service" "outscale_nat_service" {
-		depends_on   = ["outscale_route.outscale_route"]
-		subnet_id    = "${outscale_subnet.outscale_subnet.subnet_id}"
-		public_ip_id = "${outscale_public_ip.outscale_public_ip.public_ip_id}"
-	}
+resource "outscale_internet_service_link" "outscale_internet_service_link" {
+  internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
+  net_id              = "${outscale_net.outscale_net.net_id}"
+}
 
-	resource "outscale_route_table" "outscale_route_table" {
-		net_id = "${outscale_net.outscale_net.net_id}"
-	}
+resource "outscale_route_table" "outscale_route_table" {
+	net_id = "${outscale_net.outscale_net.id}"
+	
+  depends_on = ["outscale_internet_service_link.outscale_internet_service_link"]
+}
 
-	resource "outscale_route" "outscale_route" {
-		destination_ip_range = "0.0.0.0/0"
-		gateway_id           = "${outscale_internet_service.outscale_internet_service.internet_service_id}"
-		route_table_id       = "${outscale_route_table.outscale_route_table.route_table_id}"
-	}
+resource "outscale_route_table_link" "outscale_route_table_link" {
+  route_table_id = "${outscale_route_table.outscale_route_table.id}"
+	subnet_id      = "${outscale_subnet.outscale_subnet.id}"
+	
+  depends_on = ["outscale_route_table.outscale_route_table"]
+}
 
-	resource "outscale_route_table_link" "outscale_route_table_link" {
-		subnet_id      = "${outscale_subnet.outscale_subnet.subnet_id}"
-		route_table_id = "${outscale_route_table.outscale_route_table.id}"
-	}
+resource "outscale_public_ip" "outscale_public_ip" {}
 
-	resource "outscale_internet_service" "outscale_internet_service" {}
+resource "outscale_route" "outscale_route" {
+  destination_ip_range = "0.0.0.0/0"
+  gateway_id           = "${outscale_internet_service.outscale_internet_service.id}"
+	route_table_id       = "${outscale_route_table.outscale_route_table.id}"
+	
+  depends_on = ["outscale_route_table_link.outscale_route_table_link", "outscale_internet_service_link.outscale_internet_service_link"]
+}
 
-	resource "outscale_internet_service_link" "outscale_internet_service_link" {
-		net_id              = "${outscale_net.outscale_net.net_id}"
-		internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
-	}
+resource "outscale_nat_service" "outscale_nat_service" {
+  subnet_id    = "${outscale_subnet.outscale_subnet.id}"
+  public_ip_id = "${outscale_public_ip.outscale_public_ip.public_ip_id}"
+
+  depends_on = ["'outscale_route_table_link.outscale_route_table_link",outscale_route.outscale_route"]
+}
 `
+
 const testAccOAPINatGatewayConfigWithDataSource = `
-	resource "outscale_net" "outscale_net" {
-		ip_range = "10.0.0.0/16"
-	}
-
-	resource "outscale_subnet" "outscale_subnet" {
-		net_id     = "${outscale_net.outscale_net.net_id}"
-		ip_range = "10.0.0.0/18"
-	}
-
-	resource "outscale_public_ip" "outscale_public_ip" {
-	}
-
-	resource "outscale_nat_service" "outscale_nat_service" {
-		depends_on   = ["outscale_route.outscale_route"]
-		subnet_id    = "${outscale_subnet.outscale_subnet.subnet_id}"
-		public_ip_id = "${outscale_public_ip.outscale_public_ip.public_ip_id}"
-	}
-
-	resource "outscale_route_table" "outscale_route_table" {
-		net_id = "${outscale_net.outscale_net.net_id}"
-	}
-
-	resource "outscale_route" "outscale_route" {
-		destination_ip_range = "0.0.0.0/0"
-		gateway_id           = "${outscale_internet_service.outscale_internet_service.internet_service_id}"
-		route_table_id       = "${outscale_route_table.outscale_route_table.route_table_id}"
-	}
-
-	resource "outscale_route_table_link" "outscale_route_table_link" {
-		subnet_id      = "${outscale_subnet.outscale_subnet.subnet_id}"
-		route_table_id = "${outscale_route_table.outscale_route_table.id}"
-	}
-
-	resource "outscale_internet_service" "outscale_internet_service" {
-	}
-
-	resource "outscale_internet_service_link" "outscale_internet_service_link" {
-		net_id              = "${outscale_net.outscale_net.net_id}"
-		internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
-	}
-
-	data "outscale_nat_service" "outscale_nat_service" {
-		filter {
-				name   = "nat_service_ids"
-				values = ["${outscale_nat_service.outscale_nat_service.nat_service_id}"]
-		}    
-	}
+resource "outscale_net" "outscale_net" {
+	ip_range = "10.0.0.0/16"
+}
+resource "outscale_subnet" "outscale_subnet" {
+	net_id     = "${outscale_net.outscale_net.net_id}"
+	ip_range = "10.0.0.0/18"
+}
+resource "outscale_public_ip" "outscale_public_ip" {
+}
+resource "outscale_nat_service" "outscale_nat_service" {
+	depends_on   = ["outscale_route.outscale_route"]
+	subnet_id    = "${outscale_subnet.outscale_subnet.subnet_id}"
+	public_ip_id = "${outscale_public_ip.outscale_public_ip.public_ip_id}"
+}
+resource "outscale_route_table" "outscale_route_table" {
+	net_id = "${outscale_net.outscale_net.net_id}"
+}
+resource "outscale_route" "outscale_route" {
+	destination_ip_range = "0.0.0.0/0"
+	gateway_id           = "${outscale_internet_service_link.outscale_internet_service_link.internet_service_id}"
+	route_table_id       = "${outscale_route_table.outscale_route_table.route_table_id}"
+}
+resource "outscale_route_table_link" "outscale_route_table_link" {
+	subnet_id      = "${outscale_subnet.outscale_subnet.subnet_id}"
+	route_table_id = "${outscale_route_table.outscale_route_table.id}"
+}
+resource "outscale_internet_service" "outscale_internet_service" {
+}
+resource "outscale_internet_service_link" "outscale_internet_service_link" {
+	net_id              = "${outscale_net.outscale_net.net_id}"
+	internet_service_id = "${outscale_internet_service.outscale_internet_service.id}"
+}
+data "outscale_nat_service" "outscale_nat_service" {
+	filter {
+			name   = "nat_service_ids"
+			values = ["${outscale_nat_service.outscale_nat_service.nat_service_id}"]
+	}    
+}
 `
