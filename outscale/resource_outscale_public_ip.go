43d2fbf4b120f275c15ce85b617e1dd9ad341af2
Marin Salinas <marinssalinas@gmail.com>
2019-12-11 15:56:40 -0600

    feat: add tags to public_ip resource

--- outscale/resource_outscale_public_ip.go
+++ outscale/resource_outscale_public_ip.go
@@ -33,13 +33,7 @@ func resourceOutscaleOAPIPublicIP() *schema.Resource {
 
 func resourceOutscaleOAPIPublicIPCreate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).OAPI
-
-	//domainOpt := resourceOutscaleOAPIPublicIPDomain(d)
-
-	allocOpts := oapi.CreatePublicIpRequest{
-		//Missing on Swagger Spec
-		//Placement: domainOpt,
-	}
+	allocOpts := oapi.CreatePublicIpRequest{}
 
 	log.Printf("[DEBUG] EIP create configuration: %#v", allocOpts)
 	resp, err := conn.POST_CreatePublicIp(allocOpts)
@@ -54,13 +48,10 @@ func resourceOutscaleOAPIPublicIPCreate(d *schema.ResourceData, meta interface{}
 	allocResp := resp.OK
 
 	log.Printf("[DEBUG] EIP Allocate: %#v", allocResp)
-	// if d.Get("placement").(string) == "vpc" {
-	// 	d.SetId(allocResp.ReservationId)
-	// } else {
-	// 	d.SetId(allocResp.PublicIp)
-	// }
 
-	d.SetId(allocResp.PublicIp.PublicIp)
+	d.SetId(allocResp.PublicIp.PublicIpId)
+
+	log.Printf("[DEBUG], allocResp: %+v", allocResp)
 
 	//SetTags
 	if tags, ok := d.GetOk("tags"); ok {
@@ -70,27 +61,17 @@ func resourceOutscaleOAPIPublicIPCreate(d *schema.ResourceData, meta interface{}
 		}
 	}
 
-	log.Printf("[INFO] EIP ID: %s (placement: %v)", d.Id(), allocResp.PublicIp)
-	return resourceOutscaleOAPIPublicIPUpdate(d, meta)
+	return resourceOutscaleOAPIPublicIPRead(d, meta)
 }
 
 func resourceOutscaleOAPIPublicIPRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).OAPI
 
-	placement := resourceOutscaleOAPIPublicIPDomain(d)
 	id := d.Id()
 
 	req := oapi.ReadPublicIpsRequest{}
 
-	//Not Used
-	//filters := []oapi.Filters{}
-
-	// if placement == "vpc" {
-	// 	req.Filters.ReservationId = []string{id}
-	// } else {
-	// 	req.Filters.PublicIps = []string{id}
-	// }
-	req.Filters.PublicIps = []string{id}
+	req.Filters.PublicIpIds = []string{id}
 
 	var describeAddresses *oapi.ReadPublicIpsResponse
 	resp, err := conn.POST_ReadPublicIps(req)
@@ -105,108 +86,49 @@ func resourceOutscaleOAPIPublicIPRead(d *schema.ResourceData, meta interface{}) 
 		return fmt.Errorf("Error retrieving EIP: %s", err)
 	}
 
-	if len(describeAddresses.PublicIps) != 1 ||
-		placement == "vpc" && describeAddresses.PublicIps[0].LinkPublicIpId != id ||
-		describeAddresses.PublicIps[0].PublicIp != id {
-		if err != nil {
-			return fmt.Errorf("Unable to find EIP: %#v", describeAddresses.PublicIps)
-		}
+	if len(describeAddresses.PublicIps) != 1 || describeAddresses.PublicIps[0].PublicIpId != id {
+		return fmt.Errorf("Unable to find Public IP: %#v", describeAddresses.PublicIps)
+
 	}
 
-	address := describeAddresses.PublicIps[0]
+	publicIP := describeAddresses.PublicIps[0]
 
-	log.Printf("[DEBUG] EIP read configuration: %+v", address)
+	log.Printf("[DEBUG] EIP read configuration: %+v", publicIP)
 
-	if address.LinkPublicIpId != "" {
-		d.Set("link_public_ip_id", address.LinkPublicIpId)
+	if publicIP.LinkPublicIpId != "" {
+		d.Set("link_public_ip_id", publicIP.LinkPublicIpId)
 	} else {
 		d.Set("link_public_ip_id", "")
 	}
-	if address.VmId != "" {
-		d.Set("vm_id", address.VmId)
+	if publicIP.VmId != "" {
+		d.Set("vm_id", publicIP.VmId)
 	} else {
 		d.Set("vm_id", "")
 	}
-	if address.NicId != "" {
-		d.Set("nic_id", address.NicId)
+	if publicIP.NicId != "" {
+		d.Set("nic_id", publicIP.NicId)
 	} else {
 		d.Set("nic_id", "")
 	}
-	if address.NicAccountId != "" {
-		d.Set("nic_account_id", address.NicAccountId)
+	if publicIP.NicAccountId != "" {
+		d.Set("nic_account_id", publicIP.NicAccountId)
 	} else {
 		d.Set("nic_account_id", "")
 	}
-	d.Set("private_ip", address.PrivateIp)
-	d.Set("public_ip", address.PublicIp)
+	d.Set("private_ip", publicIP.PrivateIp)
+	d.Set("public_ip", publicIP.PublicIp)
 
-	d.Set("public_ip_id", address.PublicIpId)
+	d.Set("public_ip_id", publicIP.PublicIpId)
 
-	// if address.Placement == "vpc" && net.ParseIP(id) != nil {
-	// 	log.Printf("[DEBUG] Re-assigning EIP ID (%s) to it's Allocation ID (%s)", d.Id(), address.ReservationId)
-	// 	d.SetId(address.ReservationId)
-	// } else {
-	// 	d.SetId(address.PublicIp)
-	// }
-	if err := d.Set("tags", tagsOAPIToMap(address.Tags)); err != nil {
-		fmt.Printf("[WARN] ERROR TAGS PROBLEME (%s)", err)
+	if err := d.Set("tags", tagsOAPIToMap(publicIP.Tags)); err != nil {
+		log.Printf("[WARN] error setting tags for PublicIp(%s): %s", publicIP.PublicIp, err)
 	}
-	d.SetId(address.PublicIp)
 
 	return d.Set("request_id", describeAddresses.ResponseContext.RequestId)
 }
 
 func resourceOutscaleOAPIPublicIPUpdate(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).OAPI
-
-	placement := resourceOutscaleOAPIPublicIPDomain(d)
-
-	vInstance, okInstance := d.GetOk("vm_id")
-	vInterface, okInterface := d.GetOk("nic_id")
-
-	if okInstance || okInterface {
-		instanceID := vInstance.(string)
-		networkInterfaceID := vInterface.(string)
-
-		assocOpts := oapi.LinkPublicIpRequest{
-			VmId:     instanceID,
-			PublicIp: d.Id(),
-		}
-
-		if placement == "vpc" {
-			var privateIPAddress string
-			if v := d.Get("private_ip").(string); v != "" {
-				privateIPAddress = v
-			}
-			assocOpts = oapi.LinkPublicIpRequest{
-				NicId: networkInterfaceID,
-				VmId:  instanceID,
-				//ReservationId: d.Id(),
-				PrivateIp: privateIPAddress,
-			}
-		}
-
-		err := resource.Retry(120*time.Second, func() *resource.RetryError {
-			var err error
-			_, err = conn.POST_LinkPublicIp(assocOpts)
-
-			if err != nil {
-				if e := fmt.Sprint(err); strings.Contains(e, "InvalidAllocationID.NotFound") || strings.Contains(e, "InvalidAddress.NotFound") {
-					return resource.RetryableError(err)
-				}
-
-				return resource.NonRetryableError(err)
-			}
-
-			return nil
-		})
-
-		if err != nil {
-			d.Set("vm_id", "")
-			d.Set("nic_id", "")
-			return fmt.Errorf("Failure associating EIP: %s", err)
-		}
-	}
 
 	d.Partial(true)
 
@@ -231,52 +153,12 @@ func resourceOutscaleOAPIPublicIPDelete(d *schema.ResourceData, meta interface{}
 		return nil
 	}
 
-	vInstance, okInstance := d.GetOk("vm_id")
-	linkPublicIPID, okAssociationID := d.GetOk("link_public_ip_id")
-
-	if (okInstance && vInstance.(string) != "") || (okAssociationID && linkPublicIPID.(string) != "") {
-		log.Printf("[DEBUG] Disassociating EIP: %s", d.Id())
-		var err error
-		switch resourceOutscaleOAPIPublicIPDomain(d) {
-		case "vpc":
-			_, err = conn.POST_UnlinkPublicIp(oapi.UnlinkPublicIpRequest{
-				LinkPublicIpId: d.Get("link_public_ip_id").(string),
-			})
-		case "standard":
-			_, err = conn.POST_UnlinkPublicIp(oapi.UnlinkPublicIpRequest{
-				PublicIp: d.Get("public_ip").(string),
-			})
-		}
-
-		if err != nil {
-			if e := fmt.Sprint(err); strings.Contains(e, "InvalidAllocationID.NotFound") || strings.Contains(e, "InvalidAddress.NotFound") {
-				return nil
-			}
-			return err
-		}
-	}
-
-	//placement := resourceOutscaleOAPIPublicIPDomain(d)
 	return resource.Retry(3*time.Minute, func() *resource.RetryError {
 		var err error
-		// switch placement {
-		// case "vpc":
-		// 	fmt.Printf(
-		// 		"[DEBUG] EIP release (destroy) address allocation: %v",
-		// 		d.Id())
-		// 	_, err = conn.POST_DeletePublicIp(oapi.DeletePublicIpRequest{
-		// 		ReservationId: d.Id(),
-		// 	})
-		// case "standard":
-		// 	log.Printf("[DEBUG] EIP release (destroy) address: %v", d.Id())
-		// 	_, err = conn.POST_DeletePublicIp(oapi.DeletePublicIpRequest{
-		// 		PublicIp: d.Id(),
-		// 	})
-		// }
 
-		log.Printf("[DEBUG] EIP release (destroy) address: %v", d.Id())
+		log.Printf("[DEBUG] EIP release (destroy) IP address: %v", d.Id())
 		_, err = conn.POST_DeletePublicIp(oapi.DeletePublicIpRequest{
-			PublicIp: d.Id(),
+			PublicIpId: d.Id(),
 		})
 
 		if e := fmt.Sprint(err); strings.Contains(e, "InvalidAllocationID.NotFound") || strings.Contains(e, "InvalidAddress.NotFound") {
@@ -292,16 +174,6 @@ func resourceOutscaleOAPIPublicIPDelete(d *schema.ResourceData, meta interface{}
 
 		return resource.RetryableError(err)
 	})
-}
-
-func resourceOutscaleOAPIPublicIPDomain(d *schema.ResourceData) string {
-	if v, ok := d.GetOk("placement"); ok {
-		return v.(string)
-	} else if strings.Contains(d.Id(), "eipalloc") {
-		return "vpc"
-	}
-
-	return "standard"
 }
 
 func getOAPIPublicIPSchema() map[string]*schema.Schema {
