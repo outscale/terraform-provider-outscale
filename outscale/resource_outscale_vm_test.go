e7d88c0f820469744e8fccfb24dd0cd9bdefafec
Marin Salinas <marinssalinas@gmail.com>
2019-12-18 11:42:45 -0600

    test: add test case of vm with net, route table and internet services

--- outscale/resource_outscale_vm_test.go
+++ outscale/resource_outscale_vm_test.go
@@ -251,6 +251,35 @@ func TestAccOutscaleOAPIVMTags_Update(t *testing.T) {
 	})
 }
 
+func TestAccOutscaleOAPIVM_WithNet(t *testing.T) {
+	var server oapi.Vm
+	omi := getOMIByRegion("eu-west-2", "centos").OMI
+	region := os.Getenv("OUTSCALE_REGION")
+	vmType := "t2.micro"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			skipIfNoOAPI(t)
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOutscaleOAPIVMDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckOutscaleOAPIVMConfigWithNet(omi, vmType, region),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleOAPIVMExists("outscale_vm.outscale_vmnet", &server),
+					testAccCheckOutscaleOAPIVMAttributes(t, &server, omi),
+					resource.TestCheckResourceAttr(
+						"outscale_vm.outscale_vmnet", "image_id", omi),
+					resource.TestCheckResourceAttr(
+						"outscale_vm.outscale_vmnet", "vm_type", vmType),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckOutscaleDeletionProtectionUpdateBasic(omi, deletionProtection string) string {
 	return fmt.Sprintf(`
 		resource "outscale_vm" "outscale_vm" {
@@ -689,6 +718,76 @@ func testAccCheckOutscaleOAPIVMConfigWithBlockDeviceMappings(omi, vmType, region
 		  }
 		}
 	  }
+	`, omi, vmType, region)
+}
+
+func testAccCheckOutscaleOAPIVMConfigWithNet(omi, vmType, region string) string {
+	return fmt.Sprintf(`
+	resource "outscale_net" "outscale_net" {
+		ip_range = "10.0.0.0/16"
+		
+		tags  {                               
+			key   = "name"
+			value = "Terraform_net"
+		}
+	}
+	resource "outscale_subnet" "outscale_subnet" {
+		net_id         = "${outscale_net.outscale_net.net_id}"
+		ip_range       = "10.0.0.0/24"
+		subregion_name = "%[3]sb"    
+		
+		tags {                               
+			key   = "name"
+			value = "Terraform_subnet"
+		}
+	}
+
+	resource "outscale_security_group" "outscale_sg" {
+		description         = "sg for terraform tests"
+		security_group_name = "terraform-sg"
+		net_id              = "${outscale_net.outscale_net.net_id}"
+	}
+	
+	resource "outscale_internet_service" "outscale_internet_service" {}
+
+	resource "outscale_route_table" "outscale_route_table" {
+		net_id = "${outscale_net.outscale_net.net_id}"
+		
+		tags {                               
+			key   = "name"
+			value = "Terraform_RT"
+		}
+	}
+
+	resource "outscale_route_table_link" "outscale_route_table_link" {
+		route_table_id  = "${outscale_route_table.outscale_route_table.route_table_id}"
+		subnet_id       = "${outscale_subnet.outscale_subnet.subnet_id}"
+	}
+
+	resource "outscale_internet_service_link" "outscale_internet_service_link" {
+		internet_service_id = "${outscale_internet_service.outscale_internet_service.internet_service_id}" 
+		net_id              = "${outscale_net.outscale_net.net_id}"
+	}
+
+	resource "outscale_route" "outscale_route" {
+		gateway_id           = "${outscale_internet_service.outscale_internet_service.internet_service_id}"
+		destination_ip_range = "0.0.0.0/0"
+		route_table_id       = "${outscale_route_table.outscale_route_table.route_table_id}"
+	} 
+	resource "outscale_vm" "outscale_vmnet" {
+		image_id           = "%[1]s"
+		vm_type            = "%[2]s"
+		keypair_name       = "terraform-basic"
+		security_group_ids = ["${outscale_security_group.outscale_sg.security_group_id}"]
+		subnet_id          ="${outscale_subnet.outscale_subnet.subnet_id}"
+	}
+
+	resource "outscale_public_ip" "outscale_public_ip" {}
+
+	resource "outscale_public_ip_link" "outscale_public_ip_link" {
+		vm_id     = "${outscale_vm.outscale_vmnet.vm_id}"
+		public_ip = "${outscale_public_ip.outscale_public_ip.public_ip}"
+	}
 	`, omi, vmType, region)
 }
 
