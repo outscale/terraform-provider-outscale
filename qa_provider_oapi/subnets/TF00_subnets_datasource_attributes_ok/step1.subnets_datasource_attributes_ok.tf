a91cf255465f6fdb4f81f763325fdcbad2ad2abe
meriem.zouari <meriem.zouari@outscale.com>
2020-01-08 09:36:54 +0000

    Update step1.subnets_datasource_attributes_ok.tf

--- qa_provider_oapi/subnets/TF00_subnets_datasource_attributes_ok/step1.subnets_datasource_attributes_ok.tf
+++ qa_provider_oapi/subnets/TF00_subnets_datasource_attributes_ok/step1.subnets_datasource_attributes_ok.tf
@@ -1,23 +1,28 @@
 resource "outscale_net" "outscale_net" {
-   # count = 1
-
+  
     ip_range = "10.0.0.0/16"
 }
 
 resource "outscale_subnet" "outscale_subnet" {
-   # count = 1
 
     subregion_name = format("%s%s", var.region, "a")
     ip_range       = "10.0.10.0/24"
     net_id         = outscale_net.outscale_net.net_id
+    tags {
+     key = "name"
+     value = "subnet-tags-1"
+    }
 }
 
 resource "outscale_subnet" "outscale_subnet2" {
-    # count = 1
-
+  
     subregion_name = format("%s%s", var.region, "a")
     ip_range       = "10.0.20.0/24"
     net_id         = outscale_net.outscale_net.net_id
+    tags {
+     key = "name"
+     value = "subnet-tags-2"
+    }
 }
 data "outscale_subnets" "outscale_subnets" {
     filter {
