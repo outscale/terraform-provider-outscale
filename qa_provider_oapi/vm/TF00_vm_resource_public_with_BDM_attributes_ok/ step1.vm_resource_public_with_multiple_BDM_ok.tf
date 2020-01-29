1af9234efd1e756ba5617424f67790df0c6a3028
meriem.zouari <meriem.zouari@outscale.com>
2020-01-29 12:27:52 +0000

    test public VM with multiple BDM

---
+++ qa_provider_oapi/vm/TF00_vm_resource_public_with_BDM_attributes_ok/ step1.vm_resource_public_with_multiple_BDM_ok.tf
@@ -0,0 +1,25 @@
+## Test Public  VM with Block Device Mapping with multiple volumes  ##
+
+resource "outscale_vm" "outscale_vm2" {
+    image_id            = var.image_id
+    vm_type             = var.vm_type
+    keypair_name        = var.keypair_name
+    block_device_mappings {
+     device_name = "/dev/sdb"
+     bsu = {
+         volume_size=15
+         volume_type = "gp2"
+         snapshot_id = var.snapshot_id
+       }
+    }
+    block_device_mappings {
+     device_name = "/dev/sdc"
+     bsu = {
+         volume_size=22
+         volume_type = "io1"
+         iops      = 150
+         snapshot_id = var.snapshot_id
+         delete_on_vm_deletion = true
+      }
+    }
+}
