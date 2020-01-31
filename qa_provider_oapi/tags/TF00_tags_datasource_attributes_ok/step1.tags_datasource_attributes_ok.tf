955788fa78286d60f08c5f1efd180ab9e4ec5b73
meriem.zouari <meriem.zouari@outscale.com>
2020-01-31 11:01:04 +0000

    Update step1.tags_datasource_attributes_ok.tf

--- qa_provider_oapi/tags/TF00_tags_datasource_attributes_ok/step1.tags_datasource_attributes_ok.tf
+++ qa_provider_oapi/tags/TF00_tags_datasource_attributes_ok/step1.tags_datasource_attributes_ok.tf
@@ -1,41 +1,23 @@
 resource "outscale_vm" "outscale_vm" {
-    #image_id               = "ami-880caa66"
-    #image_id               = "ami-7f57f68f"      #dv1
-    image_id               = "ami-5c450b62" #IN2
-    vm_type                = "c4.large"
-    #keypair_name           = "integ_sut_keypair"
-    #keypair_name           = "terraform"
-    keypair_name           = "testkp"
-    #firewall_rules_set_ids = ["sg-c73d3b6b"]
-    #firewall_rules_set_id  = "sg-c73d3b6b"    # tempo tests
-    #security_group_ids     = ["sg-419f2c0c"]   # SV1
-    security_group_ids     = ["sg-9752b7a6"]   # IN2
-
-    tags = {                               # NOK should be tag not tags
+    image_id               = var.image_id
+    vm_type                = var.vm_type
+    keypair_name           = var.keypair_name
+    security_group_ids     = [var.security_group_id]
+    tags  {                              
         key   = "name7"
-        value = "testDataSource7"          # NOK delete doesn't delete tag
-        #name8 = "testDataSource8"          # tfa doesn't display correctly
-    }                                      # tfs displays nothing
+        value = "testDataSource7"        
+    }                           
 }
 
 resource "outscale_vm" "outscale_vm2" {
-    #image_id               = "ami-880caa66"
-    #image_id               = "ami-7f57f68f"      #dv1
-    image_id               = "ami-5c450b62" #IN2
-    vm_type                = "c4.large"
-    #keypair_name           = "integ_sut_keypair"
-    #keypair_name           = "terraform"
-    keypair_name           = "testkp"
-    #firewall_rules_set_ids = ["sg-c73d3b6b"]
-    #firewall_rules_set_id  = "sg-c73d3b6b"    # tempo tests
-    #security_group_ids     = ["sg-419f2c0c"]   # SV1
-    security_group_ids     = ["sg-9752b7a6"]   # IN2
-
-    tags = {                               # NOK should be tag not tags
-        key   = "name72"
-        value = "testDataSource72"          # NOK delete doesn't delete tag
-        #name8 = "testDataSource8"          # tfa doesn't display correctly
-    }                                      # tfs displays nothing
+    image_id               = var.image_id
+    vm_type                = var.vm_type
+    keypair_name           = var.keypair_name
+    security_group_ids     = [var.security_group_id]
+    tags  {                              
+        key   = "name7"
+        value = "testDataSource72"        
+    }                 
 }
 
 data "outscale_tags" "outscale_tags" {
