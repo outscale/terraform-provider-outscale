61d19e2ebd54f7e2d1527a0d9c0821009ae7ae78
Thiery Ouattara <thiery.ouattara@outscale.com>
2020-01-31 13:58:42 +0000

    fix firewall

--- outscale/resource_outscale_firewall_rules_set.go
+++ outscale/resource_outscale_firewall_rules_set.go
@@ -5,12 +5,8 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
-	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/hashicorp/terraform/helper/hashcode"
-	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
@@ -52,27 +48,6 @@ func getIPPerms() *schema.Schema {
 	}
 }
 
-func resourceOutscaleSecurityGroupDelete(d *schema.ResourceData, meta interface{}) error {
-	conn := meta.(*OutscaleClient).FCU
-
-	return resource.Retry(5*time.Minute, func() *resource.RetryError {
-		_, err := conn.VM.DeleteSecurityGroup(&fcu.DeleteSecurityGroupInput{
-			GroupId: aws.String(d.Id()),
-		})
-
-		if err != nil {
-			if strings.Contains(err.Error(), "RequestLimitExceeded") || strings.Contains(err.Error(), "DependencyViolation") {
-				return resource.RetryableError(err)
-			} else if strings.Contains(err.Error(), "InvalidGroup.NotFound") {
-				return nil
-			}
-			return resource.NonRetryableError(err)
-		}
-
-		return nil
-	})
-}
-
 func idHash(rType, protocol string, toPort, fromPort int64, self bool) string {
 	var buf bytes.Buffer
 	buf.WriteString(fmt.Sprintf("%s-", rType))
@@ -82,86 +57,6 @@ func idHash(rType, protocol string, toPort, fromPort int64, self bool) string {
 	buf.WriteString(fmt.Sprintf("%t-", self))
 
 	return fmt.Sprintf("rule-%d", hashcode.String(buf.String()))
-}
-
-func flattenSecurityGroups(list []*fcu.UserIdGroupPair, ownerID *string) []*fcu.GroupIdentifier {
-	result := make([]*fcu.GroupIdentifier, 0, len(list))
-	for _, g := range list {
-		var userID *string
-		if g.UserId != nil && *g.UserId != "" && (ownerID == nil || *ownerID != *g.UserId) {
-			userID = g.UserId
-		}
-
-		vpc := g.GroupName == nil || *g.GroupName == ""
-		var ID *string
-		if vpc {
-			ID = g.GroupId
-		} else {
-			ID = g.GroupName
-		}
-
-		if userID != nil {
-			ID = aws.String(*userID + "/" + *ID)
-		}
-
-		if vpc {
-			result = append(result, &fcu.GroupIdentifier{
-				GroupId: ID,
-			})
-		} else {
-			result = append(result, &fcu.GroupIdentifier{
-				GroupId:   g.GroupId,
-				GroupName: ID,
-			})
-		}
-	}
-	return result
-}
-
-// SGStateRefreshFunc ...
-func SGStateRefreshFunc(conn *fcu.Client, ID string) resource.StateRefreshFunc {
-	return func() (interface{}, string, error) {
-		req := &fcu.DescribeSecurityGroupsInput{
-			GroupIds: []*string{aws.String(ID)},
-		}
-
-		var err error
-		var resp *fcu.DescribeSecurityGroupsOutput
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			resp, err = conn.VM.DescribeSecurityGroups(req)
-
-			if err != nil {
-				if strings.Contains(err.Error(), "RequestLimitExceeded") {
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
-			}
-
-			return nil
-		})
-
-		if err != nil {
-			if ec2err, ok := err.(awserr.Error); ok {
-				if ec2err.Code() == "InvalidSecurityGroupID.NotFound" ||
-					ec2err.Code() == "InvalidGroup.NotFound" {
-					resp = nil
-					err = nil
-				}
-			}
-
-			if err != nil {
-				fmt.Printf("\n\nError on SGStateRefresh: %s", err)
-				return nil, "", err
-			}
-		}
-
-		if resp == nil {
-			return nil, "", nil
-		}
-
-		group := resp.SecurityGroups[0]
-		return group, "exists", nil
-	}
 }
 
 func protocolForValue(v string) string {
