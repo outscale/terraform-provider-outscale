65c658dba805e79ded8a6aa3227153197e7bb4f8
PacoDw <__pm@outlook.com>
2020-02-26 11:49:07 -0600

    chore: added test case to check new filters

--- outscale/data_source_outscale_volumes_test.go
+++ outscale/data_source_outscale_volumes_test.go
@@ -45,13 +45,33 @@ func TestAccOutscaleOAPIVolumeDataSource_multipleVIdsFilters(t *testing.T) {
 	})
 }
 
+func TestAccOutscaleOAPIVolumesDataSource_withVM(t *testing.T) {
+	region := os.Getenv("OUTSCALE_REGION")
+	omi := os.Getenv("OUTSCALE_IMAGEID")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckOutscaleOAPIVolumesDataSourceConfigWithVM(region, omi),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleOAPIVolumeDataSourceID("data.outscale_volumes.outscale_volumes"),
+					// resource.TestCheckResourceAttr("data.outscale_volumes.outscale_volumes", "volumes.0.size", "1"),
+					// resource.TestCheckResourceAttr("data.outscale_volumes.outscale_volumes", "volumes.0.volume_type", "gp2"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckOutscaleOAPIVolumeDataSourceConfigWithMultipleFilters(region string) string {
 	return fmt.Sprintf(`
 		resource "outscale_volume" "external" {
 			subregion_name = "%sa"
 			volume_type    = "gp2"
 			size           = 1
-		
+
 			tags {
 				key   = "Name"
 				value = "tf-acc-test-ebs-volume-test"
@@ -91,4 +111,71 @@ func testAccCheckOutscaleOAPIVolumesDataSourceConfigWithMultipleVolumeIDsFilter(
 			}
 		}
 	`, region)
+}
+
+func testAccCheckOutscaleOAPIVolumesDataSourceConfigWithVM(region, imageID string) string {
+	return fmt.Sprintf(`
+		resource "outscale_volume" "outscale_volume" {
+			subregion_name = "%[1]sa"
+			volume_type    = "gp2"
+			size           = 25
+			tags {
+				key   = "Name"
+				value = "volume-gp2-1"
+			}
+		}
+
+		resource "outscale_volume" "outscale_volume2" {
+			subregion_name = "%[1]sa"
+			volume_type    = "standard"
+			size           = 13
+			tags {
+				key   = "Name"
+				value = "volume-standard-2"
+			}
+		}
+
+		resource "outscale_volume" "outscale_volume3" {
+			subregion_name = "%[1]sa"
+			size           = 40
+			iops           = 100
+			volume_type    = "io1"
+			tags {
+				key   = "type"
+				value = "io1"
+			}
+		}
+
+		resource "outscale_vm" "outscale_vm" {
+			image_id           = "%s"
+			vm_type            = "t2.micro"
+			keypair_name       = "terraform-basic"
+			security_group_ids = ["sg-f4b1c2f8"]
+		}
+
+		resource "outscale_volumes_link" "outscale_volumes_link" {
+			device_name = "/dev/xvdc"
+			volume_id   = "${outscale_volume.outscale_volume.id}"
+			vm_id       = "${outscale_vm.outscale_vm.id}"
+		}
+
+		resource "outscale_volumes_link" "outscale_volumes_link_2" {
+			device_name = "/dev/xvdd"
+			volume_id   = "${outscale_volume.outscale_volume2.id}"
+			vm_id       = "${outscale_vm.outscale_vm.id}"
+		}
+
+		resource "outscale_volumes_link" "outscale_volumes_link_3" {
+			device_name = "/dev/xvde"
+			volume_id   = "${outscale_volume.outscale_volume3.id}"
+			vm_id       = "${outscale_vm.outscale_vm.id}"
+		}
+
+		data "outscale_volumes" "outscale_volumes" {
+			filter {
+				name   = "link_volume_vm_ids"
+				values = ["${outscale_vm.outscale_vm.vm_id}"]
+			}
+		}
+	`, region, imageID)
 }
