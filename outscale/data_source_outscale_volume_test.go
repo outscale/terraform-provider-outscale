4653fe23e8cfed3d9b324a0acf7462c080b7008d
PacoDw <__pm@outlook.com>
2020-02-27 15:15:20 -0600

    chore: added tags import test function

--- outscale/data_source_outscale_volume_test.go
+++ outscale/data_source_outscale_volume_test.go
@@ -28,6 +28,25 @@ func TestAccOutscaleOAPIVolumeDataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccOutscaleOAPIVolumeDataSource_filterByTags(t *testing.T) {
+	region := os.Getenv("OUTSCALE_REGION")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckOutscaleOAPIVolumeDataSourceConfigFilterByTags(region),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleOAPIVolumeDataSourceID("data.outscale_volume.ebs_volume"),
+					resource.TestCheckResourceAttr("data.outscale_volume.ebs_volume", "size", "10"),
+					resource.TestCheckResourceAttr("data.outscale_volume.ebs_volume", "volume_type", "gp2"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckOutscaleOAPIVolumeDataSourceID(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -48,18 +67,46 @@ func testAccCheckOutscaleOAPIVolumeDataSourceConfig(region string) string {
 			subregion_name = "%sa"
 			volume_type    = "gp2"
 			size           = 10
-		
+
 			tags {
 				key   = "Name"
 				value = "External Volume"
 			}
 		}
-		
+
 		data "outscale_volume" "ebs_volume" {
 			filter {
 				name   = "volume_ids"
 				values = ["${outscale_volume.example.id}"]
 			}
-		}	
+		}
+	`, region)
+}
+
+func testAccCheckOutscaleOAPIVolumeDataSourceConfigFilterByTags(region string) string {
+	return fmt.Sprintf(`
+		resource "outscale_volume" "example" {
+			subregion_name = "%sa"
+			volume_type    = "gp2"
+			size           = 10
+
+			tags {
+				key   = "Name"
+				value = "volume-io1-2"
+			}
+		}
+
+		data "outscale_volume" "ebs_volume" {
+			filter {
+				name   = "tags"
+				values = ["Name=volume-io1-2"]
+			}
+
+			filter {
+				name   = "volume_ids"
+				values = ["${outscale_volume.example.id}"]
+			}
+
+		}
 	`, region)
 }
