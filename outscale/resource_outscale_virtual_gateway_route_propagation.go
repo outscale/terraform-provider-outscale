c1ba6dfae6f89dac23872f20df62ffc498a2a18d
Edgar López <edgars.ealo@gmail.com>
2020-03-12 12:27:18 -0600

    added files of virtual gateway route propagation

---
+++ outscale/resource_outscale_virtual_gateway_route_propagation.go
@@ -0,0 +1,157 @@
+package outscale
+
+import (
+	"context"
+	"fmt"
+	"github.com/antihax/optional"
+	oscgo "github.com/marinsalinas/osc-sdk-go"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceOutscaleOAPIVirtualGatewayRoutePropagation() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceOutscaleOAPIVpnGatewayRoutePropagationEnable,
+		Read:   resourceOutscaleOAPIVpnGatewayRoutePropagationRead,
+		Delete: resourceOutscaleOAPIVpnGatewayRoutePropagationDisable,
+
+		Schema: map[string]*schema.Schema{
+			"virtual_gateway_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"route_table_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"enable": &schema.Schema{
+				Type:     schema.TypeBool,
+				Required: true,
+				ForceNew: true,
+			},
+			"request_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceOutscaleOAPIVpnGatewayRoutePropagationEnable(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*OutscaleClient).OSCAPI
+
+	gwID := d.Get("virtual_gateway_id").(string)
+	rtID := d.Get("route_table_id").(string)
+	enable := d.Get("enable").(bool)
+
+	log.Printf("\n\n[INFO] Enabling virtual gateway route propagation from %s to %s", gwID, rtID)
+
+	var err error
+	var resp oscgo.UpdateRoutePropagationResponse
+
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		resp, _, err = conn.VirtualGatewayApi.UpdateRoutePropagation(context.Background(), &oscgo.UpdateRoutePropagationOpts{UpdateRoutePropagationRequest: optional.NewInterface(oscgo.UpdateRoutePropagationRequest{
+			VirtualGatewayId: gwID,
+			RouteTableId:     rtID,
+			Enable:           enable,
+		})})
+		if err != nil {
+			if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("error enabling VGW propagation: %s", err)
+	}
+
+	d.SetId(fmt.Sprintf("%s_%s", gwID, rtID))
+	d.Set("request_id", resp.ResponseContext.GetRequestId())
+
+	return nil
+}
+
+func resourceOutscaleOAPIVpnGatewayRoutePropagationDisable(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*OutscaleClient).OSCAPI
+
+	gwID := d.Get("virtual_gateway_id").(string)
+	rtID := d.Get("route_table_id").(string)
+	enable := d.Get("enable").(bool)
+
+	log.Printf("\n\n[INFO] Disabling VGW propagation from %s to %s", gwID, rtID)
+
+	var err error
+
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		_, _, err = conn.VirtualGatewayApi.UpdateRoutePropagation(context.Background(), &oscgo.UpdateRoutePropagationOpts{UpdateRoutePropagationRequest: optional.NewInterface(oscgo.UpdateRoutePropagationRequest{
+			VirtualGatewayId: gwID,
+			RouteTableId:     rtID,
+			Enable:           enable,
+		})})
+		if err != nil {
+			if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("error disabling VGW propagation: %s", err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func resourceOutscaleOAPIVpnGatewayRoutePropagationRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*OutscaleClient).OSCAPI
+
+	gwID := d.Get("virtual_gateway_id").(string)
+	rtID := d.Get("route_table_id").(string)
+
+	var resp oscgo.ReadRouteTablesResponse
+	var err error
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		resp, _, err = conn.RouteTableApi.ReadRouteTables(context.Background(), &oscgo.ReadRouteTablesOpts{ReadRouteTablesRequest: optional.NewInterface(oscgo.ReadRouteTablesRequest{
+			Filters: &oscgo.FiltersRouteTable{RouteTableIds: &[]string{rtID}},
+		})})
+		if err != nil {
+			if strings.Contains(fmt.Sprint(err), "RequestLimitExceeded") {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	rt := resp.GetRouteTables()[0]
+
+	exists := false
+	for _, vgw := range rt.GetRoutePropagatingVirtualGateways() {
+		if vgw.GetVirtualGatewayId() == gwID {
+			exists = true
+		}
+	}
+	if !exists {
+		log.Printf("\n\n[INFO] %s is no longer propagating to %s, so dropping route propagation from state", rtID, gwID)
+		d.SetId("")
+		return nil
+	}
+
+	return nil
+}
---
+++ outscale/resource_outscale_virtual_gateway_route_propagation_test.go
@@ -0,0 +1,148 @@
+package outscale
+
+import (
+	"context"
+	"fmt"
+	"github.com/antihax/optional"
+	oscgo "github.com/marinsalinas/osc-sdk-go"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+)
+
+func TestAccOutscaleOAPIVirtualRoutePropagation_basic(t *testing.T) {
+	rBgpAsn := acctest.RandIntRange(64512, 65534)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckOAPIVirtualRoutePropagationDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccOutscaleOAPIVpnRoutePropagationConfig(rBgpAsn),
+				Check: resource.ComposeTestCheckFunc(
+					testAccOutscaleOAPIVpnRoutePropagation(
+						"outscale_virtual_gateway_route_propagation.outscale_virtual_gateway_route_propagation",
+					),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckOAPIVirtualRoutePropagationDestroy(s *terraform.State) error {
+	oscapi := testAccProvider.Meta().(*OutscaleClient).OSCAPI
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "outscale_virtual_gateway_route_propagation" {
+			continue
+		}
+
+		var resp oscgo.ReadVirtualGatewaysResponse
+		var err error
+
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			resp, _, err = oscapi.VirtualGatewayApi.ReadVirtualGateways(context.Background(), &oscgo.ReadVirtualGatewaysOpts{ReadVirtualGatewaysRequest: optional.NewInterface(oscgo.ReadVirtualGatewaysRequest{
+				Filters: &oscgo.FiltersVirtualGateway{VirtualGatewayIds: &[]string{rs.Primary.Attributes["gateway_id"]}},
+			})})
+			if err != nil {
+				if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+					return resource.RetryableError(err)
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if len(resp.GetVirtualGateways()) > 0 {
+			err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+				_, _, err := oscapi.VirtualGatewayApi.DeleteVirtualGateway(context.Background(), &oscgo.DeleteVirtualGatewayOpts{DeleteVirtualGatewayRequest: optional.NewInterface(&oscgo.DeleteVirtualGatewayRequest{
+					VirtualGatewayId: resp.GetVirtualGateways()[0].GetVirtualGatewayId(),
+				})})
+				if err == nil {
+					return nil
+				}
+
+				ec2err, ok := err.(awserr.Error)
+				if !ok {
+					return resource.RetryableError(err)
+				}
+
+				switch ec2err.Code() {
+				case "InvalidVirtualGatewayID.NotFound":
+					return nil
+				case "IncorrectState":
+					return resource.RetryableError(err)
+				}
+
+				return resource.NonRetryableError(err)
+			})
+
+			if err != nil {
+				return fmt.Errorf("ERROR => %s", err)
+			}
+
+		} else {
+			return nil
+		}
+	}
+
+	return nil
+}
+
+func testAccOutscaleOAPIVpnRoutePropagation(routeProp string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[routeProp]
+		if !ok {
+			return fmt.Errorf("Not found: %s", routeProp)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		return nil
+	}
+}
+
+func testAccOutscaleOAPIVpnRoutePropagationConfig(rBgpAsn int) string {
+	return fmt.Sprintf(`
+		resource "outscale_virtual_gateway" "outscale_virtual_gateway" {
+ connection_type = "ipsec.1"
+}
+
+resource "outscale_net" "outscale_net" {
+    ip_range = "10.0.0.0/18"
+}
+resource "outscale_virtual_gateway_link" "outscale_virtual_gateway_link" {
+    virtual_gateway_id = outscale_virtual_gateway.outscale_virtual_gateway.virtual_gateway_id
+    net_id              = outscale_net.outscale_net.net_id
+}
+
+resource "outscale_route_table" "outscale_route_table" {
+    net_id = outscale_net.outscale_net.net_id
+    tags {
+     key = "name"
+     value = "terraform-RT"
+    }
+}
+
+resource "outscale_virtual_gateway_route_propagation" "outscale_virtual_gateway_route_propagation" {
+virtual_gateway_id = outscale_virtual_gateway_link.outscale_virtual_gateway_link.virtual_gateway_id
+    route_table_id  = outscale_route_table.outscale_route_table.route_table_id
+    enable = true 
+}	
+	`)
+}
