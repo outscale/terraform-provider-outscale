9f1e142815fb2220699560322ffe1ecb3da82410
PacoDw <__pm@outlook.com>
2020-03-18 16:27:41 -0600

    chore: added dhcp_option plural data source and test file

---
+++ outscale/data_source_outscale_dhcp_options.go
@@ -0,0 +1,135 @@
+package outscale
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/antihax/optional"
+	oscgo "github.com/marinsalinas/osc-sdk-go"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceOutscaleDHCPOptions() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceOutscaleDHCPOptionsRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": dataSourceFiltersSchema(),
+			"dhcp_options_set_ids": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"dhcp_options": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"domain_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"domain_name_servers": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"ntp_servers": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"default": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"dhcp_options_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"tags": tagsOAPIListSchemaComputed(),
+					},
+				},
+			},
+			"request_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceOutscaleDHCPOptionsRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*OutscaleClient).OSCAPI
+
+	filters, filtersOk := d.GetOk("filter")
+	dhcpIDs, dhcpIDOk := d.GetOk("dhcp_options_set_ids")
+	if !dhcpIDOk && !filtersOk {
+		return fmt.Errorf("One of filters, or dhcp_options_set_id must be provided")
+	}
+
+	params := oscgo.ReadDhcpOptionsRequest{}
+	if dhcpIDOk {
+		params.Filters = &oscgo.FiltersDhcpOptions{
+			DhcpOptionsSetIds: expandStringValueListPointer(dhcpIDs.([]interface{})),
+		}
+	}
+	if filtersOk {
+		params.Filters = buildOutscaleDataSourceDHCPOptionFilters(filters.(*schema.Set))
+	}
+
+	var resp oscgo.ReadDhcpOptionsResponse
+	var err error
+	err = resource.Retry(120*time.Second, func() *resource.RetryError {
+		resp, _, err = conn.DhcpOptionApi.ReadDhcpOptions(context.Background(), &oscgo.ReadDhcpOptionsOpts{
+			ReadDhcpOptionsRequest: optional.NewInterface(params),
+		})
+		if err != nil {
+			if strings.Contains(err.Error(), "RequestLimitExceeded:") {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	if len(resp.GetDhcpOptionsSets()) == 0 {
+		return fmt.Errorf("Unable to find DHCP Option")
+	}
+
+	if err := d.Set("dhcp_options", flattenDHCPOption(resp.GetDhcpOptionsSets())); err != nil {
+		return err
+	}
+
+	d.SetId(resource.UniqueId())
+
+	return nil
+}
+
+func flattenDHCPOption(dhcpOptions []oscgo.DhcpOptionsSet) []map[string]interface{} {
+	dhcpOptionsMap := make([]map[string]interface{}, len(dhcpOptions))
+
+	for i, dhcpOption := range dhcpOptions {
+		dhcpOptionsMap[i] = map[string]interface{}{
+			"domain_name":         dhcpOption.GetDomainName(),
+			"domain_name_servers": dhcpOption.GetDomainNameServers(),
+			"ntp_servers":         dhcpOption.GetNtpServers(),
+			"default":             dhcpOption.GetDefault(),
+			"dhcp_options_name":   dhcpOption.GetDhcpOptionsName(),
+			"tags":                tagsOSCAPIToMap(dhcpOption.GetTags()),
+		}
+	}
+	return dhcpOptionsMap
+}
