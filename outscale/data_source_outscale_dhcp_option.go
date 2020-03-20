0d1cec25825ed336fdbd649ee7da3b90dfe05742
PacoDw <__pm@outlook.com>
2020-03-20 12:42:03 -0600

    fix: added missing filters

--- outscale/data_source_outscale_dhcp_option.go
+++ outscale/data_source_outscale_dhcp_option.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/antihax/optional"
 	oscgo "github.com/marinsalinas/osc-sdk-go"
+	"github.com/spf13/cast"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
@@ -145,11 +146,19 @@ func buildOutscaleDataSourceDHCPOptionFilters(set *schema.Set) *oscgo.FiltersDhc
 		switch name := m["name"].(string); name {
 		case "dhcp_options_set_ids":
 			filters.SetDhcpOptionsSetIds(filterValues)
+		case "dhcp_options_set_id":
+			filters.SetDhcpOptionsSetIds(filterValues)
 		case "domain_name_servers":
+			filters.SetDomainNameServers(filterValues)
+		case "domain_name_server":
 			filters.SetDomainNameServers(filterValues)
 		case "domain_names":
 			filters.SetDomainNames(filterValues)
+		case "domain_name":
+			filters.SetDomainNames(filterValues)
 		case "ntp_servers":
+			filters.SetNtpServers(filterValues)
+		case "ntp_server":
 			filters.SetNtpServers(filterValues)
 		case "tag_keys":
 			filters.SetTagKeys(filterValues)
@@ -157,6 +166,8 @@ func buildOutscaleDataSourceDHCPOptionFilters(set *schema.Set) *oscgo.FiltersDhc
 			filters.SetTagValues(filterValues)
 		case "tags":
 			filters.SetTags(filterValues)
+		case "default":
+			filters.SetDefault(cast.ToBool(filterValues[0]))
 		default:
 			log.Printf("[Debug] Unknown Filter Name: %s.", name)
 		}
