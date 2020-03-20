dd69b3aa79b039cc04e908a9a53af1c04b3a04d4
PacoDw <__pm@outlook.com>
2020-03-20 12:40:09 -0600

    fix: added missing request id attribute

--- outscale/data_source_outscale_dhcp_options.go
+++ outscale/data_source_outscale_dhcp_options.go
@@ -112,6 +112,9 @@ func dataSourceOutscaleDHCPOptionsRead(d *schema.ResourceData, meta interface{})
 	if err := d.Set("dhcp_options", flattenDHCPOption(resp.GetDhcpOptionsSets())); err != nil {
 		return err
 	}
+	if err := d.Set("request_id", resp.ResponseContext.GetRequestId()); err != nil {
+		return err
+	}
 
 	d.SetId(resource.UniqueId())
 
