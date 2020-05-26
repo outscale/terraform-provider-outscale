50d6a2b8733033f9c86f3c932c38d5c60ce02775
PacoDw <__pm@outlook.com>
2020-05-26 09:16:32 -0500

    fix: refactored security group  test and resource file

--- outscale/resource_outscale_security_group_test.go
+++ outscale/resource_outscale_security_group_test.go
@@ -1,13 +1,9 @@
 package outscale
 
 import (
-	"context"
 	"fmt"
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/antihax/optional"
 	oscgo "github.com/marinsalinas/osc-sdk-go"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
@@ -44,59 +40,18 @@ func testAccCheckOutscaleOAPISGRuleDestroy(s *terraform.State) error {
 			continue
 		}
 
-		// Retrieve our group
-		req := oscgo.ReadSecurityGroupsRequest{
-			Filters: &oscgo.FiltersSecurityGroup{
-				SecurityGroupIds: &[]string{rs.Primary.ID},
-			},
+		_, resp, err := readSecurityGroups(conn, rs.Primary.ID)
+		if err == nil || len(resp.GetSecurityGroups()) > 0 {
+			return fmt.Errorf("Outscale Security Group(%s) still exists", rs.Primary.ID)
 		}
-
-		var resp oscgo.ReadSecurityGroupsResponse
-		var err error
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			resp, _, err = conn.SecurityGroupApi.ReadSecurityGroups(context.Background(), &oscgo.ReadSecurityGroupsOpts{ReadSecurityGroupsRequest: optional.NewInterface(req)})
-
-			if err != nil {
-				if strings.Contains(err.Error(), "RequestLimitExceeded") {
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
-			}
-
-			return nil
-		})
-
-		var errString string
-
-		if err != nil {
-			if strings.Contains(err.Error(), "InvalidGroup.NotFound") {
-				return err
-			}
-			//fmt.Printf("\n\nError on SGStateRefresh: %s", err)
-			errString = err.Error()
-
-			return fmt.Errorf("Error on SGStateRefresh: %s", errString)
-		}
-		if err == nil {
-			if len(resp.GetSecurityGroups()) > 0 && resp.GetSecurityGroups()[0].GetSecurityGroupId() == rs.Primary.ID {
-				return fmt.Errorf("Security Group (%s) still exists", rs.Primary.ID)
-			}
-
-			return nil
-		}
-
-		if resp.GetSecurityGroups() == nil {
-			return nil
-		}
-
-		return err
 	}
-
 	return nil
 }
 
 func testAccCheckOutscaleOAPISecurityGroupRuleExists(n string, group *oscgo.SecurityGroup) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
+		conn := testAccProvider.Meta().(*OutscaleClient).OSCAPI
+
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
 			return fmt.Errorf("Not found: %s", n)
@@ -106,52 +61,11 @@ func testAccCheckOutscaleOAPISecurityGroupRuleExists(n string, group *oscgo.Secu
 			return fmt.Errorf("No Security Group is set")
 		}
 
-		conn := testAccProvider.Meta().(*OutscaleClient).OSCAPI
-		req := oscgo.ReadSecurityGroupsRequest{
-			Filters: &oscgo.FiltersSecurityGroup{
-				SecurityGroupIds: &[]string{rs.Primary.ID},
-			},
+		_, resp, err := readSecurityGroups(conn, rs.Primary.ID)
+		if err != nil || len(resp.GetSecurityGroups()) < 1 {
+			return fmt.Errorf("Outscale Security Group(%s) does not exists: %s", rs.Primary.ID, err)
 		}
-
-		var resp oscgo.ReadSecurityGroupsResponse
-		var err error
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			resp, _, err = conn.SecurityGroupApi.ReadSecurityGroups(context.Background(), &oscgo.ReadSecurityGroupsOpts{ReadSecurityGroupsRequest: optional.NewInterface(req)})
-
-			if err != nil {
-				if strings.Contains(err.Error(), "RequestLimitExceeded") {
-					return resource.RetryableError(err)
-				}
-				return resource.NonRetryableError(err)
-			}
-
-			return nil
-		})
-
-		if err != nil {
-			return err
-		}
-
-		var errString string
-
-		if err != nil {
-			if strings.Contains(fmt.Sprint(err), "InvalidSecurityGroupID.NotFound") ||
-				strings.Contains(fmt.Sprint(err), "InvalidGroup.NotFound") {
-				err = nil
-			} else {
-				//fmt.Printf("\n\nError on SGStateRefresh: %s", err)
-				errString = err.Error()
-			}
-
-			return fmt.Errorf("Error on SGStateRefresh: %s", errString)
-		}
-
-		if len(resp.GetSecurityGroups()) > 0 && resp.GetSecurityGroups()[0].GetSecurityGroupId() == rs.Primary.ID {
-			*group = resp.GetSecurityGroups()[0]
-			return nil
-		}
-
-		return fmt.Errorf("Security Group not found")
+		return nil
 	}
 }
 
@@ -165,17 +79,17 @@ func testAccOutscaleOAPISecurityGroupConfig(rInt int) string {
 				value = "testacc-security-group-rs"
 			}
 		}
-		
+
 		resource "outscale_security_group" "web" {
 			security_group_name = "terraform_test_%d"
 			description         = "Used in the terraform acceptance tests"
-		
+
 			tags {
 				key   = "Name"
 				value = "tf-acc-test"
 			}
-		
+
 			net_id = "${outscale_net.net.id}"
-		}	
+		}
 	`, rInt)
 }
