3a749ff6001a8be1333c1c2cd016b1056cdf16d5
PacoDw <__pm@outlook.com>
2020-06-09 11:47:10 -0500

    feat: added access key plural datasource and test file

---
+++ outscale/data_source_outscale_access_keys_test.go
@@ -0,0 +1,66 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccOutscaleDataSourceAccessKeys_basic(t *testing.T) {
+	dataSourceName := "data.outscale_access_keys.outscale_access_key"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccClientAccessKeysDataSourceBasic(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "access_keys.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "access_key_ids.#"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccOutscaleDataSourceAccessKeys_withFilters(t *testing.T) {
+	dataSourceName := "data.outscale_access_keys.outscale_access_key"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccClientAccessKeysDataSourceWithFilters(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "access_keys.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "filter.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccClientAccessKeysDataSourceBasic() string {
+	return `
+		resource "outscale_access_key" "outscale_access_key" {}
+
+		data "outscale_access_keys" "outscale_access_key" {
+			access_key_ids = ["${outscale_access_key.outscale_access_key.id}"]
+		}
+	`
+}
+
+func testAccClientAccessKeysDataSourceWithFilters() string {
+	return `
+		resource "outscale_access_key" "outscale_access_key" {}
+
+		data "outscale_access_keys" "outscale_access_key" {
+			filter {
+				name = "access_key_ids"
+				values = ["${outscale_access_key.outscale_access_key.id}"]
+			}
+		}
+	`
+}
