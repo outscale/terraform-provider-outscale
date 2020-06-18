d5148706f5f966c8b6b3732cba9ef05f87db9cb1
matthias.gatto <matthias.gatto@outscale.com>
2020-06-18 15:26:51 +0200

    Revert "LBU-remove: rm data_source_outscale_load_balancer_listener_description"
    This reverts commit 867c943dec844b5569a6b4502611df53594c8ed8.

---
+++ outscale/data_source_outscale_load_balancer_listener_description.go
@@ -0,0 +1,130 @@
+package outscale
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/lbu"
+)
+
+func dataSourceOutscaleOAPILoadBalancerLD() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceOutscaleOAPILoadBalancerLDRead,
+
+		Schema: map[string]*schema.Schema{
+			"load_balancer_name": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"listener": &schema.Schema{
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"backend_port": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"backend_protocol": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"load_balancer_port": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"load_balancer_protocol": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"server_certificate_id": &schema.Schema{
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"policy_name": &schema.Schema{
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"request_id": &schema.Schema{
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceOutscaleOAPILoadBalancerLDRead(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*OutscaleClient).LBU
+	ename, ok := d.GetOk("load_balancer_name")
+
+	if !ok {
+		return fmt.Errorf("please provide the name of the load balancer")
+	}
+
+	elbName := ename.(string)
+
+	// Retrieve the ELB properties for updating the state
+	describeElbOpts := &lbu.DescribeLoadBalancersInput{
+		LoadBalancerNames: []*string{aws.String(elbName)},
+	}
+	var resp *lbu.DescribeLoadBalancersOutput
+	var describeResp *lbu.DescribeLoadBalancersResult
+	var err error
+	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+		resp, err = conn.API.DescribeLoadBalancers(describeElbOpts)
+		if err != nil {
+			if strings.Contains(fmt.Sprint(err), "Throttling:") {
+				return resource.RetryableError(err)
+			}
+			return resource.NonRetryableError(err)
+		}
+		describeResp = resp.DescribeLoadBalancersResult
+		return nil
+	})
+
+	if err != nil {
+		if isLoadBalancerNotFound(err) {
+			d.SetId("")
+			return nil
+		}
+
+		return fmt.Errorf("Error retrieving ELB: %s", err)
+	}
+
+	if describeResp.LoadBalancerDescriptions == nil {
+		return fmt.Errorf("NO ELB FOUND")
+	}
+
+	if len(describeResp.LoadBalancerDescriptions) != 1 {
+		return fmt.Errorf("Unable to find ELB: %#v", describeResp.LoadBalancerDescriptions)
+	}
+
+	lb := describeResp.LoadBalancerDescriptions[0]
+
+	v := lb.ListenerDescriptions[0]
+
+	l := make(map[string]interface{})
+	l["backend_port"] = strconv.Itoa(int(aws.Int64Value(v.Listener.InstancePort)))
+	l["backend_protocol"] = aws.StringValue(v.Listener.InstanceProtocol)
+	l["load_balancer_port"] = strconv.Itoa(int(aws.Int64Value(v.Listener.LoadBalancerPort)))
+	l["load_balancer_protocol"] = aws.StringValue(v.Listener.Protocol)
+	l["server_certificate_id"] = aws.StringValue(v.Listener.SSLCertificateId)
+
+	if err := d.Set("listener", l); err != nil {
+		return err
+	}
+
+	d.Set("request_id", resp.ResponseMetadata.RequestID)
+	d.SetId(resource.UniqueId())
+
+	return d.Set("policy_name", flattenStringList(lb.ListenerDescriptions[0].PolicyNames))
+}
---
+++ outscale/data_source_outscale_load_balancer_listener_description_test.go
@@ -0,0 +1,54 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/lbu"
+)
+
+func TestAccOutscaleOAPIDSLBUListenerDesc_basic(t *testing.T) {
+	t.Skip()
+
+	var conf lbu.LoadBalancerDescription
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			skipIfNoOAPI(t)
+			testAccPreCheck(t)
+		},
+		IDRefreshName: "outscale_load_balancer.bar",
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckOutscaleOAPILBUDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDSOutscaleOAPILBUListenerDescConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleOAPILBUExists("outscale_load_balancer.bar", &conf),
+					resource.TestCheckResourceAttr("data.outscale_load_balancer_listener_description.test", "listener.backend_port", "8000"),
+				)},
+		},
+	})
+}
+
+const testAccDSOutscaleOAPILBUListenerDescConfig = `
+	resource "outscale_load_balancer" "bar" {
+		availability_zones = ["eu-west-2a"]
+		load_balancer_name = "foobar-terraform-elb"
+
+		listeners {
+			backend_port           = 8000
+			backend_protocol       = "HTTP"
+			load_balancer_port     = 80
+			load_balancer_protocol = "HTTP"
+		}
+
+		tag {
+			bar = "baz"
+		}
+	}
+
+	data "outscale_load_balancer_listener_description" "test" {
+		load_balancer_name = "${outscale_load_balancer.bar.id}"
+	}
+`
