9229dd34a3b1edf42bb4bfec86547d2ffc38a407
matthias.gatto <matthias.gatto@outscale.com>
2020-06-22 14:40:55 +0200

    Revert "LBU-remove: rm data_source_outscale_load_balancer"
    This reverts commit 7030ff751cfcf65858a3bc6b1e7abdb25bf0841e.

---
+++ outscale/data_source_outscale_load_balancer_test.go
@@ -0,0 +1,58 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/terraform-providers/terraform-provider-outscale/osc/lbu"
+)
+
+func TestAccOutscaleOAPIDSLBU_basic(t *testing.T) {
+	t.Skip()
+
+	var conf lbu.LoadBalancerDescription
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			skipIfNoOAPI(t)
+			testAccPreCheck(t)
+		},
+		IDRefreshName: "outscale_load_balancer.bar",
+		Providers:     testAccProviders,
+		CheckDestroy:  testAccCheckOutscaleOAPILBUDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDSOutscaleOAPILBUConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckOutscaleOAPILBUExists("outscale_load_balancer.bar", &conf),
+					testAccCheckOutscaleOAPILBUAttributes(&conf),
+					resource.TestCheckResourceAttr(
+						"data.outscale_load_balancer.test", "sub_region_name.#", "1"),
+					resource.TestCheckResourceAttr(
+						"data.outscale_load_balancer.test", "sub_region_name.0", "eu-west-2a"),
+				)},
+		},
+	})
+}
+
+const testAccDSOutscaleOAPILBUConfig = `
+	resource "outscale_load_balancer" "bar" {
+		sub_region_name    = ["eu-west-2a"]
+		load_balancer_name = "foobar-terraform-elb"
+
+		listener {
+			backend_port           = 8000
+			backend_protocol       = "HTTP"
+			load_balancer_protocol = 80
+			protocol               = "HTTP"
+		}
+
+		tag {
+			bar = "baz"
+		}
+	}
+
+	data "outscale_load_balancer" "test" {
+		load_balancer_name = "${outscale_load_balancer.bar.id}"
+	}
+`
