64a4d3c751899708067a7685924ca40c67bddecb
meriem.zouari <meriem.zouari@outscale.com>
2020-06-22 10:58:56 +0000

    Update config.tf

--- examples/config.tf
+++ examples/config.tf
@@ -1,5 +1,5 @@
 provider "outscale" {
-    access_key_id = "${var.access_key_id}"
-    secret_key_id = "${var.secret_key_id}"
+    access_key_id = var.access_key_id
+    secret_key_id = var.secret_key_id
     region     = "eu-west-2"
 }
