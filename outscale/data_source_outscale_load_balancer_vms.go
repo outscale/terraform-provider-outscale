db730b24cff45c59d045270e1f676bc6e0a36a4a
Matthias Gatto <matthias.gatto@outscale.com>
2020-09-10 16:11:20 +0200

    nuke refacto: data_source_outscale_load_balancer_vms.go
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- outscale/data_source_outscale_load_balancer_vms.go
+++ outscale/data_source_outscale_load_balancer_vms.go
@@ -1,22 +1,13 @@
 package outscale
 
 import (
-	"context"
-	"fmt"
-	"strings"
-	"time"
-
-	"github.com/antihax/optional"
-	oscgo "github.com/marinsalinas/osc-sdk-go"
-
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceOutscaleLoadBalancerVms() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceOutscaleLoadBalancerVmsRead,
-		Schema: map[string]*schema.Schema{
+		Schema: getDataSourceSchemas(map[string]*schema.Schema{
 			"load_balancer_name": {
 				Type:     schema.TypeString,
 				ForceNew: true,
@@ -29,7 +20,7 @@ func dataSourceOutscaleLoadBalancerVms() *schema.Resource {
 				Required: true,
 				Elem:     &schema.Schema{Type: schema.TypeString},
 			},
-		},
+		}),
 	}
 }
 
@@ -37,48 +28,10 @@ func dataSourceOutscaleLoadBalancerVmsRead(d *schema.ResourceData,
 	meta interface{}) error {
 	conn := meta.(*OutscaleClient).OSCAPI
 
-	e := d.Get("load_balancer_name").(string)
-	expected := d.Get("backend_vm_ids").([]interface{})
-
-	filter := &oscgo.FiltersLoadBalancer{
-		LoadBalancerNames: &[]string{e},
-	}
-
-	req := oscgo.ReadLoadBalancersRequest{
-		Filters: filter,
-	}
-
-	describeElbOpts := &oscgo.ReadLoadBalancersOpts{
-		ReadLoadBalancersRequest: optional.NewInterface(req),
-	}
-
-	var resp oscgo.ReadLoadBalancersResponse
-	var err error
-	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		resp, _, err = conn.LoadBalancerApi.ReadLoadBalancers(
-			context.Background(),
-			describeElbOpts)
-
-		if err != nil {
-			if strings.Contains(fmt.Sprint(err), "Throttling") {
-				return resource.RetryableError(
-					fmt.Errorf("[WARN] Error, retrying: %s", err))
-			}
-			return resource.NonRetryableError(err)
-		}
-		return nil
-	})
-
+	lb, _, err := readLbs0(conn, d)
 	if err != nil {
-		return fmt.Errorf("Error retrieving LBU: %s", err)
+		return err
 	}
-
-	lbs := *resp.LoadBalancers
-	if len(lbs) != 1 {
-		return fmt.Errorf("Unable to find LBU: %s", expected)
-	}
-
-	lb := (lbs)[0]
 
 	d.Set("backend_vm_ids", flattenStringList(lb.BackendVmIds))
 
