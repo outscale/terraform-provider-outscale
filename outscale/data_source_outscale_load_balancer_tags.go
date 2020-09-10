da3e0ad257be1c1be9f8b5a32dd3dd6c67127657
Matthias Gatto <matthias.gatto@outscale.com>
2020-09-10 15:58:51 +0200

    nuke refacto: data_source_outscale_load_balancer_tags.go
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- outscale/data_source_outscale_load_balancer_tags.go
+++ outscale/data_source_outscale_load_balancer_tags.go
@@ -1,14 +1,6 @@
 package outscale
 
 import (
-	"context"
-	"fmt"
-	"strings"
-	"time"
-
-	"github.com/antihax/optional"
-	oscgo "github.com/marinsalinas/osc-sdk-go"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
@@ -17,55 +9,20 @@ func dataSourceOutscaleOAPILBUTags() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceOutscaleOAPILBUTagsRead,
 
-		Schema: getDSOAPILBUTagsSchema(),
+		Schema: getDataSourceSchemas(getDSOAPILBUTagsSchema()),
 	}
 }
 
 func dataSourceOutscaleOAPILBUTagsRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).OSCAPI
 
-	elbName := d.Get("load_balancer_names").(string)
-
-	filter := &oscgo.FiltersLoadBalancer{
-		LoadBalancerNames: &[]string{elbName},
-	}
-
-	req := oscgo.ReadLoadBalancersRequest{
-		Filters: filter,
-	}
-
-	describeElbOpts := &oscgo.ReadLoadBalancersOpts{
-		ReadLoadBalancersRequest: optional.NewInterface(req),
-	}
-
-	var resp oscgo.ReadLoadBalancersResponse
-	var err error
-	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		resp, _, err = conn.LoadBalancerApi.ReadLoadBalancers(
-			context.Background(),
-			describeElbOpts)
-
-		if err != nil {
-			if strings.Contains(fmt.Sprint(err), "Throttling:") {
-				return resource.RetryableError(err)
-			}
-			return resource.NonRetryableError(err)
-		}
-		return nil
-	})
-
+	v, resp, err := readLbs0(conn, d)
 	if err != nil {
 		return err
 	}
 
-	lbs := *resp.LoadBalancers
-	if len(lbs) != 1 {
-		return fmt.Errorf("Unable to find LBU: %s", elbName)
-	}
-
-	v := (lbs)[0]
 	t := make(map[string]interface{})
-	t["load_balancer_name"] = elbName
+	t["load_balancer_name"] = v.LoadBalancerName
 
 	ta := make([]map[string]interface{}, len(*v.Tags))
 	for k1, v1 := range *v.Tags {
