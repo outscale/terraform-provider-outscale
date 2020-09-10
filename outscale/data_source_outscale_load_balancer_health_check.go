16f2c06a1695a82847f5b37dd324122c95d3a35f
Matthias Gatto <matthias.gatto@outscale.com>
2020-09-10 15:22:30 +0200

    nuke refacto of data_source_outscale_load_balancer_health_check.go
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- outscale/data_source_outscale_load_balancer_health_check.go
+++ outscale/data_source_outscale_load_balancer_health_check.go
@@ -1,100 +1,53 @@
 package outscale
 
 import (
-	"context"
 	"fmt"
-	"strings"
-	"time"
 
-	"github.com/antihax/optional"
-	oscgo "github.com/marinsalinas/osc-sdk-go"
-
-	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 )
 
 func dataSourceOutscaleOAPILoadBalancerHealthCheck() *schema.Resource {
 	return &schema.Resource{
 		Read: dataSourceOutscaleOAPILoadBalancerHealthCheckRead,
-
-		Schema: map[string]*schema.Schema{
-			"load_balancer_name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"healthy_threshold": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"unhealthy_threshold": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"path": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"check_interval": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"timeout": {
-				Type:     schema.TypeInt,
-				Computed: true,
-			},
-			"request_id": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-		},
+		Schema: getDataSourceSchemas(
+			map[string]*schema.Schema{
+				"load_balancer_name": {
+					Type:     schema.TypeString,
+					Required: true,
+				},
+				"healthy_threshold": {
+					Type:     schema.TypeInt,
+					Computed: true,
+				},
+				"unhealthy_threshold": {
+					Type:     schema.TypeInt,
+					Computed: true,
+				},
+				"path": {
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+				"check_interval": {
+					Type:     schema.TypeInt,
+					Computed: true,
+				},
+				"timeout": {
+					Type:     schema.TypeInt,
+					Computed: true,
+				},
+				"request_id": {
+					Type:     schema.TypeString,
+					Computed: true,
+				},
+			}),
 	}
 }
 
 func dataSourceOutscaleOAPILoadBalancerHealthCheckRead(d *schema.ResourceData, meta interface{}) error {
 	conn := meta.(*OutscaleClient).OSCAPI
-	ename, ok := d.GetOk("load_balancer_name")
-
-	if !ok {
-		return fmt.Errorf("please provide the name of the load balancer")
-	}
-
-	elbName := ename.(string)
-
-	filter := &oscgo.FiltersLoadBalancer{
-		LoadBalancerNames: &[]string{elbName},
-	}
-
-	req := oscgo.ReadLoadBalancersRequest{
-		Filters: filter,
-	}
-
-	describeElbOpts := &oscgo.ReadLoadBalancersOpts{
-		ReadLoadBalancersRequest: optional.NewInterface(req),
-	}
-
-	var resp oscgo.ReadLoadBalancersResponse
-	var err error
-	err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-		resp, _, err = conn.LoadBalancerApi.ReadLoadBalancers(
-			context.Background(),
-			describeElbOpts)
-
-		if err != nil {
-			if strings.Contains(fmt.Sprint(err), "Throttling:") {
-				return resource.RetryableError(err)
-			}
-			return resource.NonRetryableError(err)
-		}
-		return nil
-	})
-
+	resp, elbName, err := readLbs(conn, d)
 	if err != nil {
-		if isLoadBalancerNotFound(err) {
-			d.SetId("")
-			return nil
-		}
-
-		return fmt.Errorf("Error retrieving ELB: %s", err)
+		return err
 	}
 
 	lbs := *resp.LoadBalancers
