0ba4df5d47bbdf804e9ca65dc41f41f1e9c20a68
Matthias Gatto <matthias.gatto@outscale.com>
2020-09-25 17:04:06 +0200

    resource_outscale_load_balancer_cookiepolicy.go -> resource_outscale_load_balancer_policy.go
    Signed-off-by: Matthias Gatto <matthias.gatto@outscale.com>

--- outscale/resource_outscale_load_balancer_policy_test.go
+++ outscale/resource_outscale_load_balancer_policy_test.go
@@ -1,46 +1,187 @@
 package outscale
 
 import (
+	"context"
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/antihax/optional"
+	oscgo "github.com/marinsalinas/osc-sdk-go"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func TestOutscaleOAPILoadBalancerPolicy_basic(t *testing.T) {
+func TestAccOutscaleAppCookieStickinessPolicy_basic(t *testing.T) {
+	lbName := fmt.Sprintf("tf-test-lb-%s", acctest.RandString(5))
+
 	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-		Providers: testAccProviders,
-		// CheckDestroy: testAccCheckOutscaleLoadBalancerDestroy,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAppCookieStickinessPolicyDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOutscaleOAPILoadBalancerPolicyPrefixNameConfig,
+				Config: testAccAppCookieStickinessPolicyConfig(lbName),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"outscale_load_balancer_policy.outscale_load_balancer_policy", "load_balancer_port", "25"),
+					testAccCheckAppCookieStickinessPolicy(
+						"outscale_load_balancer.lb",
+						"outscale_load_balancer_cookiepolicy.foo",
+					),
+				),
+			},
+			{
+				Config: testAccAppCookieStickinessPolicyConfigUpdate(lbName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppCookieStickinessPolicy(
+						"outscale_load_balancer.lb",
+						"outscale_load_balancer_cookiepolicy.foo",
+					),
 				),
 			},
 		},
 	})
 }
 
-const testAccOutscaleOAPILoadBalancerPolicyPrefixNameConfig = `
-resource "outscale_load_balancer" "outscale_load_balancer" {
-    load_balancer_name     = "foobar-terraform-elb"
-    subregion_names 	   = ["eu-west-2a"]
+func TestAccOutscaleAppCookieStickinessPolicy_missingLB(t *testing.T) {
+	lbName := fmt.Sprintf("tf-test-lb-%s", acctest.RandString(5))
 
-    listener {
-    	backend_port = 8000
-    	backend_protocol = "HTTP"
-    	load_balancer_port = 80
-    	load_balancer_protocol = "HTTP"
-    }
+	// check that we can destroy the policy if the LB is missing
+	removeLB := func() {
+		conn := testAccProvider.Meta().(*OutscaleClient).OSCAPI
+
+		request := oscgo.DeleteLoadBalancerRequest{
+			LoadBalancerName: lbName,
+		}
+
+		deleteElbOpts := oscgo.DeleteLoadBalancerOpts{
+			optional.NewInterface(request),
+		}
+
+		var err error
+		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
+			_, _, err = conn.LoadBalancerApi.DeleteLoadBalancer(context.Background(), &deleteElbOpts)
+
+			if err != nil {
+				if strings.Contains(fmt.Sprint(err), "Throttling") {
+					return resource.RetryableError(
+						fmt.Errorf("[WARN] Error creating ELB Listener with SSL Cert, retrying: %s", err))
+				}
+				return resource.NonRetryableError(err)
+			}
+			return nil
+		})
+
+		if err != nil {
+			t.Fatalf("Error deleting ELB: %s", err)
+		}
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckAppCookieStickinessPolicyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccAppCookieStickinessPolicyConfig(lbName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckAppCookieStickinessPolicy(
+						"outscale_load_balancer.lb",
+						"outscale_load_balancer_cookiepolicy.foo",
+					),
+				),
+			},
+			{
+				PreConfig: removeLB,
+				Config:    testAccAppCookieStickinessPolicyConfigDestroy(lbName),
+			},
+		},
+	})
 }
 
-resource "outscale_load_balancer_policy" "outscale_load_balancer_policy" {
-    load_balancer_name = "${outscale_load_balancer.outscale_load_balancer.load_balancer_name}"
-    
-    load_balancer_port = "${outscale_load_balancer.outscale_load_balancer.listeners.0.load_balancer_port}"
+func testAccCheckAppCookieStickinessPolicyDestroy(s *terraform.State) error {
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "outscale_load_balancer_cookiepolicy" {
+			continue
+		}
+	}
+	return nil
 }
-`
+
+func testAccCheckAppCookieStickinessPolicy(elbResource string, policyResource string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[elbResource]
+		if !ok {
+			return fmt.Errorf("Not found: %s", elbResource)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		if !ok {
+			return fmt.Errorf("Not found: %s", policyResource)
+		}
+
+		return nil
+	}
+}
+
+func testAccAppCookieStickinessPolicyConfig(rName string) string {
+	return fmt.Sprintf(`
+resource "outscale_load_balancer" "lb" {
+	load_balancer_name = "%s"
+	 availability_zones = ["eu-west-2a"]
+  listeners {
+    instance_port = 8000
+    instance_protocol = "HTTP"
+    load_balancer_port = 80
+    protocol = "HTTP"
+  }
+}
+
+resource "outscale_load_balancer_cookiepolicy" "foo" {
+	policy_name = "foo-policy"
+	load_balancer_name = "${outscale_load_balancer.lb.id}"
+	cookie_name = "MyAppCookie"
+}`, rName)
+}
+
+// Change the cookie_name to "MyOtherAppCookie".
+func testAccAppCookieStickinessPolicyConfigUpdate(rName string) string {
+	return fmt.Sprintf(`
+resource "outscale_load_balancer" "lb" {
+	load_balancer_name = "%s"
+	 availability_zones = ["eu-west-2a"]
+  listeners {
+    instance_port = 8000
+    instance_protocol = "HTTP"
+    load_balancer_port = 80
+    protocol = "HTTP"
+  }
+}
+
+resource "outscale_load_balancer_cookiepolicy" "foo" {
+	policy_name = "foo-policy"
+	load_balancer_name = "${outscale_load_balancer.lb.id}"
+	cookie_name = "MyOtherAppCookie"
+}`, rName)
+}
+
+// attempt to destroy the policy, but we'll delete the LB in the PreConfig
+func testAccAppCookieStickinessPolicyConfigDestroy(rName string) string {
+	return fmt.Sprintf(`
+resource "outscale_load_balancer" "lb" {
+	load_balancer_name = "%s"
+	 availability_zones = ["eu-west-2a"]
+  listeners {
+    instance_port = 8000
+    instance_protocol = "HTTP"
+    load_balancer_port = 80
+    protocol = "HTTP"
+  }
+}`, rName)
+}
