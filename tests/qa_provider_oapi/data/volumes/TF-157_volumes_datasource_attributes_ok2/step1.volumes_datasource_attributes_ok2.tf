85ff450949bbd7f9d929a1b41771ef7d58c38a62
meriem.zouari <meriem.zouari@outscale.com>
2022-06-02 13:04:50 +0200

    remove gp2 volumes, update snapshot and FGPU catalog tests

--- tests/qa_provider_oapi/data/volumes/TF-157_volumes_datasource_attributes_ok2/step1.volumes_datasource_attributes_ok2.tf
+++ tests/qa_provider_oapi/data/volumes/TF-157_volumes_datasource_attributes_ok2/step1.volumes_datasource_attributes_ok2.tf
@@ -1,26 +1,27 @@
 resource "outscale_volume" "outscale_volume" {
-    subregion_name = "${var.region}b"
-    size            = 12
-    volume_type    = "gp2"
-    tags {
-        key = "Name"
-        value = "volume-gp2-1"
-    }
+  subregion_name = "${var.region}b"
+  size           = 12
+  volume_type    = "standard"
+  tags {
+    key   = "Name"
+    value = "volume-standard-1"
+  }
 
 }
 resource "outscale_volume" "outscale_volume2" {
-    subregion_name = "${var.region}a"
-    size            = 13
-    volume_type    = "gp2"
-    tags {
-        key = "Name"
-        value = "volume-gp2-1"
-    }
+  subregion_name = "${var.region}a"
+  size           = 13
+  volume_type    = "io1"
+  iops           = 120
+  tags {
+    key   = "Name"
+    value = "volume-io1-1"
+  }
 
 }
 data "outscale_volumes" "outscale_volumes" {
-    filter {
-        name   = "volume_ids"
-        values = [outscale_volume.outscale_volume.volume_id, outscale_volume.outscale_volume2.volume_id]
-    }
+  filter {
+    name   = "volume_ids"
+    values = [outscale_volume.outscale_volume.volume_id, outscale_volume.outscale_volume2.volume_id]
+  }
 }
