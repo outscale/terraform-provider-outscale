eb77b69a8cf16d2966be76ff2c0b81beb23c3b09
Thiery Ouattara <thiery.ouattara@outscale.com>
2022-08-15 20:40:37 +0000

    add API access policy

---
+++ outscale/data_source_outscale_api_access_policy_test.go
@@ -0,0 +1,29 @@
+package outscale
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourceOutscaleOAPIApiAccessPolicy_basic(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceOutscaleOAPIApiAccessPolicyConfig(),
+			},
+		},
+	})
+}
+
+func testAccDataSourceOutscaleOAPIApiAccessPolicyConfig() string {
+	return fmt.Sprintf(`
+              data "outscale_api_access_policy" "api_access_policy" {}
+	`)
+}
