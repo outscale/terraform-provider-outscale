a4851a57f3477c0b73b6fdd778afc81f9478cf32
Frederic Berot-Armand <frederic.berot-armand@outscale.com>
2022-10-27 08:32:41 +0000

    Add accounts datasource

--- outscale/provider.go
+++ outscale/provider.go
@@ -177,6 +177,7 @@ func Provider() terraform.ResourceProvider {
 			"outscale_api_access_rules":             dataSourceOutscaleOAPIApiAccessRules(),
 			"outscale_api_access_policy":            dataSourceOutscaleOAPIApiAccessPolicy(),
 			"outscale_public_catalog":               dataSourceOutscaleOAPIPublicCatalog(),
+			"outscale_accounts":                     dataSourceAccounts(),
 		},
 
 		ConfigureFunc: providerConfigureClient,
