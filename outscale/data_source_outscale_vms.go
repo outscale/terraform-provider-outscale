9423826efb46a02e90b0e5c285f5c0ee77fdae88
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2022-11-25 09:20:28 +0000

    Add schema.go

--- outscale/data_source_outscale_vms.go
+++ outscale/data_source_outscale_vms.go
@@ -21,28 +21,6 @@ func datasourceOutscaleOApiVMS() *schema.Resource {
 	}
 }
 
-func dataSourceFiltersSchema() *schema.Schema {
-	return &schema.Schema{
-		Type:     schema.TypeSet,
-		Optional: true,
-		ForceNew: true,
-		Elem: &schema.Resource{
-			Schema: map[string]*schema.Schema{
-				"name": {
-					Type:     schema.TypeString,
-					Required: true,
-				},
-
-				"values": {
-					Type:     schema.TypeList,
-					Required: true,
-					Elem:     &schema.Schema{Type: schema.TypeString},
-				},
-			},
-		},
-	}
-}
-
 func datasourceOutscaleOApiVMSSchema() map[string]*schema.Schema {
 	wholeSchema := map[string]*schema.Schema{
 		"filter": dataSourceFiltersSchema(),
