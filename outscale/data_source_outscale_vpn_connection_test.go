ef66368fbdf403fc9f43cff0fa9d1bbde4576d70
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:27:58 +0000

    Merge vpn_connection datasource testacc

--- outscale/data_source_outscale_vpn_connection_test.go
+++ outscale/data_source_outscale_vpn_connection_test.go
@@ -9,37 +9,28 @@ import (
 	"github.com/terraform-providers/terraform-provider-outscale/utils"
 )
 
-func TestAccOutscaleVPNConnectionDataSource_basic(t *testing.T) {
+func TestAcc_VPNConnection_DataSource(t *testing.T) {
 	t.Parallel()
 	publicIP := fmt.Sprintf("172.0.0.%d", utils.RandIntRange(1, 255))
+	dataSourceName := "data.outscale_vpn_connection.test"
+	dataSourcesName := "data.outscale_vpn_connections.test"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOutscaleVPNConnectionDataSourceConfigBasic(publicIP),
+				Config: testAcc_VPNConnection_DataSource_Config(publicIP),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "vpn_connection_id"),
+					resource.TestCheckResourceAttr(dataSourcesName, "vpn_connections.#", "1"),
+				),
 			},
 		},
 	})
 }
 
-func TestAccOutscaleVPNConnectionDataSource_withFilters(t *testing.T) {
-	t.Parallel()
-	publicIP := fmt.Sprintf("172.0.0.%d", utils.RandIntRange(1, 255))
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccOutscaleVPNConnectionDataSourceConfigWithFilters(publicIP),
-			},
-		},
-	})
-}
-
-func testAccOutscaleVPNConnectionDataSourceConfigBasic(publicIP string) string {
+func testAcc_VPNConnection_DataSource_Config(publicIP string) string {
 	return fmt.Sprintf(`
 		resource "outscale_virtual_gateway" "virtual_gateway" {
 			connection_type = "ipsec.1"
@@ -56,39 +47,16 @@ func testAccOutscaleVPNConnectionDataSourceConfigBasic(publicIP string) string {
 			virtual_gateway_id = "${outscale_virtual_gateway.virtual_gateway.id}"
 			connection_type    = "ipsec.1"
 			static_routes_only  = true
+		}
 
-			tags {
-        key   = "Name"
-        value = "test-VPN"
+		data "outscale_vpn_connection" "test" {
+			filter {
+				name = "vpn_connection_ids"
+				values = ["${outscale_vpn_connection.foo.id}"]
 			}
 		}
 
-		data "outscale_vpn_connection" "test" {
-			vpn_connection_id = "${outscale_vpn_connection.foo.id}"
-		}
-	`, publicIP)
-}
-
-func testAccOutscaleVPNConnectionDataSourceConfigWithFilters(publicIP string) string {
-	return fmt.Sprintf(`
-		resource "outscale_virtual_gateway" "virtual_gateway" {
-			connection_type = "ipsec.1"
-		}
-
-		resource "outscale_client_gateway" "customer_gateway" {
-			bgp_asn         = 3
-			public_ip       = "%s"
-			connection_type = "ipsec.1"
-		}
-
-		resource "outscale_vpn_connection" "foo" {
-			client_gateway_id  = "${outscale_client_gateway.customer_gateway.id}"
-			virtual_gateway_id = "${outscale_virtual_gateway.virtual_gateway.id}"
-			connection_type    = "ipsec.1"
-			static_routes_only  = true
-		}
-
-		data "outscale_vpn_connection" "test" {
+		data "outscale_vpn_connections" "test" {
 			filter {
 				name = "vpn_connection_ids"
 				values = ["${outscale_vpn_connection.foo.id}"]
