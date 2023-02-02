69dfd10c5e51a5cf8b532ae789c657c2fd419ea5
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 14:57:54 +0000

    Merge client_gateway datasource testacc

--- outscale/data_source_outscale_client_gateway_test.go
+++ outscale/data_source_outscale_client_gateway_test.go
@@ -9,8 +9,10 @@ import (
 	"github.com/terraform-providers/terraform-provider-outscale/utils"
 )
 
-func TestAccOutscaleGatewayDatasource_basic(t *testing.T) {
+func TestAcc_ClientGateway_Datasource(t *testing.T) {
 	t.Parallel()
+	dataSourceName := "data.outscale_client_gateway.test"
+	dataSourcesName := "data.outscale_client_gateways.test"
 	rBgpAsn := utils.RandIntRange(64512, 65534)
 	value := fmt.Sprintf("testacc-%s", acctest.RandString(5))
 
@@ -19,47 +21,24 @@ func TestAccOutscaleGatewayDatasource_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccClientGatewayDatasourceBasic(rBgpAsn, value),
+				Config: testAcc_ClientGateway_Datasource(rBgpAsn, value),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "client_gateway_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "bgp_asn"),
+
+					resource.TestCheckResourceAttrSet(dataSourcesName, "client_gateways.#"),
+					resource.TestCheckResourceAttrSet(dataSourcesName, "filter.#"),
+				),
 			},
 		},
 	})
 }
 
-func TestAccOutscaleGatewayDatasource_withFilters(t *testing.T) {
-	t.Parallel()
-	// datasourceName := "data.outscale_client_gateway.test"
-	rBgpAsn := utils.RandIntRange(64512, 65534)
-	value := fmt.Sprintf("testacc-%s", acctest.RandString(5))
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccClientGatewayDatasourceWithFilters(rBgpAsn, value),
-			},
-		},
-	})
-}
-
-func TestAccOutscaleGatewayDatasource_withFiltersNoLocalhost(t *testing.T) {
-	t.Parallel()
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccClientGatewayDatasourceWithFiltersNoLocalhost(),
-			},
-		},
-	})
-}
-
-func testAccClientGatewayDatasourceBasic(rBgpAsn int, value string) string {
+func testAcc_ClientGateway_Datasource(rBgpAsn int, value string) string {
 	return fmt.Sprintf(`
 		resource "outscale_client_gateway" "foo" {
 			bgp_asn         = %d
-			public_ip       = "172.0.0.1"
+			public_ip       = "171.33.75.123"
 			connection_type = "ipsec.1"
 
 			tags {
@@ -67,52 +46,25 @@ func testAccClientGatewayDatasourceBasic(rBgpAsn int, value string) string {
 				value = "%s"
 			}
 		}
-
-		data "outscale_client_gateway" "test" {
-			client_gateway_id = "${outscale_client_gateway.foo.id}"
-		}
-	`, rBgpAsn, value)
-}
-
-func testAccClientGatewayDatasourceWithFilters(rBgpAsn int, value string) string {
-	return fmt.Sprintf(`
-		resource "outscale_client_gateway" "foo" {
-			bgp_asn         = %d
+	
+		resource "outscale_client_gateway" "foo2" {
+			bgp_asn         = 4
 			public_ip       = "172.0.0.1"
 			connection_type = "ipsec.1"
-
-			tags {
-				key = "Name"
-				value = "%s"
+		}
+	
+		data "outscale_client_gateways" "test" {
+			filter {
+				name = "client_gateway_ids"
+				values = [outscale_client_gateway.foo.id, outscale_client_gateway.foo2.id]
 			}
 		}
-
+		
 		data "outscale_client_gateway" "test" {
 			filter {
 				name = "client_gateway_ids"
-				values = ["${outscale_client_gateway.foo.id}"]
+				values = [outscale_client_gateway.foo.id]
 			}
 		}
 	`, rBgpAsn, value)
-}
-
-func testAccClientGatewayDatasourceWithFiltersNoLocalhost() string {
-	return fmt.Sprintf(`
-	resource "outscale_client_gateway" "outscale_client_gateway" {
-		bgp_asn     = 571
-		public_ip  = "171.33.75.123"
-		connection_type        = "ipsec.1"
-		tags {
-		 key = "name-mzi"
-		 value = "CGW_1_mzi"
-		}
-	}
-	
-	data "outscale_client_gateway" "outscale_client_gateway_2" {
-		filter {
-		   name   = "client_gateway_ids"
-		   values = [outscale_client_gateway.outscale_client_gateway.client_gateway_id]
-		}
-	}
-	`)
 }
