15f4d0451d8488c2fc8adb05da3a2e02ca117682
akram.arab <akram.arab@outscale.com>
2023-02-02 17:03:15 +0100

    Update test TF-178 and ref files

--- tests/qa_provider_oapi/data/vm/TF-178_vm_resource_update_attributes_ok/step7.vm_resource_update_vm_type_ok.tf
+++ tests/qa_provider_oapi/data/vm/TF-178_vm_resource_update_attributes_ok/step7.vm_resource_update_vm_type_ok.tf
@@ -1,31 +1,28 @@
 resource "outscale_keypair" "keypair-TF66" {
-    count                = 2
-    keypair_name         = "keyname_TF66-${count.index}"
+  count        = 2
+  keypair_name = "keyname_TF66-${count.index}"
 }
 
 resource "outscale_security_group" "security_group_TF66" {
-     count               = 2
-     description         = "test-terraform-TF66"
-     security_group_name = "terraform-sg-TF66-${count.index}"
+  count               = 2
+  description         = "test-terraform-TF66"
+  security_group_name = "terraform-sg-TF66-${count.index}"
 }
 
 resource "outscale_vm" "outscale_vm-TF66" {
-    block_device_mappings {
-        device_name       = "/dev/sdb"
-        bsu  {
-            volume_size   = 20
-            volume_type   = "standard"
-            delete_on_vm_deletion = true
-         }
+  block_device_mappings {
+    device_name                  = "/dev/sdb"
+      bsu {
+        volume_size              = 20
+        volume_type              = "standard"
+        delete_on_vm_deletion    = true
       }
-
-    image_id            = var.image_id
-    vm_type             = "tinav4.c3r3"
-    performance         = "high"
-    deletion_protection = false
-    vm_initiated_shutdown_behavior  = "restart"
-    security_group_ids  = [outscale_security_group.security_group_TF66[0].security_group_id,outscale_security_group.security_group_TF66[1].security_group_id]
-    keypair_name        = outscale_keypair.keypair-TF66[1].keypair_name
-
+    }
+  image_id                       = var.image_id
+  vm_type                        = "tinav4.c3r3p2"
+  performance                    = "high"
+  deletion_protection            = false
+  vm_initiated_shutdown_behavior = "restart"
+  security_group_ids             = [outscale_security_group.security_group_TF66[0].security_group_id,outscale_security_group.security_group_TF66[1].security_group_id]
+  keypair_name                   = outscale_keypair.keypair-TF66[1].keypair_name
 }
-
