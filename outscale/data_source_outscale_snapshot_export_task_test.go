60be89cac1c0265fecc8fac470baf1b56620568a
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:27:02 +0000

    Merge snapshot_export_task datasource testacc

--- outscale/data_source_outscale_snapshot_export_task_test.go
+++ outscale/data_source_outscale_snapshot_export_task_test.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccOutscaleOAPISnapshotExportTaskDataSource_basic(t *testing.T) {
+func TestAcc_SnapshotExportTask_DataSource(t *testing.T) {
 	t.Parallel()
 	imageName := acctest.RandomWithPrefix("terraform-export-")
+	dataSourceName := "data.outscale_snapshot_export_task.export_task"
+	dataSourcesName := "data.outscale_snapshot_export_tasks.export_tasks"
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -20,35 +20,21 @@ func TestAccOutscaleOAPISnapshotExportTaskDataSource_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOutscaleOAPISnapshotExportTaskDataSourceConfig(imageName),
+				Config: testAcc_SnapshotExportTask_DataSource_Config(imageName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleSnapshotExportTaskDataSourceID("data.outscale_snapshot_export_task.export_task"),
+					resource.TestCheckResourceAttrSet(dataSourcesName, "snapshot_export_tasks.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "task_id"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckOutscaleSnapshotExportTaskDataSourceID(n string) resource.TestCheckFunc {
-	// Wait for IAM role
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("can't find Snapshot Export Task data source: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("Snapshot Export Task data source ID not set")
-		}
-		return nil
-	}
-}
-
-func testAccOutscaleOAPISnapshotExportTaskDataSourceConfig(testName string) string {
+func testAcc_SnapshotExportTask_DataSource_Config(testName string) string {
 	var stringTemplate = `
 		resource "outscale_volume" "outscale_volume_snap" {
 			subregion_name   = "eu-west-2a"
-			size                = 10
+			size             = 1
 		}
 
 		resource "outscale_snapshot" "outscale_snapshot" {
@@ -56,15 +42,22 @@ func testAccOutscaleOAPISnapshotExportTaskDataSourceConfig(testName string) stri
 		}
 
 		resource "outscale_snapshot_export_task" "outscale_snapshot_export_task" {
-			snapshot_id                     = outscale_snapshot.outscale_snapshot.snapshot_id
+			snapshot_id           = outscale_snapshot.outscale_snapshot.snapshot_id
 			osu_export {
 				disk_image_format = "qcow2"
 				osu_bucket        = "%s"
 				osu_prefix        = "new-export"
-				}
+			}
 		}
 
 		data "outscale_snapshot_export_task" "export_task" {
+			filter {
+				name = "task_ids"
+				values = [outscale_snapshot_export_task.outscale_snapshot_export_task.id]
+			}
+		}
+
+		data "outscale_snapshot_export_tasks" "export_tasks" {
 			filter {
 				name = "task_ids"
 				values = [outscale_snapshot_export_task.outscale_snapshot_export_task.id]
