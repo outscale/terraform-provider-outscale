222ded7454e2a51c99d8d4daae126c0db5ab0beb
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:23:33 +0000

    Merge flexible_gpu datasource testacc

--- outscale/data_source_outscale_flexible_gpu_test.go
+++ outscale/data_source_outscale_flexible_gpu_test.go
@@ -8,9 +8,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
-func TestAccDataSourceOutscaleOAPIFlexibleGpu_basic(t *testing.T) {
+func TestAcc_FlexibleGpu_DataSource(t *testing.T) {
 	t.Parallel()
 	region := fmt.Sprintf("%sa", os.Getenv("OUTSCALE_REGION"))
+	dataSourceName := "data.outscale_flexible_gpu.fgpu"
+	dataSourcesName := "data.outscale_flexible_gpus.fgpus"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -19,46 +21,77 @@ func TestAccDataSourceOutscaleOAPIFlexibleGpu_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceOutscaleOAPIFlexibleGpuConfig(region, region),
+				Config: testAcc_FlexibleGpu_DataSource_Config(region),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataSourceName, "flexible_gpu_id"),
+					resource.TestCheckResourceAttrSet(dataSourcesName, "flexible_gpus.#"),
+				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceOutscaleOAPIFlexibleGpuConfig(region, region1 string) string {
+func testAcc_FlexibleGpu_DataSource_Config(region string) string {
 	return fmt.Sprintf(`
-                resource "outscale_flexible_gpu" "fGPU-data1" {
-                        model_name             =  "nvidia-p6"
-                        generation             =  "v5"
-                        subregion_name         =  "%s"
-                        delete_on_vm_deletion  =   true
-                }
+	resource "outscale_flexible_gpu" "fGPU-1" {
+		model_name             =  "nvidia-p6"
+		generation             =  "v5"
+		subregion_name         =  "%[1]s"
+		delete_on_vm_deletion  =   true
+	}
 
-		data "outscale_flexible_gpu" "data_fGPU-1" {
-			filter {
-				name = "flexible_gpu_ids"
-				values = [outscale_flexible_gpu.fGPU-data1.flexible_gpu_id]
-			}
-                        filter {
-                                name = "delete_on_vm_deletion"
-                                values = [true]
-                        }
-                        filter {
-                                name = "generations"
-                                values = ["v5"]
-                        }
-                        filter {
-                                name = "states"
-                                values = ["allocated"]
-                        }
-                        filter {
-                                name = "model_names"
-                                values = ["nvidia-p6"]
-                        }
-	                filter {
-                                name = "subregion_names" 
-                                values = ["%s"]
-                        }
+	data "outscale_flexible_gpu" "fgpu" {
+		filter {
+			name = "flexible_gpu_ids"
+			values = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id]
 		}
-	`, region, region)
+		filter {
+			name = "delete_on_vm_deletion"
+			values = [true]
+		}
+		filter {
+			name = "generations"
+			values = ["v5"]
+		}
+		filter {
+			name = "states"
+			values = ["allocated"]
+		}
+		filter {
+			name = "model_names"
+			values = ["nvidia-p6"]
+		}
+		filter {
+			name = "subregion_names" 
+			values = ["%[1]s"]
+		}
+	}
+
+	data "outscale_flexible_gpus" "fgpus" {
+		filter {
+			name = "flexible_gpu_ids"
+			values = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id]
+		}
+		filter {
+			name = "delete_on_vm_deletion"
+			values = [true]
+		}
+		filter {
+			name = "generations"
+			values = ["v5"]
+		}
+		filter {
+			name = "states"
+			values = ["allocated"]
+		}
+		filter {
+			name = "model_names"
+			values = ["nvidia-p6"]
+		}
+		filter {
+			name = "subregion_names" 
+			values = ["%[1]s"]
+		}
+	}
+	`, region)
 }
