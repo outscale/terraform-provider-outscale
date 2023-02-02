3ad8cc6c21e4a35de41a22ae2eda4209428621de
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:25:42 +0000

    Merge nic datasource testacc

--- outscale/data_source_outscale_nic_test.go
+++ outscale/data_source_outscale_nic_test.go
@@ -1,142 +1,38 @@
 package outscale
 
 import (
-	"context"
 	"fmt"
 	"os"
-	"strings"
-
-	oscgo "github.com/outscale/osc-sdk-go/v2"
-	"github.com/terraform-providers/terraform-provider-outscale/utils"
 
 	"testing"
-	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccOutscaleOAPIENIDataSource_basic(t *testing.T) {
+func TestAcc_Nic_DataSource(t *testing.T) {
 	t.Parallel()
-	var conf oscgo.Nic
 	subregion := os.Getenv("OUTSCALE_REGION")
+	dataSourceName := "data.outscale_nic.nic"
+	dataSourcesName := "data.outscale_nics.nics"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:      func() { testAccPreCheck(t) },
 		IDRefreshName: "outscale_nic.outscale_nic",
 		Providers:     testAccProviders,
-		CheckDestroy:  testAccCheckOutscaleOAPIENIDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOutscaleOAPIENIDataSourceConfig(subregion),
+				Config: testAcc_Nic_DataSource_Config(subregion),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleOAPIENIExists("outscale_nic.outscale_nic", &conf),
-					testAccCheckOutscaleOAPIENIAttributes(&conf, subregion),
+					resource.TestCheckResourceAttr(dataSourcesName, "nics.#", "1"),
+
+					resource.TestCheckResourceAttrSet(dataSourceName, "nic_id"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccOutscaleOAPIENIDataSource_basicFilter(t *testing.T) {
-	t.Parallel()
-	var conf oscgo.Nic
-
-	subregion := os.Getenv("OUTSCALE_REGION")
-	if subregion == "" {
-		subregion = "in-west-2"
-	}
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:      func() { testAccPreCheck(t) },
-		IDRefreshName: "outscale_nic.outscale_nic",
-		Providers:     testAccProviders,
-		CheckDestroy:  testAccCheckOutscaleOAPIENIDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccOutscaleOAPIENIDataSourceConfigFilter,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleOAPIENIExists("outscale_nic.outscale_nic", &conf),
-					testAccCheckOutscaleOAPIENIAttributes(&conf, subregion),
-				),
-			},
-		},
-	})
-}
-
-func testAccCheckOutscaleOAPIENIDestroy(s *terraform.State) error {
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "outscale_nic" {
-			continue
-		}
-
-		var resp oscgo.ReadNicsResponse
-		conn := testAccProvider.Meta().(*OutscaleClient).OSCAPI
-		req := oscgo.ReadNicsRequest{
-			Filters: &oscgo.FiltersNic{NicIds: &[]string{rs.Primary.ID}},
-		}
-
-		var err error
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			rp, httpResp, err := conn.NicApi.ReadNics(context.Background()).ReadNicsRequest(req).Execute()
-			if err != nil {
-				return utils.CheckThrottling(httpResp, err)
-			}
-			resp = rp
-			return nil
-		})
-
-		if err != nil {
-			return err
-		}
-
-		if len(resp.GetNics()) != 0 {
-			return fmt.Errorf("Nic is not destroyed yet")
-		}
-	}
-	return nil
-}
-
-func testAccCheckOutscaleOAPINICDestroy(s *terraform.State) error {
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "outscale_nic" {
-			continue
-		}
-
-		conn := testAccProvider.Meta().(*OutscaleClient).OSCAPI
-		dnir := oscgo.ReadNicsRequest{
-			Filters: &oscgo.FiltersNic{NicIds: &[]string{rs.Primary.ID}},
-		}
-
-		var resp oscgo.ReadNicsResponse
-		var err error
-		err = resource.Retry(5*time.Minute, func() *resource.RetryError {
-			rp, httpResp, err := conn.NicApi.ReadNics(context.Background()).ReadNicsRequest(dnir).Execute()
-			if err != nil {
-				return utils.CheckThrottling(httpResp, err)
-			}
-			resp = rp
-			return nil
-		})
-
-		if err != nil {
-			if strings.Contains(fmt.Sprint(err), "InvalidNetworkInterfaceID.NotFound") {
-				return nil
-			}
-			errString := err.Error()
-			return fmt.Errorf("Could not find network interface: %s", errString)
-
-		}
-
-		if len(resp.GetNics()) > 0 {
-			return fmt.Errorf("Nic with id %s is not destroyed yet", rs.Primary.ID)
-		}
-	}
-
-	return nil
-}
-
-func testAccOutscaleOAPIENIDataSourceConfig(subregion string) string {
+func testAcc_Nic_DataSource_Config(subregion string) string {
 	return fmt.Sprintf(`
 		resource "outscale_net" "outscale_net" {
 			ip_range = "10.0.0.0/16"
@@ -161,43 +57,18 @@ func testAccOutscaleOAPIENIDataSourceConfig(subregion string) string {
 			}
 		}
 
-		data "outscale_nic" "outscale_nic" {
-		     filter {
-			name = "nic_ids"
-			values = [outscale_nic.outscale_nic.nic_id]
-		     }
+		data "outscale_nic" "nic" {
+			filter {
+				name = "nic_ids"
+				values = [outscale_nic.outscale_nic.nic_id]
+			} 
 		}
+
+		data "outscale_nics" "nics" {
+			filter {
+				name = "nic_ids"
+				values = [outscale_nic.outscale_nic.nic_id]
+			} 
+		}  
 	`, subregion)
 }
-
-const testAccOutscaleOAPIENIDataSourceConfigFilter = `
-	resource "outscale_net" "outscale_net" {
-		ip_range = "10.0.0.0/16"
-
-		tags {
-			key = "Name"
-			value = "testacc-nic-ds-filter"
-		}
-	}
-	
-	resource "outscale_subnet" "outscale_subnet" {
-		subregion_name = "eu-west-2a"
-		ip_range       = "10.0.0.0/16"
-		net_id         = outscale_net.outscale_net.id
-	}
-
-	resource "outscale_nic" "outscale_nic" {
-		subnet_id = outscale_subnet.outscale_subnet.id
-		tags {
-			value = "tf-value"
-			key   = "tf-key"
-		}
-	}
-
-	data "outscale_nic" "outscale_nic" {
-		filter {
-			name = "nic_ids"
-			values = [outscale_nic.outscale_nic.nic_id]
-		} 
-	}
-`
