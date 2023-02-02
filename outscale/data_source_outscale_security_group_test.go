d12093606345f8ac5b8c11675e8bd3aeaffd2b94
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:26:36 +0000

    Merge security_group datasource testacc

--- outscale/data_source_outscale_security_group_test.go
+++ outscale/data_source_outscale_security_group_test.go
@@ -6,124 +6,62 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccDataSourceOutscaleOAPISecurityGroup_basic(t *testing.T) {
+func TestAcc_SecurityGroup_DataSource(t *testing.T) {
 	t.Parallel()
 	rInt := acctest.RandInt()
+	dataSourceName := "data.outscale_security_group.sg"
+	dataSourcesName := "data.outscale_security_groups.sgs"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceOutscaleOAPISecurityGroupConfig(rInt),
+				Config: testAcc_SecurityGroup_DataSource_Config(rInt),
 				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourceOutscaleOAPISecurityGroupCheck("data.outscale_security_group.by_id"),
-					//testAccDataSourceOutscaleOAPISecurityGroupCheck("data.outscale_security_group.by_filter"),
+					resource.TestCheckResourceAttr(dataSourcesName, "security_groups.#", "2"),
+
+					resource.TestCheckResourceAttr(dataSourceName, "description", "Used in the terraform acceptance tests datasource"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccDataSourceOutscaleOAPISecurityGroupPublic(t *testing.T) {
-	t.Parallel()
-	rInt := acctest.RandInt()
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceOutscaleOAPISecurityGroupPublicConfig(rInt),
-				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourceOutscaleOAPISecurityGroupCheck("data.outscale_security_group.by_filter_public"),
-				),
-			},
-		},
-	})
-}
-
-func testAccDataSourceOutscaleOAPISecurityGroupCheck(name string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[name]
-		if !ok {
-			return fmt.Errorf("root module has no resource called %s", name)
-		}
-
-		SGRs, ok := s.RootModule().Resources["outscale_security_group.test"]
-		if !ok {
-			return fmt.Errorf("can't find outscale_security_group.test in state")
-		}
-
-		attr := rs.Primary.Attributes
-
-		if attr["security_group_id"] != SGRs.Primary.Attributes["id"] {
-			return fmt.Errorf(
-				"security_group_id is %s; want %s",
-				attr["security_group_id"],
-				SGRs.Primary.Attributes["id"],
-			)
-		}
-
-		//TODO: validate tags
-		// if attr["tag.Name"] != "tf-acctest" {
-		// 	return fmt.Errorf("bad Name tag %s", attr["tag.Name"])
-		// }
-
-		return nil
-	}
-}
-
-func testAccDataSourceOutscaleOAPISecurityGroupConfig(rInt int) string {
-	return fmt.Sprintf(`
-		resource "outscale_net" "vpc" {
-				ip_range = "10.0.0.0/16"
-				tags {
-					key = "Name"
-					value = "testacc-sec-group-ds"
-				}
-		}
-
-		resource "outscale_security_group" "test" {
-			net_id = "${outscale_net.vpc.id}"
-			description = "Used in the terraform acceptance tests"
-			security_group_name = "test-%d"
-			#tag = {
-			#	Name = "tf-acctest"
-			#	Seed = "%d"
-			#}
-		}
-
-		data "outscale_security_group" "by_id" {
-			security_group_id = "${outscale_security_group.test.id}"
-		}
-
-		#data "outscale_security_group" "by_filter" {
-		#	filter {
-		#		name = "security_group_names"
-		#		values = ["${outscale_security_group.test.security_group_name}"]
-		#	}
-		#}`, rInt, rInt)
-}
-
-func testAccDataSourceOutscaleOAPISecurityGroupPublicConfig(rInt int) string {
+func testAcc_SecurityGroup_DataSource_Config(rInt int) string {
 	return fmt.Sprintf(`
 		resource "outscale_security_group" "test" {
-			description = "Used in the terraform acceptance tests"
-			security_group_name = "test-%d"
+			description = "Used in the terraform acceptance tests datasource"
+			security_group_name = "test-1-%[1]d"
 			tag = {
 				Name = "tf-acctest"
-				Seed = "%d"
+				Seed = "%[1]d"
 			}
 		}
 
-		data "outscale_security_group" "by_filter_public" {
-			filter {
-		    name = "security_group_names"
-
-				// name = "group_name"
-				values = ["${outscale_security_group.test.security_group_name}"]
+		resource "outscale_security_group" "test2" {
+			description = "Used in the terraform acceptance tests datasource"
+			security_group_name = "test-2-%[1]d"
+			tag = {
+				Name = "tf-acctest"
+				Seed = "%[1]d"
 			}
-		}`, rInt, rInt)
+		}
+
+		data "outscale_security_group" "sg" {
+			filter {
+				name = "security_group_ids"
+				values = [outscale_security_group.test.security_group_id]
+			}
+		}
+		
+		data "outscale_security_groups" "sgs" {
+			filter {
+				name = "security_group_ids"
+				values = [outscale_security_group.test.security_group_id, outscale_security_group.test2.security_group_id]
+			}
+		}`,
+		rInt)
 }
