b6190eea1dabe0840ab39cc82770789a47d3e9c0
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:25:23 +0000

    Merge net_access_point datasource testacc

--- outscale/data_source_outscale_net_access_point_test.go
+++ outscale/data_source_outscale_net_access_point_test.go
@@ -8,10 +8,11 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccDataSourceOutscaleOAPINetAccessPoint_basic(t *testing.T) {
+func TestAcc_NetAccessPoint_DataSource(t *testing.T) {
 	t.Parallel()
 	serviceName := fmt.Sprintf("com.outscale.%s.api", os.Getenv("OUTSCALE_REGION"))
-
+	dataSourceName := "data.outscale_net_access_point.data_net_access_point"
+	dataSourcesName := "data.outscale_net_access_points.data_net_access_points"
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -19,59 +20,90 @@ func TestAccDataSourceOutscaleOAPINetAccessPoint_basic(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceOutscaleOAPINetAccessPointConfig(serviceName, serviceName),
+				Config: testAcc_NetAccessPoint_DataSource_Config(serviceName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourcesName, "net_access_points.#", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "service_name", serviceName),
+				),
 			},
 		},
 	})
 }
 
-func testAccDataSourceOutscaleOAPINetAccessPointConfig(sName, sName2 string) string {
+func testAcc_NetAccessPoint_DataSource_Config(sName string) string {
 	return fmt.Sprintf(`
-                resource "outscale_net" "outscale_net" {
-                        ip_range = "10.0.0.0/16"
-                }
+       resource "outscale_net" "outscale_net" {
+              ip_range = "10.0.0.0/16"
+       }
 
-                resource "outscale_route_table" "route_table-1" {
-                        net_id = outscale_net.outscale_net.net_id
-                }
+       resource "outscale_route_table" "route_table-1" {
+              net_id = outscale_net.outscale_net.net_id
+       }
 
-                resource "outscale_net_access_point" "net_access_point_1" {
-                        net_id          = outscale_net.outscale_net.net_id
-                        route_table_ids = [outscale_route_table.route_table-1.route_table_id] 
-                        service_name    = "%s"
-                        tags { 
-                              key       = "name" 
-                              value     = "terraform-Net-Access-Point" 
-                        }
+       resource "outscale_net_access_point" "net_access_point_1" {
+              net_id          = outscale_net.outscale_net.net_id
+              route_table_ids = [outscale_route_table.route_table-1.route_table_id] 
+              service_name    = "%[1]s"
+              tags { 
+                     key       = "name" 
+                     value     = "terraform-Net-Access-Point" 
+              }
 
-                }
+       }
 
-               data "outscale_net_access_point" "data_net_access_point" {
-                        filter {
-                               name     = "net_access_point_ids"
-                               values   = [outscale_net_access_point.net_access_point_1.net_access_point_id]
-                        }
-                        filter {
-                               name     = "net_ids"
-                               values   = [outscale_net.outscale_net.net_id]
-                        }
-                        filter {
-                               name     = "service_names"
-                               values   = [ "%s"]
-                        }
-                        filter {
-                               name     = "states"
-                               values   = ["available"]
-                        }
-                        filter {
-                               name     = "tag_keys"
-                               values   = ["name"]
-                        }
-                        filter {
-                               name     = "tag_values"
-                               values   = ["terraform-Net-Access-Point"]
-                        }
-               }
+       data "outscale_net_access_point" "data_net_access_point" {
+              filter {
+                     name     = "net_access_point_ids"
+                     values   = [outscale_net_access_point.net_access_point_1.net_access_point_id]
+              }
+              filter {
+                     name     = "net_ids"
+                     values   = [outscale_net.outscale_net.net_id]
+              }
+              filter {
+                     name     = "service_names"
+                     values   = [ "%[1]s"]
+              }
+              filter {
+                     name     = "states"
+                     values   = ["available"]
+              }
+              filter {
+                     name     = "tag_keys"
+                     values   = ["name"]
+              }
+              filter {
+                     name     = "tag_values"
+                     values   = ["terraform-Net-Access-Point"]
+              }
+       }
 
-	`, sName, sName2)
+
+       data "outscale_net_access_points" "data_net_access_points" {
+              filter {
+                     name     = "net_access_point_ids"
+                     values   = [outscale_net_access_point.net_access_point_1.net_access_point_id]
+              }
+              filter {
+                     name     = "net_ids"
+                     values   = [outscale_net.outscale_net.net_id]
+              }
+              filter {
+                     name     = "service_names"
+                     values   = [ "%[1]s"]
+              }
+              filter {
+                     name     = "states"
+                     values   = ["available"]
+              }
+              filter {
+                     name     = "tag_keys"
+                     values   = ["name"]
+              }
+              filter {
+                     name     = "tag_values"
+                     values   = ["terraform-Net-Access-Point"]
+              }
+       }
+	`, sName)
 }
