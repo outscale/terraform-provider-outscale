9c3d9024cf9d211d010be018114fecdf98fa7b2f
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 14:56:27 +0000

    Merge ca datasource testacc

--- outscale/data_source_outscale_ca_test.go
+++ outscale/data_source_outscale_ca_test.go
@@ -1,87 +1,67 @@
 package outscale
 
 import (
-	"context"
 	"fmt"
 	"testing"
-	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	oscgo "github.com/outscale/osc-sdk-go/v2"
 	"github.com/terraform-providers/terraform-provider-outscale/utils"
 )
 
-func TestAccDataOutscaleOAPICa_basic(t *testing.T) {
-	resourceName := "outscale_ca.ca_test"
-
+func TestAcc_Ca_DataSource(t *testing.T) {
+	dataSourceName := "data.outscale_ca.ca_data"
+	dataSourcesName := "data.outscale_cas.cas_data"
+	dataSourcesAllName := "data.outscale_cas.all_cas"
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccDataCheckOutscaleCaDestroy,
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataOutscaleOAPICaConfig(utils.TestCaPem),
+				Config: testAcc_Ca_DataSource_Config(utils.TestCaPem),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleCaExists(resourceName),
+					resource.TestCheckResourceAttrSet(dataSourceName, "ca_fingerprint"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "ca_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "description"),
+
+					resource.TestCheckResourceAttrSet(dataSourcesName, "cas.#"),
+					resource.TestCheckResourceAttrSet(dataSourcesName, "filter.#"),
+
+					resource.TestCheckResourceAttrSet(dataSourcesAllName, "cas.#"),
 				),
 			},
 		},
 	})
 }
 
-func testAccDataCheckOutscaleCaDestroy(s *terraform.State) error {
-	conn := testAccProvider.Meta().(*OutscaleClient).OSCAPI
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "outscale_ca" {
-			continue
-		}
-		req := oscgo.ReadCasRequest{}
-		req.Filters = &oscgo.FiltersCa{
-			CaIds: &[]string{rs.Primary.ID},
-		}
-
-		var resp oscgo.ReadCasResponse
-		var err error
-		exists := false
-		err = resource.Retry(120*time.Second, func() *resource.RetryError {
-			rp, httpResp, err := conn.CaApi.ReadCas(context.Background()).ReadCasRequest(req).Execute()
-			if err != nil {
-				return utils.CheckThrottling(httpResp, err)
-			}
-			resp = rp
-			return nil
-		})
-		if err != nil {
-			return fmt.Errorf("Ca reading (%s)", rs.Primary.ID)
-		}
-
-		for _, ca := range resp.GetCas() {
-			if ca.GetCaId() == rs.Primary.ID {
-				exists = true
-			}
-		}
-
-		if exists {
-			return fmt.Errorf("Ca still exists (%s)", rs.Primary.ID)
-		}
-	}
-	return nil
-}
-
-func testAccDataOutscaleOAPICaConfig(ca_pem string) string {
+func testAcc_Ca_DataSource_Config(ca_pem string) string {
 	return fmt.Sprintf(`
 resource "outscale_ca" "ca_test" { 
    ca_pem        =  %[1]q
-   description        = "Ca testacc create"
+   description   = "Ca testacc create"
 }
 
 data "outscale_ca" "ca_data" { 
- filter {
+  filter {
     name   = "ca_ids"
-     values = ["${outscale_ca.ca_test.id}"]
+    values = ["${outscale_ca.ca_test.id}"]
   }
 }
+
+data "outscale_cas" "cas_data" { 
+  filter {
+    name   = "ca_ids"
+    values = ["${outscale_ca.ca_test.id}"]
+  }
+  filter {
+	name   = "descriptions"
+	values = ["${outscale_ca.ca_test.description}"]
+  }
+  filter {
+	name   = "ca_fingerprints"
+	values = ["${outscale_ca.ca_test.ca_fingerprint}"]
+  }
+}
+
+data "outscale_cas" "all_cas" {}
 `, ca_pem)
 }
