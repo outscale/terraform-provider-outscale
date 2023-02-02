dea3b79290c2c90b117012f27712a24101d66493
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:26:21 +0000

    Merge route_table datasource testacc

--- outscale/data_source_outscale_route_table_test.go
+++ outscale/data_source_outscale_route_table_test.go
@@ -1,23 +1,26 @@
 package outscale
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func TestAccDataSourceOutscaleOAPIRouteTable_basic(t *testing.T) {
+func TestAcc_RouteTable_DataSource(t *testing.T) {
+	dataSourceName := "data.outscale_route_table.route_table"
+	dataSourcesName := "data.outscale_route_tables.route_tables"
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceOutscaleOAPIRouteTableGroupConfig,
+				Config: testAcc_RouteTable_DataSource_Config,
 				Check: resource.ComposeTestCheckFunc(
-					testAccDataSourceOutscaleOAPIRouteTableCheck("data.outscale_route_table.by_filter"),
-					testAccDataSourceOutscaleOAPIRouteTableCheck("data.outscale_route_table.by_id"),
+					resource.TestCheckResourceAttr(dataSourcesName, "route_tables.#", "1"),
+
+					resource.TestCheckResourceAttrSet(dataSourceName, "net_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "route_table_id"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "routes.#"),
 				),
 				ExpectNonEmptyPlan: true,
 			},
@@ -25,66 +28,7 @@ func TestAccDataSourceOutscaleOAPIRouteTable_basic(t *testing.T) {
 	})
 }
 
-func testAccDataSourceOutscaleOAPIRouteTableCheck(name string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[name]
-
-		if !ok {
-			return fmt.Errorf("root module has no resource called %s", name)
-		}
-
-		rts, ok := s.RootModule().Resources["outscale_route_table.test"]
-		if !ok {
-			return fmt.Errorf("can't find outscale_route_table.test in state")
-		}
-		vpcRs, ok := s.RootModule().Resources["outscale_net.test"]
-		if !ok {
-			return fmt.Errorf("can't find outscale_net.test in state")
-		}
-		// FIXME: Missing route_table_links on ReadRouteTables request
-		// subnetRs, ok := s.RootModule().Resources["outscale_subnet.test"]
-		// if !ok {
-		// 	return fmt.Errorf("can't find outscale_subnet.test in state")
-		// }
-		attr := rs.Primary.Attributes
-
-		if attr["id"] != rts.Primary.Attributes["id"] {
-			return fmt.Errorf(
-				"id is %s; want %s",
-				attr["id"],
-				rts.Primary.Attributes["id"],
-			)
-		}
-
-		if attr["route_table_id"] != rts.Primary.Attributes["id"] {
-			return fmt.Errorf(
-				"route_table_id is %s; want %s",
-				attr["route_table_id"],
-				rts.Primary.Attributes["id"],
-			)
-		}
-
-		if attr["net_id"] != vpcRs.Primary.Attributes["id"] {
-			return fmt.Errorf(
-				"net_id is %s; want %s",
-				attr["net_id"],
-				vpcRs.Primary.Attributes["id"],
-			)
-		}
-		// FIXME: Missing route_table_links on ReadRouteTables request
-		// if attr["route_table_links.0.subnet_id"] != subnetRs.Primary.Attributes["id"] {
-		// 	return fmt.Errorf(
-		// 		"subnet_id is %v; want %s",
-		// 		attr["route_table_link.0.subnet_id"],
-		// 		subnetRs.Primary.Attributes["id"],
-		// 	)
-		// }
-
-		return nil
-	}
-}
-
-const testAccDataSourceOutscaleOAPIRouteTableGroupConfig = `
+const testAcc_RouteTable_DataSource_Config = `
 	resource "outscale_net" "test" {
 		ip_range = "172.16.0.0/16"
 
@@ -97,10 +41,6 @@ const testAccDataSourceOutscaleOAPIRouteTableGroupConfig = `
 	resource "outscale_subnet" "test" {
 		ip_range = "172.16.0.0/24"
 		net_id   = "${outscale_net.test.id}"
-
-		#tag {
-		#  Name = "terraform-testacc-data-source"
-		#}
 	}
 
 	resource "outscale_route_table" "test" {
@@ -117,7 +57,7 @@ const testAccDataSourceOutscaleOAPIRouteTableGroupConfig = `
 		route_table_id = "${outscale_route_table.test.id}"
 	}
 
-	data "outscale_route_table" "by_filter" {
+	data "outscale_route_table" "route_table" {
 		filter {
 			name   = "route_table_ids"
 			values = ["${outscale_route_table.test.id}"]
@@ -126,8 +66,10 @@ const testAccDataSourceOutscaleOAPIRouteTableGroupConfig = `
 		depends_on = ["outscale_route_table_link.a"]
 	}
 
-	data "outscale_route_table" "by_id" {
-		route_table_id = "${outscale_route_table.test.id}"
-		depends_on     = ["outscale_route_table_link.a"]
+	data "outscale_route_tables" "route_tables" {
+		filter {
+			name   = "route_table_ids"
+			values = ["${outscale_route_table.test.id}"]
+		}
 	}
 `
