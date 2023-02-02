e6e09cd377e709287758bef2fbabcef81562542e
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:25:04 +0000

    Merge internet_service datasource testacc

--- outscale/data_source_outscale_internet_service_test.go
+++ outscale/data_source_outscale_internet_service_test.go
@@ -6,29 +6,39 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccOutscaleOAPIInternetServiceDatasource_basic(t *testing.T) {
+func TestAcc_InternetService_Datasource(t *testing.T) {
 	t.Parallel()
+	dataSourceName := "data.outscale_internet_service.internet_service"
+	dataSourcesName := "data.outscale_internet_services.internet_services"
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOutscaleOAPIInternetServiceDatasourceConfig,
+				Config: testAcc_InternetService_Datasource_Config,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.outscale_internet_service.outscale_internet_serviced", "internet_service_id"),
+					resource.TestCheckResourceAttrSet(dataSourcesName, "internet_services.#"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "internet_service_id"),
 				),
 			},
 		},
 	})
 }
 
-const testAccOutscaleOAPIInternetServiceDatasourceConfig = `
-	resource "outscale_internet_service" "outscale_internet_service" {}
+const testAcc_InternetService_Datasource_Config = `
+	resource "outscale_internet_service" "gateway" {}
 
-	data "outscale_internet_service" "outscale_internet_serviced" {
+	data "outscale_internet_service" "internet_service" {
 		filter {
 			name = "internet_service_ids"
-			values = ["${outscale_internet_service.outscale_internet_service.internet_service_id}"]
+			values = [outscale_internet_service.gateway.internet_service_id]
+		}
+	}
+
+	data "outscale_internet_services" "internet_services" {
+		filter {
+			name = "internet_service_id"
+			values = [outscale_internet_service.gateway.internet_service_id]
 		}
 	}
 `
