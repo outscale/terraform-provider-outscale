8e56123559686243f66c7360a76ba53c17bd22cf
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:23:46 +0000

    Merge image datasource testacc

--- outscale/data_source_outscale_image_test.go
+++ outscale/data_source_outscale_image_test.go
@@ -7,14 +7,16 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestAccOutscaleOAPIImageDataSource_Instance(t *testing.T) {
+func TestAcc_Image_DataSource(t *testing.T) {
 	t.Parallel()
 	omi := os.Getenv("OUTSCALE_IMAGEID")
 	region := os.Getenv("OUTSCALE_REGION")
 	imageName := fmt.Sprintf("image-test-%d", acctest.RandInt())
+
+	dataSourceName := "data.outscale_image.omi"
+	dataSourcesName := "data.outscale_images.omis"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
@@ -23,92 +25,55 @@ func TestAccOutscaleOAPIImageDataSource_Instance(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckOutscaleOAPIImageConfigBasic(omi, "tinav4.c2r2p2", region, imageName),
+				Config: testAcc_Image_DataSource_Config(omi, "tinav4.c2r2p2", region, imageName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleOAPIImageDataSourceID("data.outscale_image.nat_ami"),
-					resource.TestCheckResourceAttr("data.outscale_image.nat_ami", "architecture", "x86_64"),
+
+					resource.TestCheckResourceAttrSet(dataSourceName, "image_id"),
+					resource.TestCheckResourceAttr(dataSourcesName, "images.#", "2"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccOutscaleOAPIImageDataSource_basic(t *testing.T) {
-	t.Parallel()
-	omi := os.Getenv("OUTSCALE_IMAGEID")
-	region := os.Getenv("OUTSCALE_REGION")
-	imageName := fmt.Sprintf("image-test-%d", acctest.RandInt())
-
-	resource.Test(t, resource.TestCase{
-		PreCheck: func() {
-			testAccPreCheck(t)
-		},
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckOutscaleOAPIImageDataSourceBasicConfig(omi, "tinav4.c2r2p2", region, imageName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleOAPIImageDataSourceID("data.outscale_image.omi"),
-				),
-			},
-		},
-	})
-}
-
-func testAccCheckOutscaleOAPIImageDataSourceID(n string) resource.TestCheckFunc {
-	// Wait for IAM role
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Can't find AMI data source: %s", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("AMI data source ID not set")
-		}
-		return nil
-	}
-}
-
-func testAccCheckOutscaleOAPIImageDataSourceBasicConfig(omi, vmType, region, imageName string) string {
+func testAcc_Image_DataSource_Config(omi, vmType, region, imageName string) string {
 	return fmt.Sprintf(`
-		resource "outscale_vm" "basic" {
+		resource "outscale_vm" "basic_one" {
 			image_id                 = "%[1]s"
 			vm_type                  = "%[2]s"
 			keypair_name             = "terraform-basic"
 			placement_subregion_name = "%[3]sa"
 		}
 		
-		resource "outscale_image" "image" {
-			image_name = "%[4]s"
-			vm_id      = "${outscale_vm.basic.id}"
+		resource "outscale_vm" "basic_two" {
+			image_id			     = "%[1]s"
+			vm_type                  = "%[2]s"
+			keypair_name	         = "terraform-basic"
+			placement_subregion_name = "%[3]sa"
+		}
+
+		resource "outscale_image" "image_one" {
+			image_name = "%[4]s-one"
+			vm_id = outscale_vm.basic_one.id
+		}
+
+		resource "outscale_image" "image_two" {
+			image_name = "%[4]s-two"
+			vm_id = outscale_vm.basic_two.id
 		}
 		
 		data "outscale_image" "omi" {
 			filter {
 				name   = "image_ids"
-				values = ["${outscale_image.image.id}"]
+				values = [outscale_image.image_one.id]
 			}
 		}
-	`, omi, vmType, region, imageName)
-}
 
-func testAccCheckOutscaleOAPIImageConfigBasic(omi, vmType, region, imageName string) string {
-	return fmt.Sprintf(`
-		resource "outscale_vm" "basic" {
-			image_id			     = "%[1]s"
-			vm_type                  = "%[2]s"
-			keypair_name	           = "terraform-basic"
-			placement_subregion_name = "%[3]sa"
-		}
-
-		resource "outscale_image" "foo" {
-			image_name = "%[4]s"
-			vm_id      = "${outscale_vm.basic.id}"
-		}
-
-		data "outscale_image" "nat_ami" {
-			image_id = "${outscale_image.foo.id}"
+		data "outscale_images" "omis" {
+			filter {
+				name = "image_ids"
+				values = [outscale_image.image_one.id, outscale_image.image_two.id]
+			}
 		}
 	`, omi, vmType, region, imageName)
 }
