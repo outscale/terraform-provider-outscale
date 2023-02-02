7268aa215130c7a04923d6884ddf4112d33f1755
Frédéric Bérot-Armand <frederic.berot-armand@outscale.com>
2023-02-02 15:27:50 +0000

    Merge volume datasource testacc

--- outscale/data_source_outscale_volume_test.go
+++ outscale/data_source_outscale_volume_test.go
@@ -6,109 +6,86 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func TestAccOutscaleOAPIVolumeDataSource_basic(t *testing.T) {
+func TestAcc_Volume_DataSource(t *testing.T) {
 	t.Parallel()
 	region := os.Getenv("OUTSCALE_REGION")
+
+	dataSourceName := "data.outscale_volume.volume"
+	dataSourcesName := "data.outscale_volumes.volumes"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckOutscaleOAPIVolumeDataSourceConfig(region),
+				Config: testAcc_Volume_DataSource_Config(region),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleOAPIVolumeDataSourceID("data.outscale_volume.ebs_volume"),
-					resource.TestCheckResourceAttr("data.outscale_volume.ebs_volume", "size", "10"),
-					resource.TestCheckResourceAttr("data.outscale_volume.ebs_volume", "volume_type", "standard"),
+					resource.TestCheckResourceAttr(dataSourceName, "size", "2"),
+					resource.TestCheckResourceAttr(dataSourceName, "volume_type", "standard"),
+
+					resource.TestCheckResourceAttr(dataSourcesName, "volumes.#", "2"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccOutscaleOAPIVolumeDataSource_filterByTags(t *testing.T) {
-	t.Parallel()
-	region := os.Getenv("OUTSCALE_REGION")
+func testAcc_Volume_DataSource_Config(region string) string {
+	return fmt.Sprintf(`
+	resource "outscale_volume" "vol" {
+		subregion_name = "%[1]sa"
+		volume_type    = "standard"
+		size           = 2
 
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCheckOutscaleOAPIVolumeDataSourceConfigFilterByTags(region),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckOutscaleOAPIVolumeDataSourceID("data.outscale_volume.ebs_volume"),
-					resource.TestCheckResourceAttr("data.outscale_volume.ebs_volume", "size", "10"),
-					resource.TestCheckResourceAttr("data.outscale_volume.ebs_volume", "volume_type", "standard"),
-				),
-			},
-		},
-	})
-}
-
-func testAccCheckOutscaleOAPIVolumeDataSourceID(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("Can't find Volume data source: %s", n)
+		tags {
+			key   = "Name"
+			value = "volume-standard-1"
 		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("Volume data source ID not set")
-		}
-		return nil
 	}
-}
 
-func testAccCheckOutscaleOAPIVolumeDataSourceConfig(region string) string {
-	return fmt.Sprintf(`
-		resource "outscale_volume" "example" {
-			subregion_name = "%sa"
-			volume_type    = "standard"
-			size           = 10
+	resource "outscale_volume" "vol2" {
+		subregion_name = "%[1]sa"
+		size           = 4
+		iops           = 100
+		volume_type    = "io1"
+		tags {
+			key   = "type"
+			value = "io1"
+		}
+	}
 
-			tags {
-				key   = "Name"
-				value = "External Volume"
-			}
+	data "outscale_volume" "volume" {
+		filter {
+			name   = "tag_values"
+			values = ["volume-standard-1"]
+		}
+		filter {
+			name   = "volume_ids"
+			values = ["${outscale_volume.vol.id}"]
+		}
+		filter {
+			name   = "volume_sizes"
+			values = ["${outscale_volume.vol.size}"]
+		}
+	}
+
+	data "outscale_volumes" "volumes" {
+		filter {
+			name   = "volume_ids"
+			values = ["${outscale_volume.vol.volume_id}", "${outscale_volume.vol2.volume_id}"]
 		}
 
-		data "outscale_volume" "ebs_volume" {
-			filter {
-				name   = "volume_ids"
-				values = ["${outscale_volume.example.id}"]
-			}
-		}
-	`, region)
-}
-
-func testAccCheckOutscaleOAPIVolumeDataSourceConfigFilterByTags(region string) string {
-	return fmt.Sprintf(`
-		resource "outscale_volume" "example" {
-			subregion_name = "%sa"
-			volume_type    = "standard"
-			size           = 10
-
-			tags {
-				key   = "Name"
-				value = "volume-io1-2"
-			}
+		filter {
+			name   = "volume_sizes"
+			values = ["${outscale_volume.vol.size}", "${outscale_volume.vol2.size}"]
 		}
 
-		data "outscale_volume" "ebs_volume" {
-			filter {
-				name   = "tags"
-				values = ["Name=volume-io1-2"]
-			}
-
-			filter {
-				name   = "volume_ids"
-				values = ["${outscale_volume.example.id}"]
-			}
-
+		filter {
+			name   = "volume_types"
+			values = ["${outscale_volume.vol.volume_type}", "${outscale_volume.vol2.volume_type}"]
 		}
+	}
 	`, region)
 }
