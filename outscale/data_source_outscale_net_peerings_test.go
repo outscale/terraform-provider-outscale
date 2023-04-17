ebc8b085a1b68467b2ec8e3c170ef06a2f3ca7d7
Thiery Ouattara <opensource@outscale.com>
2023-04-17 16:12:55 +0000

    Update integration test

--- outscale/data_source_outscale_net_peerings_test.go
+++ outscale/data_source_outscale_net_peerings_test.go
@@ -40,8 +40,8 @@ const testAccDataSourceOutscaleOAPILinPeeringsConnectionConfig = `
 	}
 
 	resource "outscale_net_peering" "outscale_net_peering" {
-		accepter_net_id = "${outscale_net.outscale_net.net_id}"
-		source_net_id   = "${outscale_net.outscale_net2.net_id}"
+		accepter_net_id = outscale_net.outscale_net.net_id
+		source_net_id   = outscale_net.outscale_net2.net_id
 		tags {
 			key = "okht"
 			value = "testacc-peerings-ds"
@@ -50,14 +50,14 @@ const testAccDataSourceOutscaleOAPILinPeeringsConnectionConfig = `
 	}
 
 	resource "outscale_net_peering" "outscale_net_peering2" {
-		accepter_net_id = "${outscale_net.outscale_net.net_id}"
-		source_net_id   = "${outscale_net.outscale_net2.net_id}"
+		accepter_net_id = outscale_net.outscale_net.net_id
+		source_net_id   = outscale_net.outscale_net2.net_id
 	}
 
 	data "outscale_net_peerings" "outscale_net_peerings" {
 		filter {
 			name   = "net_peering_ids"
-			values = ["${outscale_net_peering.outscale_net_peering.net_peering_id}"]
+			values = [outscale_net_peering.outscale_net_peering.net_peering_id]
 		}
 	}
 `
