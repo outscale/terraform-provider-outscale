9b182e1f878b02183dfde2ababef5eb070ec810c
Thiery Ouattara <opensource@outscale.com>
2024-12-11 16:30:22 +0000

    Add EIM policy/policy_version resource and datasources

---
+++ outscale/resource_outscale_policy_test.go
@@ -0,0 +1,33 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccOthers_policy_basic(t *testing.T) {
+	t.Parallel()
+	resourceName := "outscale_policy.basic_policy"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccPolicyBasicConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "policy_name"),
+					resource.TestCheckResourceAttr(resourceName, "path", "/"),
+				),
+			},
+		},
+	})
+}
+
+const testAccPolicyBasicConfig = `
+	resource "outscale_policy" "basic_policy" {
+	  policy_name = "TestACC_resoucePolicy"
+	  document = "{\"Statement\": [ {\"Effect\": \"Allow\", \"Action\": [\"*\"], \"Resource\": [\"*\"]} ]}"
+	  path = "/"
+	}`
