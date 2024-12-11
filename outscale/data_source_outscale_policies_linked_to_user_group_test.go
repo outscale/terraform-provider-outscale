8b05c9fbcbaee9aca157fe1c0caca2a84003a17d
Thiery Ouattara <opensource@outscale.com>
2024-12-11 16:36:28 +0000

    Add EIM other data_sources

---
+++ outscale/data_source_outscale_policies_linked_to_user_group_test.go
@@ -0,0 +1,50 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccOthers_policies_linked_to_user_group_basic(t *testing.T) {
+	t.Parallel()
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataPoliciesLinkedConfig,
+			},
+		},
+	})
+}
+
+const testAccDataPoliciesToGroupConfig = `
+resource "outscale_user_group" "userGroupPolicies01" {
+    user_group_name = "usergroup"
+    path      = "/policiesGroup/"
+    policy {
+	   policy_orn = outscale_policy.groupLinked_01.orn
+	}
+	policy {
+	   policy_orn = outscale_policy.groupLinked_02.orn
+	}
+}
+
+resource "outscale_policy" "groupLinked_01" {
+   description = "Example Linked to user_group"
+   document    = "{\"Statement\": [ {\"Effect\": \"Allow\", \"Action\": [\"*\"], \"Resource\": [\"*\"]} ]}"
+   path        = "/TestAllOK/"
+   policy_name = "policiesGroupLinked"
+}
+resource "outscale_policy" "groupLinked_02" {
+   description = "Example Linked policy to group"
+   document    = "{\"Statement\": [ {\"Effect\": \"Deny\", \"Action\": [\"*\"], \"Resource\": [\"*\"]} ]}"
+   path        = "/Okhtgroup/"
+   policy_name = "DenyPolicy"
+}
+data "outscale_policies_linked_to_user_group" "policiesLinkToGroup" {
+	user_group_name = outscale_user_group.userGroupPolicies01.user_group_name
+	depends_on = [outscale_user_group.userGroupPolicies01]
+}
+`
---
+++ outscale/data_source_outscale_policies_linked_to_user_test.go
@@ -0,0 +1,54 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccOthers_policies_linked_to_user_basic(t *testing.T) {
+	t.Parallel()
+	resourceName := "data.outscale_policies_linked_to_user.policiesLinkedToUser"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataPoliciesLinkedConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "policies.#"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataPoliciesLinkedConfig = `
+resource "outscale_user" "userPolicies01" {
+    user_name = "userLedGroup"
+    path      = "/policiesUser/"
+    policy {
+	   policy_orn = outscale_policy.policyLinked_01.orn
+	}
+	policy {
+	   policy_orn = outscale_policy.policyLinked_02.orn
+	}
+}
+
+resource "outscale_policy" "policyLinked_01" {
+   description = "Example Linked to user"
+   document    = "{\"Statement\": [ {\"Effect\": \"Allow\", \"Action\": [\"*\"], \"Resource\": [\"*\"]} ]}"
+   path        = "/Allow_est/"
+   policy_name = "policiesLinked"
+}
+resource "outscale_policy" "policyLinked_02" {
+   description = "Example Linked policy to user"
+   document    = "{\"Statement\": [ {\"Effect\": \"Deny\", \"Action\": [\"*\"], \"Resource\": [\"*\"]} ]}"
+   path        = "/OkhtTest/"
+   policy_name = "policyStopAll"
+}
+data "outscale_policies_linked_to_user" "policiesLinkedToUser" {
+	user_name = outscale_user.userPolicies01.user_name
+	depends_on = [outscale_user.userPolicies01]
+}`
