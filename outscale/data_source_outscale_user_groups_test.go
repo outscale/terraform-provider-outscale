1a0f807df36c5d1ba23561cea9330ca707cb83b3
Thiery Ouattara <opensource@outscale.com>
2024-12-11 16:24:19 +0000

    Add EIM user_group resource and datasources

---
+++ outscale/data_source_outscale_user_groups_test.go
@@ -0,0 +1,38 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccOthers_user_groups_basic(t *testing.T) {
+	t.Parallel()
+	resourceName := "data.outscale_user_groups.basicUGTest"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataUserGroupsBasicConfig,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "user_groups.#"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataUserGroupsBasicConfig = `
+	resource "outscale_user_group" "uGroupData" {
+		user_group_name = "TestACC_uGdata"
+		path = "/"
+	}
+	data "outscale_user_groups" "basicUGTest" {
+		filter {
+			name   = "user_group_ids"
+			values = [outscale_user_group.uGroupData.user_group_id]
+		}
+    }
+`
