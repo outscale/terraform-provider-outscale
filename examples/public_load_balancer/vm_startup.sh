e1cb81260acb53db51350cd942b1421dbed89b2f
Thiery Ouattara <opensource@outscale.com>
2025-03-19 14:19:28 +0000

    :rotating_light: Update example tests OMI and vm type

--- examples/public_load_balancer/vm_startup.sh
+++ examples/public_load_balancer/vm_startup.sh
@@ -4,6 +4,7 @@ colors=("blue" "red" "green" "yellow" "black")
 node_color=${colors[node_number]}
 dest="/var/www/html/index.html"
 apt-get update -y
+apt-get -qy upgrade
 apt-get install -y lighttpd
 echo "<html><body style=\"background-color:${node_color};color:white;text-align:center;font-size:80px;\">Node ${node_number}</body></html>" > $dest
 chmod a+r $dest
--- examples/yunohost/vm_startup.sh
+++ examples/yunohost/vm_startup.sh
@@ -4,7 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get -y update
 apt-get -qy upgrade
 apt-get -y install curl
-apt-get -y install lighttpd
 curl -o /tmp/install.sh "https://install.yunohost.org"
 chmod +x /tmp/install.sh
 /tmp/install.sh -a
