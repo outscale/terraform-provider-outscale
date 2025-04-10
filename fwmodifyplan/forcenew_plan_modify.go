17343a3e4c64a5f717ed33754b51577a8b128645
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-04-10 13:20:15 +0000

    :recycle: refactor: subnet resource migrate from SDKv2 to Framework and update tests

---
+++ fwmodifyplan/forcenew_plan_modify.go
@@ -0,0 +1,39 @@
+package fwmodifyplan
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+)
+
+var _ planmodifier.String = forceNewPlanModify{}
+
+type forceNewPlanModify struct{}
+
+func (f forceNewPlanModify) Description(_ context.Context) string {
+	return "Changing the parameter will destroy and recreate the resource."
+}
+
+func (f forceNewPlanModify) MarkdownDescription(ctx context.Context) string {
+	return f.Description(ctx)
+}
+
+// Only force new the attribute when State and Config values differ.
+// This is necessary when RequiresReplace is combined with Computed and Optional,
+// see https://github.com/hashicorp/terraform-plugin-framework/issues/187
+func (f forceNewPlanModify) PlanModifyString(ctx context.Context, req planmodifier.StringRequest, resp *planmodifier.StringResponse) {
+	resp.RequiresReplace = false
+
+	// Do nothing if there is no state value or an unknown configuration value.
+	if req.StateValue.IsNull() || req.ConfigValue.IsUnknown() || req.ConfigValue.IsNull() {
+		return
+	}
+
+	if req.StateValue.ValueString() != req.ConfigValue.ValueString() {
+		resp.RequiresReplace = true
+	}
+}
+
+func ForceNewFramework() planmodifier.String {
+	return forceNewPlanModify{}
+}
---
+++ fwmodifyplan/forcenew_plan_modify_test.go
@@ -0,0 +1,62 @@
+package fwmodifyplan
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+// This test covers possible cases with Computed,
+// Optionnal and the Plan Modification on an attribute
+func TestFwForceNewmodifyplan(t *testing.T) {
+	t.Parallel()
+
+	s1 := "eu-west-2a"
+	s2 := "eu-west-2b"
+	cases := map[string]struct {
+		ConfigValue     types.String
+		StateValue      types.String
+		RequiresReplace bool
+	}{
+		"same_values": {
+			ConfigValue:     types.StringValue(s1),
+			StateValue:      types.StringValue(s1),
+			RequiresReplace: false,
+		},
+		"different_values": {
+			ConfigValue:     types.StringValue(s1),
+			StateValue:      types.StringValue(s2),
+			RequiresReplace: true,
+		},
+		"config_null": {
+			ConfigValue:     types.StringNull(),
+			StateValue:      types.StringValue(s1),
+			RequiresReplace: false,
+		},
+		"config_unknown": {
+			ConfigValue:     types.StringUnknown(),
+			StateValue:      types.StringValue(s1),
+			RequiresReplace: false,
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			req := planmodifier.StringRequest{
+				ConfigValue: tc.ConfigValue,
+				StateValue:  tc.StateValue,
+			}
+			resp := planmodifier.StringResponse{}
+
+			ForceNewFramework().PlanModifyString(context.Background(), req, &resp)
+			if !tc.RequiresReplace && resp.RequiresReplace {
+				t.Errorf("got unexpected error: %s", resp.Diagnostics.Errors())
+			}
+			if tc.RequiresReplace && !resp.RequiresReplace {
+				t.Error("expected replace, got none")
+			}
+		})
+	}
+}
