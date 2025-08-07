94a77f1e46867d454e2542125afbf1f3ef3a4804
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-08-07 16:18:47 +0000

    :sparkles: feat: OKS Project resource

---
+++ fwvalidators/cidr_validator.go
@@ -0,0 +1,41 @@
+package fwvalidators
+
+import (
+	"context"
+	"net"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/helpers/validatordiag"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+var _ validator.String = cidrValidator{}
+
+type cidrValidator struct{}
+
+func (v cidrValidator) Description(_ context.Context) string {
+	return "Value must be a CIDR"
+}
+
+func (v cidrValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+func (v cidrValidator) ValidateString(ctx context.Context, req validator.StringRequest, response *validator.StringResponse) {
+	if req.ConfigValue.IsNull() || req.ConfigValue.IsUnknown() {
+		return
+	}
+
+	if _, _, err := net.ParseCIDR(req.ConfigValue.ValueString()); err != nil {
+		response.Diagnostics.Append(validatordiag.InvalidAttributeValueDiagnostic(
+			req.Path,
+			v.Description(ctx)+err.Error(),
+			req.ConfigValue.ValueString(),
+		))
+		return
+	}
+
+}
+
+func IsCIDR() validator.String {
+	return cidrValidator{}
+}
