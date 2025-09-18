10f65b8cab8fbf050ca57b594a6d820754a08297
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-09-18 15:32:26 +0000

    :construction: test: cluster integration tests

--- tests/qa_provider_oapi/data/oks_project/TF-207_oks_project_resource_attributes_ok/step2.oks_project_resource_attributes_ok.tf
+++ tests/qa_provider_oapi/data/oks_project/TF-207_oks_project_resource_attributes_ok/step2.oks_project_resource_attributes_ok.tf
@@ -1,11 +1,11 @@
 resource "outscale_oks_project" "project" {
-  name = "oks-project-tf-207"
-  cidr = "10.50.0.0/18"
-  region = "eu-west-2"
+  name                    = "oks-project-tf-207"
+  cidr                    = "10.50.0.0/18"
+  region                  = "eu-west-2"
   disable_api_termination = false
-  description = "TF207 OKS project"
+  description             = "TF207 OKS project"
   tags = {
-    name = "TF-207"
-    key = "value"
+    test = "TF-207"
+    key  = "value"
   }
 }
