8878b66fda993b18818ec996e7af1c0bcc055265
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-09-23 12:59:20 +0000

    :construction: test: add datasource tests

---
+++ outscale/data_source_oks_quotas_test.go
@@ -0,0 +1,25 @@
+package outscale
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+)
+
+func TestAccOKSQuotasDataSource_basic(t *testing.T) {
+	resourceName := "data.outscale_oks_quotas.quotas"
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: defineTestProviderFactoriesV6(),
+
+		Steps: []resource.TestStep{
+			{
+				Config: `data "outscale_oks_quotas" "quotas" {}`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "clusters_per_project"),
+					resource.TestCheckResourceAttrSet(resourceName, "projects"),
+				),
+			},
+		},
+	})
+}
