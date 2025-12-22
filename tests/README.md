84e64828d043eeba662a53666d16978626e572fc
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-12-22 12:26:42 +0000

    🗑️ chore: clean qa files

--- tests/README.md
+++ tests/README.md
@@ -1,10 +1,10 @@
-# Test Terraform provider for Outscale API
+# Outscale API provider tests
 
-## Getting Started
+## How do test (Draft)
 
 ### Python prerequisites
 
-You will need Python 3.
+You need Python 3.
 
 You can create a Virtual Env and install dependencies following command lines below:
 ```bash
@@ -15,25 +15,17 @@ pip install -r requirements.txt
 
 ### Terraform prerequisites
 
-You will need a Terraform v0.12.16 in your PATH.
-
-## Usage
+You need Terraform v0.13 or greater installed in /usr/local/bin.
 
 ### Configuration
 
-TODO: Template....
+* Add AK, SK and region name in provider.auto.tfvars
+* Configure account_id, image_id (and other needed resources) in resources.auto.tfvars
 
 ### Execute tests
 
 ```bash
-cd qa_provider_oapi
 pytest [-s] [-k <test_name>] -v ./test_provider_oapi.py
 ```
 * use '-s' for more detailed log in console
 * use '-k' to execute a subset of tests
-
-## Add new tests
-
-TODO...
-
-
--- tests/check.py
+++ tests/check.py
@@ -1,48 +1,51 @@
 #!/usr/bin/python
-"""
-"""
+
 import json
+import logging
 import pprint
 import sys
-import logging
 
 
 def looking_for_attributes(resource):
     """Fetch attributes dict in resource
-    In the following example resources.instances[0].attributes will be returned
-{
-  "version": 4,
-  "terraform_version": "0.12.16",
-  "serial": 3,
-  "lineage": "5e62916e-5910-65a9-a2a5-4463f0c9a308",
-  "outputs": {},
-  "resources": [
+        In the following example resources.instances[0].attributes will be returned
     {
-      "mode": "managed",
-      "type": "outscale_vm",
-      "name": "vm001",
-      "provider": "provider.outscale",
-      "instances": [
+      "version": 4,
+      "terraform_version": "0.12.16",
+      "serial": 3,
+      "lineage": "5e62916e-5910-65a9-a2a5-4463f0c9a308",
+      "outputs": {},
+      "resources": [
         {
-          "schema_version": 0,
-          "attributes": {
-            "admin_password": "",
-            "architecture": "x86_64",
-            "block_device_mappings": [],
-    ....
+          "mode": "managed",
+          "type": "outscale_vm",
+          "name": "vm001",
+          "provider": "provider.outscale",
+          "instances": [
+            {
+              "schema_version": 0,
+              "attributes": {
+                "admin_password": "",
+                "architecture": "x86_64",
+                "block_device_mappings": [],
+        ....
 
 
     """
-    target_resource_key = [res for res in resource.keys() if res not in ['mode', 'type', 'name', 'provider', 'each']]
+    target_resource_key = [
+        res
+        for res in resource.keys()
+        if res not in ["mode", "type", "name", "provider", "each"]
+    ]
     if len(target_resource_key) != 1:
-        print('Can not guess resource in {}'.format(resource.keys()))
+        print("Can not guess resource in {}".format(resource.keys()))
         return None
     target_resource_key = target_resource_key[0]
     if not isinstance(resource[target_resource_key], list):
-        print('Expecting resource to be a list')
+        print("Expecting resource to be a list")
         return None
     target_resource = resource[target_resource_key][0]
-    attributes = target_resource['attributes']
+    attributes = target_resource["attributes"]
     return attributes
 
 
@@ -54,12 +57,12 @@ def flattern(content, line, result):
         if isinstance(right, (bool, int, str)) or right is None:
             result.append("{}.{}".format(line, left))  # end
         elif isinstance(right, list) and len(right) != 0:
-            result.append('{}.{}.#'.format(line, left))
+            result.append("{}.{}.#".format(line, left))
             for rright_count, rright in enumerate(right):
-                current_line = '{}.{}.{}'.format(line, left, rright_count)
+                current_line = "{}.{}.{}".format(line, left, rright_count)
                 flattern(rright, current_line, result)
         elif isinstance(right, list) and len(right) == 0:
-            result.append('{}.{}'.format(line, left))
+            result.append("{}.{}".format(line, left))
 
 
 def parse_terraform_state_pull(reported, item):
@@ -68,16 +71,20 @@ def parse_terraform_state_pull(reported, item):
     :param str item: item you want to check
     """
     result = []
-    with open(reported, 'r') as report:
+    with open(reported, "r") as report:
         reported_content = json.loads(report.read())
-    resource_item_content = [resource for resource in reported_content['resources'] if resource['type'] == item]
+    resource_item_content = [
+        resource
+        for resource in reported_content["resources"]
+        if resource["type"] == item
+    ]
     if len(resource_item_content) == 0:
-        print('ERROR. Looking for type={}. See content:\n'.format(item))
+        print("ERROR. Looking for type={}. See content:\n".format(item))
         pprint.pprint(reported_content)
         return 1
     resource_item_content = resource_item_content[0]
     attributes = looking_for_attributes(resource_item_content)
-    flattern(attributes, '', result)
+    flattern(attributes, "", result)
     result = [res[1:] for res in result]
     result.sort()
     return result
@@ -87,7 +94,7 @@ def patch_item(item):
     """ugly way to patch without modifying all existing runCmd(./check.py ... )
     Terraform0.11 has items like outscale_public_ip.outscale_public_ip where terraform 0.12 "type" field is just outscale_public_ip
     """
-    items = item.split('.')
+    items = item.split(".")
     if len(items) == 2:
         return items[1]
     return item
@@ -99,35 +106,38 @@ def main(reported, attended, item):
     :param str attended: filepath to file which lists datasources attributes
     :param str item: item you want to check
     """
-    logger = logging.getLogger('tpd_test')
+    logger = logging.getLogger("tpd_test")
     item = patch_item(item)
     reported_result = parse_terraform_state_pull(reported, item)
     attended_result = []
-    with open(attended, 'r') as att:
-        attended_result = [line for line in att.read().splitlines() if not line.startswith('#')]
+    with open(attended, "r") as att:
+        attended_result = [
+            line for line in att.read().splitlines() if not line.startswith("#")
+        ]
     if reported_result == 1:
-        logger.debug('???')
-        return ['unknown error ???']
+        logger.debug("???")
+        return ["unknown error ???"]
     missing = set(attended_result) - set(reported_result)
     unknown = set(reported_result) - set(attended_result)
     if unknown or missing:
-        logger.debug('===== check.py : Differences between terraform reported (pull result) attributes and attended attributes')
+        logger.debug(
+            "===== check.py : Differences between terraform reported (pull result) attributes and attended attributes"
+        )
     for item in unknown:
-        logger.debug('Unknown: {}'.format(item))
+        logger.debug("Unknown: {}".format(item))
     for item in missing:
-        logger.debug('Missing: {}'.format(item))
+        logger.debug("Missing: {}".format(item))
     if len(missing) != 0:
         return missing
     return None
 
 
 def check():
-    """Ce patch degueu pour eviter de commenter tous les tests avec from check import check
-    """
+    """Ce patch degueu pour eviter de commenter tous les tests avec from check import check"""
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) == 4:
         return_code = main(sys.argv[1], sys.argv[2], sys.argv[3])
         sys.exit(return_code)
