95dcdf0ce7dd480adea50a043d021be2e7a7abc6
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2025-12-23 13:53:02 +0000

    :white_check_mark: test: update ref files

--- tests/data/keypair/TF-77_keypair_resource_attributes_ok2/step1.keypair_resource_attributes_ok2.tf
+++ tests/data/keypair/TF-77_keypair_resource_attributes_ok2/step1.keypair_resource_attributes_ok2.tf
@@ -1,4 +1,4 @@
 resource "outscale_keypair" "outscale_keypair" {
     keypair_name = "test-keypair-${random_string.suffix[0].result}"
-    public_key   = file("keypair/TF-77_keypair_resource_attributes_ok2/keypair_public_test.pub")
+    public_key   = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCow1EsH10/Xs1/8ZWiOttAaIJnSC2IlKxWTZftyotGU7CwNUtsw9g6MjTLYGTbOeSilbhAxRyuvrm2h4AwLo9Me4Oc6UhAzQwhANHYGMP0YA7t0R6qJ+d4/sPojnwcI3cT6ysIw0y7GwDeWHVq88AazdXXqK/JhNAE+pbdUV4eESj2z/jgDXaZepdCphd0cBA0vgz4+m2ONq41TWcAZl3/2ZzqGn8931a8QlmZCiUw3EPnB9O3GYJimk6pqV651PokWcgkYhuCDUjuGAbSGtPfHKTGcZc6DCVXBXapbdejZ3CqBGfvH8Zt+0XklaesmvwYc/UTgGK1+cEbTZ5n+H07 meriem.zouari.ext@mariem-zouari.local"
 }
