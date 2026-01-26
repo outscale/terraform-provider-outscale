f31eee62337ed17a8e7aa25f72c0a8ce8ecb086e
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2026-01-26 14:59:39 +0000

    :white_check_mark: test(fgpu): more restrictive datasource filtering
    - test parallelization causes datasource filters to match multpile
      fgpus which causes the tests to fail randomly

--- tests/data/oapi/flexible_gpus/TF-175_flexible_gpus_datasource_attributes_ok/step1.flexible_gpus_datasource_attributes_ok.tf
+++ tests/data/oapi/flexible_gpus/TF-175_flexible_gpus_datasource_attributes_ok/step1.flexible_gpus_datasource_attributes_ok.tf
@@ -1,78 +1,82 @@
 resource "outscale_keypair" "my_keypair" {
- keypair_name = "test-keypair-${random_string.suffix[0].result}"
+  keypair_name = "test-keypair-${random_string.suffix[0].result}"
 }
 
 resource "outscale_security_group" "my_sgfgs" {
-   description         = "test security group_fg"
-   security_group_name = "test-sg-${random_string.suffix[0].result}"
+  description         = "test security group_fg"
+  security_group_name = "test-sg-${random_string.suffix[0].result}"
 }
 
 resource "outscale_vm" "MaVM" {
-   image_id                       = var.image_id
-   vm_type                        = var.fgpu_vm_type
-   keypair_name                   = outscale_keypair.my_keypair.keypair_name
-   security_group_ids             = [outscale_security_group.my_sgfgs.security_group_id]
-   placement_subregion_name       = "${var.region}a"
-   vm_initiated_shutdown_behavior = "restart"
+  image_id                       = var.image_id
+  vm_type                        = var.fgpu_vm_type
+  keypair_name                   = outscale_keypair.my_keypair.keypair_name
+  security_group_ids             = [outscale_security_group.my_sgfgs.security_group_id]
+  placement_subregion_name       = "${var.region}a"
+  vm_initiated_shutdown_behavior = "restart"
 }
 
 resource "outscale_flexible_gpu" "fGPU-1" {
-   model_name                   =  "nvidia-p6"
-   generation                   =  var.fgpu_gen
-   subregion_name               =  "${var.region}a"
-   delete_on_vm_deletion        =   true
+  model_name            = "nvidia-p6"
+  generation            = var.fgpu_gen
+  subregion_name        = "${var.region}a"
+  delete_on_vm_deletion = true
 }
 
 resource "outscale_flexible_gpu" "fGPU-2" {
-   model_name                   =  "nvidia-p6"
-   generation                   =  var.fgpu_gen
-   subregion_name               =  "${var.region}a"
-   delete_on_vm_deletion        =   true
+  model_name            = "nvidia-p6"
+  generation            = var.fgpu_gen
+  subregion_name        = "${var.region}a"
+  delete_on_vm_deletion = true
 }
 
 resource "outscale_flexible_gpu_link" "link_fGPU" {
-  flexible_gpu_ids              = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id]
-    vm_id                        = outscale_vm.MaVM.vm_id
+  flexible_gpu_ids = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id]
+  vm_id            = outscale_vm.MaVM.vm_id
 }
 
 data "outscale_flexible_gpus" "data-fGPU-1" {
-filter {
-        name     = "flexible_gpu_ids"
-        values   = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id,outscale_flexible_gpu.fGPU-2.flexible_gpu_id]
-    }
-depends_on =[outscale_flexible_gpu_link.link_fGPU]
+  filter {
+    name   = "flexible_gpu_ids"
+    values = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id, outscale_flexible_gpu.fGPU-2.flexible_gpu_id]
+  }
+  depends_on = [outscale_flexible_gpu_link.link_fGPU]
 }
 
 data "outscale_flexible_gpu" "data-fGPU-2" {
 
-filter {
-        name     = "delete_on_vm_deletion"
-        values   = [true]
-    }
   filter {
-        name     = "generations"
-        values   = [var.fgpu_gen]
-    }
+    name   = "delete_on_vm_deletion"
+    values = [true]
+  }
   filter {
-        name     = "states"
-        values   = ["attached"]
-    }
+    name   = "generations"
+    values = [var.fgpu_gen]
+  }
   filter {
-        name     = "model_names"
-        values   = ["nvidia-p6"]
-    }
+    name   = "states"
+    values = ["attached"]
+  }
   filter {
-        name     = "subregion_names"
-        values   = ["${var.region}a"]
-    }
+    name   = "model_names"
+    values = ["nvidia-p6"]
+  }
+  filter {
+    name   = "subregion_names"
+    values = ["${var.region}a"]
+  }
+  filter {
+    name   = "flexible_gpu_ids"
+    values = [outscale_flexible_gpu.fGPU-1.flexible_gpu_id]
+  }
 
-depends_on =[outscale_flexible_gpu_link.link_fGPU]
+  depends_on = [outscale_flexible_gpu_link.link_fGPU]
 }
 
 data "outscale_flexible_gpu" "data-fGPU-3" {
- filter {
-        name     = "vm_ids"
-        values   = [outscale_vm.MaVM.vm_id]
-    }
-depends_on =[outscale_flexible_gpu_link.link_fGPU]
+  filter {
+    name   = "vm_ids"
+    values = [outscale_vm.MaVM.vm_id]
+  }
+  depends_on = [outscale_flexible_gpu_link.link_fGPU]
 }
