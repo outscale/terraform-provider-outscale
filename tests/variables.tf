d1904346ab6c82d9a571ae3d1aa90ef27c0ab6ba
Lucas Rodriguez <lucas.rodriguez@outscale.com>
2026-02-03 19:07:42 +0000

    :white_check_mark: test: update ref files

--- tests/variables.tf
+++ tests/variables.tf
@@ -1,18 +1,12 @@
-# provider configuration
-#variable "account_id" {}
-#variable "access_key_id" {}
-#variable "secret_key_id" {}
-
 # resources configuration
 variable "image_id" {}
 variable "region" {}
-variable "service_name" {}
 variable "osu_bucket_name" {}
 variable "account_id" {}
 
 variable "vm_type" {
   type    = string
-  default = "tinav5.c2r2p2"
+  default = "tinav7.c2r2p1"
 }
 
 variable "fgpu_gen" {
